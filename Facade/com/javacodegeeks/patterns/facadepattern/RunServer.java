bf55c3d248cf1eabbfdeb29ead356e5e1ac2a871
fernal73 <linus.fernandes@gmail.com>
2019-05-03 10:28:46 +0530

    Added Facade pattern

---
+++ Facade/com/javacodegeeks/patterns/facadepattern/RunServer.java
@@ -0,0 +1,27 @@
+package com.javacodegeeks.patterns.facadepattern;
+
+public class RunServer {
+
+	public static void main(String[] args) {
+		ScheduleServer scheduleServer = new ScheduleServer();
+		
+		scheduleServer.startBooting();
+		scheduleServer.readSystemConfigFile();
+		scheduleServer.init();
+		scheduleServer.initializeContext();
+		scheduleServer.initializeListeners();
+		scheduleServer.createSystemObjects();
+		
+		System.out.println("Start working......");
+		System.out.println("After work done.........");
+		
+		scheduleServer.releaseProcesses();
+		scheduleServer.destroy();
+		scheduleServer.destroySystemObjects();
+		scheduleServer.destroyListeners();
+		scheduleServer.destroyContext();
+		scheduleServer.shutdown();
+	}
+
+}
+
