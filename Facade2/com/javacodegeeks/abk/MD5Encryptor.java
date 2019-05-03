6dd9dfce09feeace079f7cda4e9e3a0d80528c96
fernal73 <linus.fernandes@gmail.com>
2019-05-03 22:09:54 +0530

    Used fastest byte to hex converter

--- Facade2/com/javacodegeeks/abk/MD5Encryptor.java
+++ Facade2/com/javacodegeeks/abk/MD5Encryptor.java
@@ -1,6 +1,7 @@
 package com.javacodegeeks.abk;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import static converter.ByteToHex.*;
 public class MD5Encryptor implements Encrypt {
     public String encrypt(String text) {
         String hash = "";
@@ -9,12 +10,7 @@ public class MD5Encryptor implements Encrypt {
             msgDigest.update(text.getBytes());
 
             byte textBytes[] = msgDigest.digest();
-            StringBuffer buffer = new StringBuffer();
-            for (int i = 0; i < textBytes.length; i++) {
-    buffer.append(Character.forDigit((textBytes[i] >> 4) & 0xF, 16));
-    buffer.append(Character.forDigit((textBytes[i] & 0xF), 16));
-}
-            hash = buffer.toString();
+            hash = getHex4(textBytes);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
--- Facade2/com/javacodegeeks/abk/SHA256.java
+++ Facade2/com/javacodegeeks/abk/SHA256.java
@@ -2,6 +2,7 @@ package com.javacodegeeks.abk;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import static converter.ByteToHex.*;
 public class SHA256 implements Encrypt {
     public String encrypt(String text) {
         String hash = "";
@@ -9,11 +10,7 @@ public class SHA256 implements Encrypt {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] textBytes = digest.digest(text.getBytes(StandardCharsets.UTF_8));
             
-            StringBuffer buffer = new StringBuffer();
-            for (int i = 0; i < textBytes.length; i++) {
-             buffer.append(Integer.toString((textBytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-                hash = buffer.toString();
+            hash = getHex4(textBytes);
         }
         catch(NoSuchAlgorithmException e) {
             e.printStackTrace();
