5a389574c22f6568639c62486b6f5b4dafecf968
fernal73 <linus.fernandes@gmail.com>
2019-05-10 17:44:42 +0530

    fix: Fixed style check for for loops

--- ByteToHex/TestByteToHex.java
+++ ByteToHex/TestByteToHex.java
@@ -5,33 +5,46 @@
  * @copyright : Copyright (c) Linus Fernandes
  * @description :
  */
+
+
+
 public class TestByteToHex {
+  /**
+   * Describe <code>main</code> method here.
+   *
+   * @param args a <code>String</code> value
+   */
   public static void main(String[] args) {
-    long start, end, elapsed;
-    String hex, hex2, hex3, hex4;
-    String byteString =
-        "@#£&_-()=%?!/:'*\"[]{}<>^¡¿~™®©¢¥€$123456789003356788990335688335678888))5778889===66://))*£&'/!!))))?:/!??????!//!!!!!!!!!?????      dffvbbfrewshjoohgvvvzscvbmmmxxvffew236889uygghhbhjkiu65fvbhbbvvvvvdew13yhgftggjioo9hhgggggvvgdWeryhhhDFGJKYRESCHJKKOKVVCSSDVNJHFDSSSGHIJJH";
-    byte[] raw = byteString.getBytes();
-    start = System.nanoTime();
-    hex = ByteToHex.getHex(raw);
-    end = System.nanoTime();
-    elapsed = end - start;
+    final String byteString =
+        "@#£&_-()=%?!/:'*\"[]{}<>^¡¿~™®©¢¥€"
+        + "$123456789003356788990335688335678888"
+        + "))5778889===66://))*£&'/!!))))?:/!?"
+        + "?????!//!!!!!!!!!?????      dffvbbfrews"
+        + "hjoohgvvvzscvbmmmxxvffew236889uygghhbhjk"
+        + "iu65fvbhbbvvvvvdew13yhgftggjioo9hhgggg"
+        + "gvvgdWeryhhhDFGJKYRESCHJKKOKVVCSSDVNJH"
+        + "FDSSSGHIJJH";
+    final byte[] raw = byteString.getBytes();
+    long start = System.nanoTime();
+    final String hex = ByteToHex.getHex(raw);
+    long end = System.nanoTime();
+    long elapsed = end - start;
     System.out.println("getHex: " + elapsed);
 
     start = System.nanoTime();
-    hex2 = ByteToHex.getHex2(raw);
+    final String hex2 = ByteToHex.getHex2(raw);
     end = System.nanoTime();
     elapsed = end - start;
     System.out.println("getHex2: " + elapsed);
 
     start = System.nanoTime();
-    hex3 = ByteToHex.getHex3(raw);
+    final String hex3 = ByteToHex.getHex3(raw);
     end = System.nanoTime();
     elapsed = end - start;
     System.out.println("getHex3: " + elapsed);
 
     start = System.nanoTime();
-    hex4 = ByteToHex.getHex4(raw);
+    final String hex4 = ByteToHex.getHex4(raw);
     end = System.nanoTime();
     elapsed = end - start;
     System.out.println("getHex4: " + elapsed);
