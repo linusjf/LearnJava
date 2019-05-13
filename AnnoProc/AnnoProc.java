e3134833613f85a6bc12de855f28728c2d2a5d0b
fernal73 <linus.fernandes@gmail.com>
2019-05-13 11:50:03 +0530

    refactor: Refactored build files to support checkstyle properties

--- AnnoProc/AnnoProc.java
+++ AnnoProc/AnnoProc.java
@@ -1,5 +1,7 @@
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
