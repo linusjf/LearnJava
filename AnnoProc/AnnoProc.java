0e1343c19985c3f664ba3c8a512439fd54db53c4
fernal73 <linus.fernandes@gmail.com>
2019-05-13 09:13:51 +0530

    style: Javadoc comments added

--- AnnoProc/AnnoProc.java
+++ AnnoProc/AnnoProc.java
@@ -1,14 +1,32 @@
-import java.util.*;
-import javax.annotation.processing.*;
-import javax.lang.model.*;
-import javax.lang.model.element.*;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
 
+
+/**
+ * Describe class <code>AnnoProc</code> here.
+ *
+ * @author <a href="mailto:root@localhost"></a>
+ * @version 1.0
+ */
 @SupportedAnnotationTypes("SuppressWarnings")
 public class AnnoProc extends AbstractProcessor {
+  /**
+   * Describe <code>process</code> method here.
+   *
+   * @param renv a <code>RoundEnvironment</code> value
+   * @return a <code>boolean</code> value
+   */
   public boolean process(Set<? extends TypeElement> elems, RoundEnvironment renv) {
     return true;
   }
 
+  /**
+   * Describe <code>getSupportedSourceVersion</code> method here.
+   *
+   * @return a <code>SourceVersion</code> value
+   */
   public SourceVersion getSupportedSourceVersion() {
     return SourceVersion.latest();
   }
