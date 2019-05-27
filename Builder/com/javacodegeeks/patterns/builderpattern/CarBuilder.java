f6005a91cd49947e75ca2609c1b3a46df205002d
fernal73 <linus.fernandes@gmail.com>
2019-05-27 19:31:13 +0530

    Another Builder example

---
+++ Builder/com/javacodegeeks/patterns/builderpattern/CarBuilder.java
@@ -0,0 +1,12 @@
+package com.javacodegeeks.patterns.builderpattern;
+
+public interface CarBuilder {
+  public void buildBodyStyle();
+  public void buildPower();
+  public void buildEngine();
+  public void buildBrakes();
+  public void buildSeats();
+  public void buildWindows();
+  public void buildFuelType();
+  public Car getCar();
+}
---
+++ Builder/com/javacodegeeks/patterns/builderpattern/package-info.java
@@ -0,0 +1,1 @@
+package com.javacodegeeks.patterns.builderpattern;
