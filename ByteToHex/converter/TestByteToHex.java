fb47df3fad4f20d4c589bf92831a2e5fd130fde3
fernal73 <linus.fernandes@gmail.com>
2019-05-29 19:06:35 +0530

    Fixed to add version to jar outout

--- ByteToHex/converter/TestByteToHex.java
+++ ByteToHex/converter/TestByteToHex.java
@@ -32,25 +32,25 @@ public enum TestByteToHex {
     final String hex = ByteToHex.getHex(raw);
     long end = System.nanoTime();
     long elapsed = end - start;
-    System.out.println("getHex: " + elapsed);
+    System.out.println("getHex: time " + elapsed);
 
     start = System.nanoTime();
     final String hex2 = ByteToHex.getHex2(raw);
     end = System.nanoTime();
     elapsed = end - start;
-    System.out.println("getHex2: " + elapsed);
+    System.out.println("getHex2: time " + elapsed);
 
     start = System.nanoTime();
     final String hex3 = ByteToHex.getHex3(raw);
     end = System.nanoTime();
     elapsed = end - start;
-    System.out.println("getHex3: " + elapsed);
+    System.out.println("getHex3: time " + elapsed);
 
     start = System.nanoTime();
     final String hex4 = ByteToHex.getHex4(raw);
     end = System.nanoTime();
     elapsed = end - start;
-    System.out.println("getHex4: " + elapsed);
+    System.out.println("getHex4: time " + elapsed);
     if (!(hex.equals(hex2) && hex2.equals(hex3) && hex3.equals(hex4)))
       throw new AssertionError("Not all hex conversions are equal");
   }
