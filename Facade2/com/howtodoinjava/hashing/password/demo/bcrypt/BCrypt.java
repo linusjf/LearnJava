634381a264d68563d32e2e883a3867c867137a66
fernal73 <linus.fernandes@gmail.com>
2019-05-29 12:40:13 +0530

    Removed BR tags

--- Facade2/com/howtodoinjava/hashing/password/demo/bcrypt/BCrypt.java
+++ Facade2/com/howtodoinjava/hashing/password/demo/bcrypt/BCrypt.java
@@ -29,25 +29,26 @@ import java.security.SecureRandom;
  * random salt, like this:</p>
  *
  * <p><code>
- * String pw_hash = BCrypt.hashpw(plain_password, BCrypt.gensalt()); <br />
+ * String pw_hash = BCrypt.hashpw(plain_password, BCrypt.gensalt()); 
+ *
  * </code></p>
  *
  * <p>To check whether a plaintext password matches one that has been hashed previously, use the
  * checkpw method:</p>
  *
  * <p><code>
- * if (BCrypt.checkpw(candidate_password, stored_hash))<br/>
- * &nbsp;&nbsp;&nbsp;&nbsp;System.out.println("It matches");<br/>
- * else<br/>
- * &nbsp;&nbsp;&nbsp;&nbsp;System.out.println("It does not match");<br/>
+ * if (BCrypt.checkpw(candidate_password, stored_hash))
+ * &nbsp;&nbsp;&nbsp;&nbsp;System.out.println("It matches");
+ * else
+ * &nbsp;&nbsp;&nbsp;&nbsp;System.out.println("It does not match");
  * </code></p>
  *
  * <p>The gensalt() method takes an optional parameter (log_rounds) that determines the
  * computational complexity of the hashing:</p>
  *
  * <p><code>
- * String strong_salt = BCrypt.gensalt(10)<br/>
- * String stronger_salt = BCrypt.gensalt(12)<br/>
+ * String strong_salt = BCrypt.gensalt(10)
+ * String stronger_salt = BCrypt.gensalt(12)
  * </code></p>
  *
  * <p>The amount of work increases exponentially (2**log_rounds), so each increment is twice as much
