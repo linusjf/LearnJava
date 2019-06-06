1ac670c3243dcc89e59e5aad40f9263a15d8d655
fernal73 <linus.fernandes@gmail.com>
2019-06-06 09:54:47 +0530

    Added Command pattern for Switch

---
+++ Switch/command/Command.java
@@ -0,0 +1,8 @@
+package command;
+
+import player.Player;
+
+public interface Command {
+
+   Player create();
+}
---
+++ Switch/command/Main.java
@@ -0,0 +1,19 @@
+package command;
+
+import player.Player;
+
+public enum Main {
+  ;
+
+  public static void main(String[] args) {
+
+    PlayerCreator playerCreator = new PlayerCreator();
+    Player tennisPlayer = playerCreator.createPlayer("TENNIS");
+
+    System.out.println(tennisPlayer);
+
+    CreatePlayerCommand createCommand = new CreatePlayerCommand();
+    Player snookerPlayer = createCommand.createPlayer("SNOOKER");
+    System.out.println(snookerPlayer);
+  }
+}
