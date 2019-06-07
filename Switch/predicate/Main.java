f36f04755779373fbeeb79048640e34b9592c1f3
fernal73 <linus.fernandes@gmail.com>
2019-06-07 18:12:15 +0530

    Updated build and test files

--- Switch/predicate/Main.java
+++ Switch/predicate/Main.java
@@ -6,5 +6,10 @@ public enum Main {
   public static void main(String[] args) {
     PlayerCreator playerCreator = new PlayerCreator();
     Player tennisPlayer = playerCreator.createPlayer("TENNIS", 5);
+    System.out.println(tennisPlayer);
+    Player footballPlayer = PlayerSupplier.supplyPlayer("FOOTBALL", 6);
+    System.out.println(footballPlayer);
+    Player snookerPlayer = PlayerTypes.supplyPlayer("SNOOKER", 10);
+		System.out.println(snookerPlayer);
   }
 }
