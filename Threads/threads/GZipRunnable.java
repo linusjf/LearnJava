a792681c4e556440c89c6b222e7531569cdf4026
fernal73 <linus.fernandes@gmail.com>
2019-06-13 07:16:51 +0530

    Added GZip example

---
+++ Threads/threads/GZipRunnable.java
@@ -0,0 +1,38 @@
+package threads;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class GZipRunnable implements Runnable {
+  private final File input;
+
+  public GZipRunnable(File input) {
+    this.input = input;
+  }
+
+  @Override
+  public void run() {
+    // don't compress an already compressed file
+    if (!input.getName().endsWith(".gz")) {
+      File output = new File(input.getParent(), input.getName() + ".gz");
+      if (!output.exists()) {
+        try (InputStream in = new BufferedInputStream(new FileInputStream(input));
+            OutputStream out =
+                new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(output))); ) {
+          int b;
+          while ((b = in.read()) != -1) out.write(b);
+          out.flush();
+        } catch (IOException ex) {
+          System.err.println(ex);
+        }
+      }
+    }
+  }
+}
