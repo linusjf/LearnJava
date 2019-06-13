8349e701e0e72f18a252e0bc155c5e1a066d37c9
fernal73 <linus.fernandes@gmail.com>
2019-06-13 21:50:20 +0530

    Added new file IPCharacteristics

---
+++ Networks/networking/IPCharacteristics.java
@@ -0,0 +1,62 @@
+package networking;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IPCharacteristics {
+  private static void handleWildcardAddress(InetAddress address) {
+    if (address.isAnyLocalAddress()) {
+      System.out.println(address + " is a wildcard address.");
+    }
+  }
+
+  private static void handleLoopbackAddress(InetAddress address) {
+    if (address.isLoopbackAddress()) {
+      System.out.println(address + " is loopback address.");
+    }
+  }
+
+  private static void handleLocalGlobalAddress(InetAddress address) {
+    if (address.isLinkLocalAddress()) {
+      System.out.println(address + " is a link-local address.");
+    } else if (address.isSiteLocalAddress()) {
+      System.out.println(address + " is a site-local address.");
+    } else {
+      System.out.println(address + " is a global address.");
+    }
+  }
+
+  private static void handleCastAddress(InetAddress address) {
+    if (address.isMulticastAddress()) {
+      if (address.isMCGlobal()) {
+        System.out.println(address + " is a global multicast address.");
+      } else if (address.isMCOrgLocal()) {
+        System.out.println(address + " is an organization wide multicast address.");
+      } else if (address.isMCSiteLocal()) {
+        System.out.println(address + " is a site wide multicast address.");
+      } else if (address.isMCLinkLocal()) {
+        System.out.println(address + " is a subnet wide multicast address.");
+      } else if (address.isMCNodeLocal()) {
+        System.out.println(address + " is an interface-local multicast address.");
+      } else {
+        System.out.println(address + " is an unknown multicast address type.");
+      }
+    } else {
+      System.out.println(address + " is a unicast address.");
+    }
+  }
+
+  public static void main(String[] args) {
+    for (String arg : args) {
+      try {
+        InetAddress address = InetAddress.getByName(arg);
+        handleWildcardAddress(address);
+        handleLoopbackAddress(address);
+        handleLocalGlobalAddress(address);
+        handleCastAddress(address);
+      } catch (UnknownHostException ex) {
+        System.err.println("Could not resolve " + arg);
+      }
+    }
+  }
+}
