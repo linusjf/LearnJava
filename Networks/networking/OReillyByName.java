a9645455f48ce19c3acb75e10fd103679e4762ec
fernal73 <linus.fernandes@gmail.com>
2019-06-13 15:49:08 +0530

    Added project Networks

---
+++ Networks/networking/OReillyByName.java
@@ -0,0 +1,15 @@
+package networking;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class OReillyByName {
+  public static void main(String[] args) {
+    try {
+      InetAddress[] addresses = InetAddress.getAllByName("www.oreilly.com");
+      for (InetAddress address : addresses) System.out.println(address);
+    } catch (UnknownHostException ex) {
+      System.err.println("Could not find www.oreilly.com" + ex.getMessage());
+    }
+  }
+}
