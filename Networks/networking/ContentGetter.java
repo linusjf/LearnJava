fe95489a53eb04ee6ae7401f7d46ebe64f73c148
fernal73 <linus.fernandes@gmail.com>
2019-06-14 22:31:59 +0530

    Added ContentGetter

---
+++ Networks/networking/ContentGetter.java
@@ -0,0 +1,24 @@
+package networking;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ContentGetter {
+  public static void main(String[] args) {
+    if (args.length > 0) {
+      // Open the URLs for reading
+      for (String arg : args) {
+        try {
+          URL u = new URL(arg);
+          Object o = u.getContent();
+          System.out.println("I got a " + o.getClass().getName());
+        } catch (MalformedURLException ex) {
+          System.err.println(args[0] + " is not a parseable URL");
+        } catch (IOException ex) {
+          System.err.println(ex);
+        }
+      }
+    }
+  }
+}
