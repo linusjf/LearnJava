69d224c269915310097dd3cca13cbe3728131947
fernal73 <linus.fernandes@gmail.com>
2019-06-15 11:04:53 +0530

    Added EncoderTest and QueryString

---
+++ Networks/networking/QueryString.java
@@ -0,0 +1,45 @@
+package networking;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class QueryString {
+  private StringBuilder query;
+
+  public QueryString() {
+    query = new StringBuilder();
+  }
+
+  public synchronized void add(String name, String value) {
+    query.append('&');
+    encode(name, value);
+  }
+
+  private synchronized void encode(String name, String value) {
+    try {
+      query.append(URLEncoder.encode(name, "UTF-8"));
+      query.append('=');
+      query.append(URLEncoder.encode(value, "UTF-8"));
+    } catch (UnsupportedEncodingException ex) {
+      throw new RuntimeException("Broken VM does not support UTF-8");
+    }
+  }
+
+  public synchronized String getQuery() {
+    return query.toString();
+  }
+
+  @Override
+  public String toString() {
+    return getQuery();
+  }
+
+  public static void main(String[] args) {
+    QueryString qs = new QueryString();
+    qs.add("hl", "en");
+    qs.add("as_q", "Java");
+    qs.add("as_epq", "I/O");
+    String url = "http://www.google.com/search?" + qs;
+    System.out.println(url);
+  }
+}
