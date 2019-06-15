c0b1c5899a778b805bf54ae17a6f1562d8d205e9
fernal73 <linus.fernandes@gmail.com>
2019-06-15 06:09:45 +0530

    Added URLSplitter. Ignore log files

---
+++ Networks/networking/URLSplitter.java
@@ -0,0 +1,32 @@
+package networking;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLSplitter {
+  public static void main(String[] args) {
+    for (String arg : args) {
+      try {
+        URL u = new URL(arg);
+        System.out.println("The URL is " + u);
+        System.out.println("The scheme is " + u.getProtocol());
+        System.out.println("The user info is " + u.getUserInfo());
+        String host = u.getHost();
+        if (host != null) {
+          int atSign = host.indexOf('@');
+          if (atSign != -1) host = host.substring(atSign + 1);
+          System.out.println("The host is " + host);
+        } else {
+          System.out.println("The host is null.");
+        }
+        System.out.println("The port is " + u.getPort());
+        System.out.println("The path is " + u.getPath());
+        System.out.println("The ref is " + u.getRef());
+        System.out.println("The query string is " + u.getQuery());
+      } catch (MalformedURLException ex) {
+        System.err.println(arg + " is not a URL I understand.");
+      }
+      System.out.println();
+    }
+  }
+}
