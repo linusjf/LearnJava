cd44c4d0b38462b4f78db9cb243784bbcaa9c4df
fernal73 <linus.fernandes@gmail.com>
2019-06-19 17:50:12 +0530

    Added CookiePolicy example

---
+++ Networks/networking/NoGovernmentCookies.java
@@ -0,0 +1,16 @@
+package networking;
+
+import java.net.CookiePolicy;
+import java.net.HttpCookie;
+import java.net.URI;
+
+public class NoGovernmentCookies implements CookiePolicy {
+  @Override
+  public boolean shouldAccept(URI uri, HttpCookie cookie) {
+    if (uri.getAuthority().toLowerCase().endsWith(".gov")
+        || cookie.getDomain().toLowerCase().endsWith(".gov")) {
+      return false;
+    }
+    return true;
+  }
+}
