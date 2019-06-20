076153e8ae340cd264711ad6fa575cdab6004805
fernal73 <linus.fernandes@gmail.com>
2019-06-20 10:47:40 +0530

    Added BinarySaver.java

---
+++ Networks/networking/BinarySaver.java
@@ -0,0 +1,56 @@
+package networking;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class BinarySaver {
+  public static void main(String[] args) {
+    for (String arg : args) {
+      try {
+        URL root = new URL(arg);
+        saveBinaryFile(root);
+      } catch (MalformedURLException ex) {
+        System.err.println(arg + " is not URL I understand.");
+      } catch (IOException ex) {
+        System.err.println(ex);
+      }
+    }
+  }
+
+  public static void saveBinaryFile(URL u) throws IOException {
+    URLConnection uc = u.openConnection();
+    String contentType = uc.getContentType();
+    int contentLength = uc.getContentLength();
+    if (contentType.startsWith("text/") || contentLength == -1) {
+      throw new IOException("This is not a binary file.");
+    }
+    try (InputStream raw = uc.getInputStream()) {
+      InputStream in = new BufferedInputStream(raw);
+      byte[] data = new byte[contentLength];
+      int offset = 0;
+      while (offset < contentLength) {
+        int bytesRead = in.read(data, offset, data.length - offset);
+        if (bytesRead == -1)
+          break;
+        offset += bytesRead;
+      }
+      if (offset != contentLength) {
+        throw new IOException("Only read " + offset + " bytes; Expected "
+                              + contentLength + " bytes");
+      }
+      String filename = u.getFile();
+      filename = filename.substring(filename.lastIndexOf('/') + 1);
+      try (OutputStream out = Files.newOutputStream(Paths.get(filename))) {
+        out.write(data);
+        out.flush();
+      }
+    }
+  }
+}
