a74a2ea426d24fa2729ae3ab5a273907aa0fd48c
fernal73 <linus.fernandes@gmail.com>
2019-06-21 09:25:41 +0530

    Fixed duplicate string literal pmd error

--- Networks/networking/EncoderTest.java
+++ Networks/networking/EncoderTest.java
@@ -4,32 +4,35 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 public final class EncoderTest {
+
+private static final String UTF8 = "UTF-8";
+
   private EncoderTest() {
     throw new IllegalStateException("Private constructor");
   }
 
   public static void main(String[] args) {
     try {
-      System.out.println(URLEncoder.encode("This string has spaces", "UTF-8"));
+      System.out.println(URLEncoder.encode("This string has spaces", UTF8));
       System.out.println(
-          URLEncoder.encode("This*string*has*asterisks", "UTF-8"));
+          URLEncoder.encode("This*string*has*asterisks", UTF8));
       System.out.println(
-          URLEncoder.encode("This%string%has%percent%signs", "UTF-8"));
-      System.out.println(URLEncoder.encode("This+string+has+pluses", "UTF-8"));
-      System.out.println(URLEncoder.encode("This/string/has/slashes", "UTF-8"));
+          URLEncoder.encode("This%string%has%percent%signs", UTF8));
+      System.out.println(URLEncoder.encode("This+string+has+pluses", UTF8));
+      System.out.println(URLEncoder.encode("This/string/has/slashes", UTF8));
       System.out.println(
-          URLEncoder.encode("This\"string\"has\"quote\"marks", "UTF-8"));
-      System.out.println(URLEncoder.encode("This:string:has:colons", "UTF-8"));
-      System.out.println(URLEncoder.encode("This~string~has~tildes", "UTF-8"));
+          URLEncoder.encode("This\"string\"has\"quote\"marks", UTF8));
+      System.out.println(URLEncoder.encode("This:string:has:colons", UTF8));
+      System.out.println(URLEncoder.encode("This~string~has~tildes", UTF8));
       System.out.println(
-          URLEncoder.encode("This(string)has(parentheses)", "UTF-8"));
-      System.out.println(URLEncoder.encode("This.string.has.periods", "UTF-8"));
+          URLEncoder.encode("This(string)has(parentheses)", UTF8));
+      System.out.println(URLEncoder.encode("This.string.has.periods", UTF8));
       System.out.println(
-          URLEncoder.encode("This=string=has=equals=signs", "UTF-8"));
+          URLEncoder.encode("This=string=has=equals=signs", UTF8));
       System.out.println(
-          URLEncoder.encode("This&string&has&ampersands", "UTF-8"));
+          URLEncoder.encode("This&string&has&ampersands", UTF8));
       System.out.println(
-          URLEncoder.encode("Thiséstringéhasé non-ASCII characters", "UTF-8"));
+          URLEncoder.encode("Thiséstringéhasé non-ASCII characters", UTF8));
     } catch (UnsupportedEncodingException ex) {
       throw new EncodingException("Broken VM does not support UTF-8", ex);
     }
