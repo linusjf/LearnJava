b31874868a6e6380ca81720ba100fb85403a7ed7
fernal73 <linus.fernandes@gmail.com>
2019-06-23 09:23:34 +0530

    Added more network examples. Added suppressions for Magic Numbers.Removed instantiation in loops rule.

---
+++ Networks/networking/URLPrinter.java
@@ -0,0 +1,17 @@
+package networking;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class URLPrinter {
+  public static void main(String[] args) {
+    try {
+      URL u = new URL("http://www.oreilly.com/");
+      URLConnection uc = u.openConnection();
+      System.out.println(uc.getURL());
+    } catch (IOException ex) {
+      System.err.println(ex);
+    }
+  }
+}
