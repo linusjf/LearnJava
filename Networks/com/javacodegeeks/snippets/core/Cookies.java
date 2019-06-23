f4793a70771b0850f61c66f502640d48e2919802
fernal73 <linus.fernandes@gmail.com>
2019-06-23 21:02:20 +0530

    Working on stripping cookies out to set in sunsequent requests

--- Networks/com/javacodegeeks/snippets/core/Cookies.java
+++ Networks/com/javacodegeeks/snippets/core/Cookies.java
@@ -2,63 +2,66 @@ package com.javacodegeeks.snippets.core;
 
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 public class Cookies {
   public static void main(String[] args) throws Exception {
-    URL url = new URL("http://www.google.com");
+    URL url = new URL("https://www.spamhaus.org");
     URLConnection conn = url.openConnection();
 
-    Map<String, List<String>> headerFields = conn.getHeaderFields();
+    Map<String, List<String>> headers = conn.getHeaderFields();
+    Map<String,List<String>> copyHeaders = new HashMap<>();
+    copyHeaders.putAll(headers);
+    copyHeaders.remove(null);
+    Map<String, List<String>> headersTree =
+        new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    headersTree.putAll(copyHeaders);
 
-    Set<String> headerFieldsSet = headerFields.keySet();
-    Iterator<String> hearerFieldsIter = headerFieldsSet.iterator();
+    List<String> headerFieldValue = headersTree.get("Set-Cookie");
+    System.out.println(headerFieldValue);
 
-    while (hearerFieldsIter.hasNext()) {
-      String headerFieldKey = hearerFieldsIter.next();
+    for (String headerValue : headerFieldValue) {
+      System.out.println("Cookie Found...");
+      String[] fields = headerValue.split(";\\s*");
+      String cookieValue = fields[0];
+      String expires = null;
+      String path = null;
+      String domain = null;
+      boolean secure = false;
 
-      if ("Set-Cookie".equalsIgnoreCase(headerFieldKey)) {
-        List<String> headerFieldValue = headerFields.get(headerFieldKey);
-
-        for (String headerValue : headerFieldValue) {
-          System.out.println("Cookie Found...");
-
-          String[] fields = headerValue.split(";\\s*");
-
-          String cookieValue = fields[0];
-          String expires = null;
-          String path = null;
-          String domain = null;
-          boolean secure = false;
-
-          // Parse each field
-          for (int j = 1; j < fields.length; j++) {
-            if ("secure".equalsIgnoreCase(fields[j])) {
-              secure = true;
-            } else if (fields[j].indexOf('=') > 0) {
-              String[] f = fields[j].split("=");
-              if ("expires".equalsIgnoreCase(f[0])) {
-                expires = f[1];
-              } else if ("domain".equalsIgnoreCase(f[0])) {
-                domain = f[1];
-              } else if ("path".equalsIgnoreCase(f[0])) {
-                path = f[1];
-              }
-            }
+      // Parse each field
+      for (int j = 1; j < fields.length; j++) {
+        if ("secure".equalsIgnoreCase(fields[j])) {
+          secure = true;
+          continue;
+        }
+        if (fields[j].indexOf('=') > 0) {
+          String[] f = fields[j].split("=");
+          if ("expires".equalsIgnoreCase(f[0])) {
+            expires = f[1];
+            continue;
           }
-
-          System.out.println("cookieValue:" + cookieValue);
-          System.out.println("expires:" + expires);
-          System.out.println("path:" + path);
-          System.out.println("domain:" + domain);
-          System.out.println("secure:" + secure);
-
-          System.out.println("*****************************************");
+          if ("domain".equalsIgnoreCase(f[0])) {
+            domain = f[1];
+            continue;
+          }
+          if ("path".equalsIgnoreCase(f[0])) {
+            path = f[1];
+            continue;
+          }
         }
       }
+
+      System.out.println("cookieValue:" + cookieValue);
+      System.out.println("expires:" + expires);
+      System.out.println("path:" + path);
+      System.out.println("domain:" + domain);
+      System.out.println("secure:" + secure);
+
+      System.out.println("*****************************************");
     }
   }
 }
