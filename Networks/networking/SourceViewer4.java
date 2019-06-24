d9307f46dde067023958d5fbf301cf20076c1749
fernal73 <linus.fernandes@gmail.com>
2019-06-24 20:24:45 +0530

    Added new example to read and print error stream

---
+++ Networks/networking/SourceViewer4.java
@@ -0,0 +1,38 @@
+package networking;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SourceViewer4 {
+  public static void main(String[] args) {
+    try {
+      URL u = new URL(args[0]);
+      HttpURLConnection uc = (HttpURLConnection)u.openConnection();
+      try (InputStream raw = uc.getInputStream()) {
+        printFromStream(raw);
+      } catch (IOException ex) {
+        printFromStream(uc.getErrorStream());
+      }
+    } catch (MalformedURLException ex) {
+      System.err.println(args[0] + " is not a parseable URL");
+    } catch (IOException ex) {
+      System.err.println(ex);
+    }
+  }
+
+  private static void printFromStream(InputStream raw) throws IOException {
+    try (InputStream buffer = new BufferedInputStream(raw)) {
+      Reader reader = new InputStreamReader(buffer);
+      int c;
+      while ((c = reader.read()) != -1) {
+        System.out.print((char)c);
+      }
+    }
+  }
+}
