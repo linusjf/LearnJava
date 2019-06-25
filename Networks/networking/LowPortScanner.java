656ebf710c59d9a2d874763931722ede7e878b46
fernal73 <linus.fernandes@gmail.com>
2019-06-25 10:27:37 +0530

    Added new example LowPortScanner

---
+++ Networks/networking/LowPortScanner.java
@@ -0,0 +1,25 @@
+package networking;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class LowPortScanner {
+  public static void main(String[] args) {
+    String[] hosts = args.length > 0 ? args : new String[] {"localhost"};
+    for (String host : hosts) {
+      for (int i = 1; i < 1024; i++) {
+        try {
+          Socket s = new Socket(host, i);
+          System.out.println("There is a server on port " + i + " of " + host);
+          s.close();
+        } catch (UnknownHostException ex) {
+          System.err.println(ex);
+          break;
+        } catch (IOException ex) {
+          // must not be a server on this port
+        }
+      }
+    }
+  }
+}
