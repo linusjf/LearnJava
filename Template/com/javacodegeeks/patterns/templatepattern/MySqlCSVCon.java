ef248d0868a3214087df232bfea26e14419e87bb
fernal73 <linus.fernandes@gmail.com>
2019-06-28 15:50:06 +0530

    Added project Template

---
+++ Template/com/javacodegeeks/patterns/templatepattern/MySqlCSVCon.java
@@ -0,0 +1,18 @@
+package com.javacodegeeks.patterns.templatepattern;
+
+public class MySqlCSVCon extends ConnectionTemplate {
+  @Override
+  public void setDBDriver() {
+    System.out.println("Setting MySQL DB drivers...");
+  }
+
+  @Override
+  public void setCredentials() {
+    System.out.println("Setting credentials for MySQL DB...");
+  }
+
+  @Override
+  public void setData() {
+    System.out.println("Setting up data from csv file....");
+  }
+}
---
+++ Template/com/javacodegeeks/patterns/templatepattern/TestTemplatePattern.java
@@ -0,0 +1,14 @@
+package com.javacodegeeks.patterns.templatepattern;
+
+public enum TestTemplatePattern {
+  ;
+
+  public static void main(String[] args) {
+    System.out.println("For MYSQL....");
+    ConnectionTemplate template = new MySqlCSVCon();
+    template.run();
+    System.out.println("For Oracle...");
+    template = new OracleTxtCon();
+    template.run();
+  }
+}
