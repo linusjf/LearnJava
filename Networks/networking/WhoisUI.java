ecd26bef7e78bbd945b26d6a7d482d51e8d4640c
fernal73 <linus.fernandes@gmail.com>
2019-06-30 09:23:06 +0530

    More modifications

--- Networks/networking/WhoisUI.java
+++ Networks/networking/WhoisUI.java
@@ -1,21 +1,41 @@
 package networking;
 
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class WhoisUI {
 
   private Whois server;
+  private String searchFor;
+  private String searchAt;
+  private boolean exactMatch;
+  private String whois;
+  private String searchIn;
 
   public WhoisUI(Whois server) {
     this.server = server;
   }
 
+  public void getUserInput(String... args) {
+    Scanner scanner = new Scanner(System.in);
+    getWhoisInput(scanner);
+  }
+
+  private void getWhoisInput(Scanner scanner) {
+    while (whois == null || whois.isEmpty()) {
+      System.out.println("Enter entity name for whois record: ");
+      if (scanner.hasNextLine())
+        whois = scanner.nextLine().trim();
+    }
+  }
+
   public static void main(String... args) {
-try {
-    Whois server = new Whois();
- WhoisUI ui = new WhoisUI(server);
-} catch (UnknownHostException uhe) {
-  System.err.println(uhe.getMessage());
-}
+    try {
+      Whois server = new Whois();
+      WhoisUI ui = new WhoisUI(server);
+      ui.getUserInput(args);
+    } catch (UnknownHostException uhe) {
+      System.err.println(uhe.getMessage());
+    }
   }
 }
