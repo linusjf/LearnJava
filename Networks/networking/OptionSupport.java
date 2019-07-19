6ff5880538c142ad2334944363abcf86994b65e9
fernal73 <linus.fernandes@gmail.com>
2019-07-19 14:29:32 +0530

    Added more examples

---
+++ Networks/networking/OptionSupport.java
@@ -0,0 +1,40 @@
+package networking;
+
+import java.io.IOException;
+import java.net.SocketOption;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.NetworkChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public enum OptionSupport {
+  ;
+
+  public static void main(String[] args) {
+    try {
+    printOptions(SocketChannel.open());
+    printOptions(ServerSocketChannel.open());
+    printOptions(AsynchronousSocketChannel.open());
+    printOptions(AsynchronousServerSocketChannel.open());
+    printOptions(DatagramChannel.open());
+  } catch (IOException ioe) {
+    System.err.println(ioe);
+  }
+  }
+
+  private static void printOptions(NetworkChannel channel) throws IOException {
+    System.out.println(channel.getClass().getSimpleName() + " supports:");
+    for (SocketOption<?> option: channel.supportedOptions()) {
+      try {
+      System.out.println(option.name() + ": " + channel.getOption(option));
+      } catch (AssertionError ae) {
+        System.err.println("Option not found for : " +
+            option.name());
+      }
+    }
+    System.out.println();
+    channel.close();
+  }
+}
---
+++ Regex/regex/SimpleRegexTest.java
@@ -0,0 +1,21 @@
+package regex;
+
+public enum SimpleRegexTest {
+  ;
+
+  public static void main(String[] args) {
+    String sampleText = "this is the 1st test string";
+    String sampleRegex = "\\d+\\w+";
+    java.util.regex.Pattern p = java.util.regex.Pattern.compile(sampleRegex);
+    java.util.regex.Matcher m = p.matcher(sampleText);
+    if (m.find()) {
+      String matchedText = m.group();
+      int matchedFrom = m.start();
+      int matchedTo = m.end();
+      System.out.println("matched [" + matchedText + "] from " + matchedFrom
+                         + " to " + matchedTo + ".");
+    } else {
+      System.out.println("didn’t match");
+    }
+  }
+}
