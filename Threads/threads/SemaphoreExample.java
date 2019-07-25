08c89b216e4cf7ffab61856541fc61e71336c391
fernal73 <linus.fernandes@gmail.com>
2019-07-25 14:28:43 +0530

    Added semaphore example

---
+++ Threads/threads/SemaphoreExample.java
@@ -0,0 +1,59 @@
+package threads;
+
+import java.util.concurrent.Semaphore;
+
+public enum SemaphoreExample {
+  ;
+
+  public static void main(String[] args) {
+    PrintQueue printQueue = new PrintQueue();
+    Thread[] thread = new Thread[10];
+    for (int i = 0; i < 10; i++)
+      thread[i] = new Thread(new Job(printQueue), "Thread" + i);
+
+    for (int i = 0; i < 10; i++)
+      thread[i].start();
+  }
+
+  static class PrintQueue {
+    private final Semaphore semaphore;
+
+    PrintQueue() {
+      semaphore = new Semaphore(1);
+    }
+
+    public void printJob(Object document) {
+      try {
+        semaphore.acquire();
+        long duration = (long)(Math.random() * 10);
+        System.out.printf("%s: PrintQueue: Printing a Job during %d seconds\n",
+                          Thread.currentThread().getName(),
+                          duration);
+        Thread.sleep(duration);
+      } catch (InterruptedException e) {
+        System.err.println(e);
+      } finally {
+        semaphore.release();
+      }
+    }
+  }
+
+  @SuppressWarnings("PMD.ShortClassName")
+  static class Job implements Runnable {
+    private PrintQueue printQueue;
+
+    Job(PrintQueue printQueue) {
+      this.printQueue = printQueue;
+    }
+
+    @Override
+    public void run() {
+      System.out.printf("%s: Going to print a job\n",
+                        Thread.currentThread().getName());
+      printQueue.printJob(new Object());
+      System.out.printf(
+          "%s: The document has been printed\n",
+          Thread.currentThread().getName());
+    }
+  }
+}
