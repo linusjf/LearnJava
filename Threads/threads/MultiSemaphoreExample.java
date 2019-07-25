0d4285a14cf1c435097f873c924b7b73e3a19e81
fernal73 <linus.fernandes@gmail.com>
2019-07-25 16:45:00 +0530

    Multi semaphore example

--- Threads/threads/MultiSemaphoreExample.java
+++ Threads/threads/MultiSemaphoreExample.java
@@ -2,8 +2,10 @@ package threads;
 
 import java.util.Random;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public enum SemaphoreExample {
+public enum MultiSemaphoreExample {
   ;
 
   public static void main(String[] args) {
@@ -18,26 +20,53 @@ public enum SemaphoreExample {
 
   static class PrintQueue {
     private final Semaphore semaphore;
+    private boolean[] freePrinters;
+    private Lock lockPrinters;
 
     PrintQueue() {
-      semaphore = new Semaphore(1, new Random().nextBoolean());
+      freePrinters = new boolean[] {true, true, true};
+      Random random = new Random();
+      boolean fair = random.nextBoolean();
+      lockPrinters = new ReentrantLock(fair);
+      semaphore = new Semaphore(3, random.nextBoolean());
       System.out.println("Semaphore fair: " + semaphore.isFair());
+      System.out.println("ReentrantLock fair: " + fair);
     }
 
     public void printJob(Object document) {
       try {
         semaphore.acquire();
+        int assignedPrinter = getPrinter();
         long duration = (long)(Math.random() * 10);
         System.out.printf(
-            "%s: PrintQueue: Printing a Job utilizing %d seconds\n",
+            "%s: PrintQueue: Printing a Job at %d utilizing %d seconds\n",
             Thread.currentThread().getName(),
+            assignedPrinter,
             duration);
         Thread.sleep(duration);
+        freePrinters[assignedPrinter] = true;
       } catch (InterruptedException e) {
         System.err.println(e);
       } finally {
         semaphore.release();
       }
+    }
+
+    private int getPrinter() {
+      int ret = -1;
+      try {
+        lockPrinters.lock();
+        for (int i = 0; i < freePrinters.length; i++) {
+          if (freePrinters[i]) {
+            ret = i;
+            freePrinters[i] = false;
+            break;
+          }
+        }
+      } finally {
+        lockPrinters.unlock();
+      }
+      return ret;
     }
   }
 
