c2f7210422ae5197bb63d104de480853c870c504
fernal73 <linus.fernandes@gmail.com>
2019-07-27 14:35:20 +0530

    Added more Executor examples

--- Threads/threads/DelayedExecutor.java
+++ Threads/threads/DelayedExecutor.java
@@ -1,10 +1,10 @@
 package threads;
 
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
 import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public enum DelayedExecutor {
   ;
@@ -15,7 +15,7 @@ public enum DelayedExecutor {
     System.out.printf("Main: Starting at: %s\n", new Date());
     for (int i = 0; i < 5; i++) {
       Task task = new Task("Task " + i);
-      executor.schedule(task, (i + 1)*(i + 1), TimeUnit.SECONDS);
+      executor.schedule(task, (i + 1) * (i + 1), TimeUnit.SECONDS);
     }
     executor.shutdown();
     try {
@@ -29,11 +29,11 @@ public enum DelayedExecutor {
   @SuppressWarnings("PMD.ShortClassName")
   static class Task implements Callable<String> {
     private String name;
-    
+
     Task(String name) {
       this.name = name;
     }
-  
+
     @Override
     public String call() throws Exception {
       System.out.printf("%s: Starting at : %s\n", name, new Date());
