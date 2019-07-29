fef8d927b7b46666929f0d32c6434e8d4e4087bf
fernal73 <linus.fernandes@gmail.com>
2019-07-29 16:51:21 +0530

    Added method to class alternateMain

--- Collections/collections/ConcurrentLinkedDequeDemo.java
+++ Collections/collections/ConcurrentLinkedDequeDemo.java
@@ -1,5 +1,6 @@
 package collections;
 
+import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public enum ConcurrentLinkedDequeDemo {
@@ -38,6 +39,47 @@ public enum ConcurrentLinkedDequeDemo {
       }
     }
     System.out.printf("Main: Size of the List: %d\n", list.size());
+    alternateMain();
+  }
+
+  private static void alternateMain() {
+
+    Random random = new Random();
+    ConcurrentLinkedDeque<String> list = new ConcurrentLinkedDeque<>();
+    Thread[] threads = new Thread[100];
+    Thread[] threads2 = new Thread[100];
+    for (int i = 0; i < threads.length; i++) {
+      AddTask task = new AddTask(list);
+      threads[i] = new Thread(task);
+      threads[i].setPriority(random.nextInt(10) + 1);
+      threads[i].start();
+      PollTask task1 = new PollTask(list);
+      threads2[i] = new Thread(task1);
+      threads2[i].setPriority(random.nextInt(10) + 1);
+      threads2[i].start();
+    }
+    System.out.printf("Alternate Main: %d AddTask threads have been launched\n",
+                      threads.length);
+    System.out.printf(
+        "Alternate Main: %d PollTask threads have been launched simultaneously\n",
+        threads2.length);
+    System.out.printf("Alternate Main: Size of the List: %d\n", list.size());
+
+    for (Thread thread: threads) {
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        System.err.println(e);
+      }
+    }
+    for (Thread thread: threads2) {
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        System.err.println(e);
+      }
+    }
+    System.out.printf("Alternate Main: Size of the List: %d\n", list.size());
   }
 
   static class AddTask implements Runnable {
