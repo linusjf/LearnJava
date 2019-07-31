03c4e46af1f53d6102c4f030e1e1853a6915ce78
fernal73 <linus.fernandes@gmail.com>
2019-07-31 07:32:22 +0530

    TaskLocalRandom

---
+++ Collections/collections/TaskLocalRandom.java
@@ -0,0 +1,30 @@
+package collections;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TaskLocalRandom implements Runnable {
+  public TaskLocalRandom() {
+    ThreadLocalRandom.current();
+  }
+
+  @Override
+  public void run() {
+    String name = Thread.currentThread().getName();
+    for (int i = 0; i < 10; i++) {
+      System.out.printf(
+          "%s with priority %d: %d \n", name, Thread.currentThread().getPriority(),
+          ThreadLocalRandom.current().nextInt(10));
+    }
+  }
+
+  public static void main(String[] args) {
+    Thread[] threads = new Thread[3];
+    for (int i = 0; i < 3; i++) {
+      TaskLocalRandom task = new TaskLocalRandom();
+      threads[i] = new Thread(task);
+      threads[i].setPriority(ThreadLocalRandom.current().nextInt(10)+1);
+    }
+    for (Thread thread: threads) 
+    thread.start();
+  }
+}
