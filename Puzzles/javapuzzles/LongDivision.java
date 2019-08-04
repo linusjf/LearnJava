705ceedbf61d1acfd0344467b418e1566180274f
fernal73 <linus.fernandes@gmail.com>
2019-08-04 07:48:34 +0530

    LongDivision

---
+++ Puzzles/javapuzzles/LongDivision.java
@@ -0,0 +1,15 @@
+package javapuzzles;
+
+public enum LongDivision {
+  ;
+  public static void main(String[] args) {
+
+    final long MICROS_PER_DAY = 24 * 60 * 60 * 1000 * 1000;
+    final long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
+    System.out.println(MICROS_PER_DAY / MILLIS_PER_DAY);
+
+    final long MICROS_PER_DAY_LONG = 24L * 60 * 60 * 1000 * 1000;
+    final long MILLIS_PER_DAY_LONG = 24L * 60 * 60 * 1000;
+    System.out.println(MICROS_PER_DAY_LONG / MILLIS_PER_DAY_LONG);
+  }
+}
