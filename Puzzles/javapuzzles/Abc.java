627722933ab15700f3faeabe703288d8c582ecc7
fernal73 <linus.fernandes@gmail.com>
2019-08-04 22:03:21 +0530

    Two more puzzles

---
+++ Puzzles/javapuzzles/Abc.java
@@ -0,0 +1,12 @@
+package javapuzzles;
+
+@SuppressWarnings("PMD.ShortClassName")
+public enum Abc {
+  ;
+
+  public static void main(String[] args) {
+    String letters = "ABC";
+    char[] numbers = {'1', '2', '3'};
+    System.out.println(letters + " easy as " + numbers);
+  }
+}
---
+++ Puzzles/javapuzzles/LastLaugh.java
@@ -0,0 +1,11 @@
+package javapuzzles;
+
+public enum LastLaugh {
+  ;
+
+  public static void main(String... args) {
+    System.out.print("H"
+                     + "a");
+    System.out.print('H' + 'a');
+  }
+}
