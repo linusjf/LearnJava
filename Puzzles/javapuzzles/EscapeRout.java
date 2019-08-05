0652080ff840a60dff02fd6a453fa0a53da3ae2c
fernal73 <linus.fernandes@gmail.com>
2019-08-05 11:26:14 +0530

    Two more examples

---
+++ Puzzles/javapuzzles/EscapeRout.java
@@ -0,0 +1,15 @@
+package javapuzzles;
+
+public enum EscapeRout {
+
+  ;
+  @SuppressWarnings({"checkstyle:avoidescapedunicodecharacters",
+                     "checkstyle:illegaltokentext"})
+  public static void
+  main(String[] args) {
+    // \u0022 is the Unicode escape for double quote "
+    System.out.println("a\u0022.length() + \u0022b".length());
+    System.out.println("a".length() + "b".length());
+    System.out.println("a\".length() + \"b".length());
+  }
+}
