924008add4876fedfac6688df3fbe5e3400a3e99
fernal73 <linus.fernandes@gmail.com>
2019-08-05 08:20:48 +0530

    AnimalFarm

---
+++ Puzzles/javapuzzles/AnimalFarm.java
@@ -0,0 +1,17 @@
+package javapuzzles;
+
+@SuppressWarnings("PMD")
+public enum AnimalFarm {
+  ;
+
+  public static void main(String[] args) {
+    final String pig = "length: 10";
+    final String cat = "length: 10";
+    final String dog = "length: " + pig.length();
+    System.out.println("Animals are equal: " + pig == dog);
+    System.out.println(("Animals are equal: " + pig) == dog);
+    System.out.println("Animals are equal: " + (pig == dog));
+    System.out.println("Animals are equal: " + pig.equals(dog));
+    System.out.println("Animals are equal: " + (cat == pig));
+  }
+}
