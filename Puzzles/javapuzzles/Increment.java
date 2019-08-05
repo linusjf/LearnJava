4a35037709a3530a1049d79afaa21193f567362a
fernal73 <linus.fernandes@gmail.com>
2019-08-05 20:14:36 +0530

    Added two more examples

---
+++ Puzzles/javapuzzles/Increment.java
@@ -0,0 +1,23 @@
+package javapuzzles;
+
+public enum Increment {
+  ;
+  public static void main(String... args) {
+    int j = 0;
+    for (int i = 0; i < 100; i++)
+      j = j++;
+    System.out.println(j);
+    j = 0;
+    for (int i = 0; i < 100; i++)
+      j = j--;
+    System.out.println(j);
+    j = 0;
+    for (int i = 0; i < 100; i++)
+      j = ++j;
+    System.out.println(j);
+    j = 0;
+    for (int i = 0; i < 100; i++)
+      j = --j;
+    System.out.println(j);
+  }
+}
