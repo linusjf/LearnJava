170a557442e4afd40f9d547d4bd809d1afd353bd
fernal73 <linus.fernandes@gmail.com>
2019-08-06 16:39:50 +0530

    Reluctant

---
+++ Puzzles/javapuzzles/Reluctant.java
@@ -0,0 +1,21 @@
+package javapuzzles;
+
+@SuppressWarnings("PMD")
+public class Reluctant {
+  private Reluctant internalInstance = new Reluctant();
+  
+  public Reluctant() throws Exception {
+    throw new Exception("I’m not coming out");
+  }
+ 
+  public static void main(String[] args) {
+    try {
+      Reluctant b = new Reluctant();
+      System.out.println("Surprise!");
+    } catch (Exception | Error ex) {
+      System.err.println("StackOverflowError caught: " +
+          ex.getMessage());
+      System.out.println("I told you so");
+    }
+  }
+}
