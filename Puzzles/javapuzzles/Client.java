b876ac0ddafe86b1ffc208f77b9876dbf11a5cf4
fernal73 <linus.fernandes@gmail.com>
2019-08-07 19:48:00 +0530

    Added Client and Null

---
+++ Puzzles/javapuzzles/Client.java
@@ -0,0 +1,35 @@
+package javapuzzles;
+
+public enum Client {
+  ;
+
+  public static void main(String[] args) {
+    System.out.println(Cache.getSum());
+  }
+}
+
+@SuppressWarnings({"checkstyle:onetoplevelclass", "PMD"})
+enum Cache {
+  ;
+
+  static {
+    initializeIfNecessary();
+  }
+
+  private static int sum;
+
+  public static int getSum() {
+    initializeIfNecessary();
+    return sum;
+  }
+
+  private static boolean initialized = false;
+
+  private static synchronized void initializeIfNecessary() {
+    if (!initialized) {
+      for (int i = 0; i < 100; i++)
+        sum += i;
+      initialized = true;
+    }
+  }
+}
