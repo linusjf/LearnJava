db774e03e35b494bbd40a11cc58206a4caf004fe
fernal73 <linus.fernandes@gmail.com>
2019-08-07 15:05:55 +0530

    Added Confusing.java

---
+++ Puzzles/javapuzzles/Confusing.java
@@ -0,0 +1,15 @@
+package javapuzzles;
+
+public final class Confusing {
+  private Confusing(Object o) {
+    System.out.println("Object " + o);
+  }
+
+  private Confusing(double... array) {
+    System.out.println("double array " + array);
+  }
+
+  public static void main(String[] args) {
+    new Confusing(null);
+  }
+}
