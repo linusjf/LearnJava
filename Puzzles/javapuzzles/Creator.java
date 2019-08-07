123f6386c4bd07467e618ec35a3bca46e2c1ed18
fernal73 <linus.fernandes@gmail.com>
2019-08-07 21:09:37 +0530

    Added Creator

---
+++ Puzzles/javapuzzles/Creator.java
@@ -0,0 +1,28 @@
+package javapuzzles;
+
+public enum Creator {
+  ;
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; i++)
+      //      Creature creature = new Creature();
+      new Creature();
+    System.out.println(Creature.getNumCreated());
+  }
+}
+
+@SuppressWarnings({"checkstyle:onetoplevelclass","PMD"})
+class Creature {
+  private static long numCreated;
+
+  Creature() {
+    synchronized (Creature.class) {
+      numCreated++;
+    }
+  }
+
+  public static long getNumCreated() {
+    synchronized (Creature.class) {
+      return numCreated;
+    }
+  }
+}
