62b11b3a29f433ede2f8f9f24477a69b1aee1862
fernal73 <linus.fernandes@gmail.com>
2019-08-08 10:47:49 +0530

    Added NameGame

---
+++ Puzzles/javapuzzles/NameGame.java
@@ -0,0 +1,24 @@
+package javapuzzles;
+
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+public enum NameGame {
+  ;
+  public static void main(String... args) {
+    Map<String, String> m = new IdentityHashMap<>();
+    m.put("Mickey", "Mouse");
+    m.put("Mickey", "Mantle");
+    System.out.println(m.size());
+    altMain(args);
+  }
+
+  public static void altMain(String... args) {
+    Map<String, String> m = new IdentityHashMap<>();
+    m.put("Mickey", "Mouse");
+    m.put(new String("Mic"
+                     + "key"),
+          "Mantle");
+    System.out.println(m.size());
+  }
+}
