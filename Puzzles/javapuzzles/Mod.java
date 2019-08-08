71ca082a6d2ea70f00ea2aa6b07665690e9a0401
fernal73 <linus.fernandes@gmail.com>
2019-08-08 15:58:30 +0530

    Mod.java

---
+++ Puzzles/javapuzzles/Mod.java
@@ -0,0 +1,40 @@
+package javapuzzles;
+
+@SuppressWarnings("PMD.ShortClassName")
+public enum Mod {
+  ;
+  private static final int MODULUS = 3;
+
+  public static void main(String[] args) {
+    int[] histogram = new int[MODULUS];
+    // Iterate over all ints (Idiom from Puzzle 26)
+    int i = Integer.MIN_VALUE;
+    try {
+      do {
+        histogram[Math.abs(i) % MODULUS]++;
+      } while (i++ != Integer.MAX_VALUE);
+      for (int j = 0; j < MODULUS; j++)
+        System.out.print(histogram[j] + " ");
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.err.println(e);
+    }
+    altMain(args);
+  }
+
+  private static int mod(int i, int modulus) {
+    int result = i % modulus;
+    return result < 0 ? result + modulus : result;
+  }
+
+  public static void altMain(String... args) {
+    final int MODULUS = 3;
+    int[] histogram = new int[MODULUS];
+    // Iterate over all ints (Idiom from Puzzle 26)
+    int i = Integer.MIN_VALUE;
+    do {
+      histogram[mod(i, MODULUS)]++;
+    } while (i++ != Integer.MAX_VALUE);
+    for (int j = 0; j < MODULUS; j++)
+      System.out.print(histogram[j] + " ");
+  }
+}
