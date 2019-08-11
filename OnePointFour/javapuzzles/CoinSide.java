c9c2a154412f9242923be4d30ba214cc58ad9ad6
fernal73 <linus.fernandes@gmail.com>
2019-08-11 14:00:17 +0530

    Added project OnePointFour for CoinSide puzzle

---
+++ OnePointFour/javapuzzles/CoinSide.java
@@ -0,0 +1,39 @@
+package javapuzzles;
+
+import java.util.Random;
+
+public abstract class CoinSide {
+  private static Random rnd = new Random();
+  
+  public static CoinSide flip() {
+    return rnd.nextBoolean() ? (CoinSide)Heads.INSTANCE : Tails.INSTANCE;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(flip());
+  }
+}
+
+final class Heads extends CoinSide {
+  public static final Heads INSTANCE = new Heads();
+  
+  private Heads() {
+    super();
+  }
+  
+  public String toString() {
+    return "heads";
+  }
+}
+
+final class Tails extends CoinSide {
+  public static final Tails INSTANCE = new Tails();
+  
+  private Tails() {
+    super();
+  }
+
+  public String toString() {
+    return "tails";
+  }
+}
