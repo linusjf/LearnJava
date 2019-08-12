fbab3e1140cf8c3064a21fb6efd46e7cad10f01e
fernal73 <linus.fernandes@gmail.com>
2019-08-12 10:52:54 +0530

    added countdownlatch example. fixed pmd errors. added entries in build files

---
+++ Concurrency/threads/CountDownLatchExample.java
@@ -0,0 +1,39 @@
+package threads;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchExample implements Runnable {
+  private static final int NUMBER_OF_THREADS = 5;
+  private static final CountDownLatch LATCH =
+      new CountDownLatch(NUMBER_OF_THREADS);
+  private static Random random = new Random(System.currentTimeMillis());
+
+  public static void main(String[] args) {
+    ExecutorService executorService =
+        Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    for (int i = 0; i < NUMBER_OF_THREADS; i++)
+      executorService.execute(new CountDownLatchExample());
+    executorService.shutdown();
+  }
+
+  @Override
+  public void run() {
+    try {
+      int randomSleepTime = random.nextInt(20_000);
+      System.out.println("[" + Thread.currentThread().getName()
+                         + "]-Sleeping for " + randomSleepTime);
+      Thread.sleep(randomSleepTime);
+      LATCH.countDown();
+      System.out.println("[" + Thread.currentThread().getName()
+                         + "]-Waiting for latch.");
+      LATCH.await();
+      System.out.println("[" + Thread.currentThread().getName()
+                         + "]-Finished.");
+    } catch (InterruptedException e) {
+      System.err.println(e);
+    }
+  }
+}
--- Concurrency/threads/SemaphoreArchetype.java
+++ Concurrency/threads/SemaphoreArchetype.java
@@ -9,6 +9,7 @@ public class SemaphoreArchetype implements Runnable {
   private static final Semaphore SEMAPHORE = new Semaphore(3, true);
   private static final AtomicInteger COUNTER = new AtomicInteger();
   private static final long END_MILLIS = System.currentTimeMillis() + 1000;
+  private static final int MAX_THREADS = 3;
 
   public static void main(String[] args) {
     ExecutorService executorService = Executors.newFixedThreadPool(5);
@@ -17,20 +18,21 @@ public class SemaphoreArchetype implements Runnable {
     executorService.shutdown();
   }
 
+  @Override
   public void run() {
     while (System.currentTimeMillis() < END_MILLIS) {
       try {
         SEMAPHORE.acquire();
       } catch (InterruptedException e) {
         System.out.println("[" + Thread.currentThread().getName()
-                           + "]←-Interrupted in acquire().");
+                           + "]-Interrupted in acquire().");
       }
       int counterValue = COUNTER.incrementAndGet();
       System.out.println("[" + Thread.currentThread().getName()
-                         + "]←-SEMAPHORE acquired: " + counterValue);
-      if (counterValue > 3) {
+                         + "]-SEMAPHORE acquired: " + counterValue);
+      if (counterValue > MAX_THREADS) {
         throw new IllegalStateException(
-            "More than three threads←-acquired the lock.");
+            "More than three threads -acquired the lock.");
       }
       COUNTER.decrementAndGet();
       SEMAPHORE.release();
