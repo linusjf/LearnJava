821bd3e8e4379e6e37514ca64da0cb1166bac001
fernal73 <linus.fernandes@gmail.com>
2019-08-13 08:44:34 +0530

    CopyDog example

---
+++ Puzzles/javapuzzles/CopyDog.java
@@ -0,0 +1,30 @@
+package javapuzzles;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public enum CopyDog {
+  // Not to be confused with copycat
+  ;
+
+  public static void main(String[] args) {
+    Dog newDog = (Dog)deepCopy(Dog.INSTANCE);
+    System.out.println(newDog == Dog.INSTANCE);
+    System.out.println(newDog);
+  }
+
+  // This method is very slow and generally a bad idea!
+  public static Object deepCopy(Object obj) {
+    try {
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      new ObjectOutputStream(bos).writeObject(obj);
+      ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray());
+      return new ObjectInputStream(bin).readObject();
+    } catch (ClassNotFoundException | IOException e) {
+      throw new IllegalArgumentException(e);
+    }
+  }
+}
---
+++ Puzzles/javapuzzles/Dog.java
@@ -0,0 +1,18 @@
+package javapuzzles;
+
+@SuppressWarnings("PMD")
+public final class Dog extends Exception {
+  public static final Dog INSTANCE = new Dog();
+
+  private Dog() {
+  }
+
+  public String toString() {
+    return "Woof";
+  }
+
+  private Object readResolve() {
+    // Accept no substitutes!
+    return INSTANCE;
+  }
+}
