944458d51617a28ee609815ac6b895ab9d175df6
fernal73 <linus.fernandes@gmail.com>
2019-08-13 19:30:04 +0530

    Added Pair example

---
+++ Puzzles/javapuzzles/Pair.java
@@ -0,0 +1,33 @@
+package javapuzzles;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Pair<T> {
+  private final T first;
+  private final T second;
+
+  public Pair(T first, T second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  public T first() {
+    return first;
+  }
+
+  public T second() {
+    return second;
+  }
+
+  public List<String> stringList() {
+    return Arrays.asList(String.valueOf(first), String.valueOf(second));
+  }
+
+  public static void main(String[] args) {
+    Pair<Object> p = new Pair<Object>(23, "skidoo");
+    System.out.println(p.first() + " " + p.second());
+    for (String s: p.stringList())
+      System.out.print(s + " ");
+  }
+}
