87b409541461e7b1f53b3df79fc43f24e42ffbe2
fernal73 <linus.fernandes@gmail.com>
2019-08-13 12:40:15 +0530

    Added SelfInterruption

---
+++ Puzzles/javapuzzles/SelfInterruption.java
@@ -0,0 +1,23 @@
+package javapuzzles;
+
+public enum SelfInterruption {
+  ;
+  public static void main(String[] args) {
+    Thread.currentThread().interrupt();
+    if (Thread.interrupted())
+      System.out.println("Interrupted: " + Thread.interrupted());
+    else
+      System.out.println("Not interrupted: " + Thread.interrupted());
+    altMain(args);
+  }
+
+  public static void altMain(String... args) {
+    Thread.currentThread().interrupt();
+    if (Thread.currentThread().isInterrupted())
+      System.out.println("Interrupted: "
+                         + Thread.currentThread().isInterrupted());
+    else
+      System.out.println("Not interrupted: "
+                         + Thread.currentThread().isInterrupted());
+  }
+}
