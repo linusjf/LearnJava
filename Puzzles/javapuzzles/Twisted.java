194ba393734df70cfe6b485cf16f09ffb62f74f2
fernal73 <linus.fernandes@gmail.com>
2019-08-14 09:22:03 +0530

    Twisted

---
+++ Puzzles/javapuzzles/Twisted.java
@@ -0,0 +1,39 @@
+package javapuzzles;
+
+@SuppressWarnings("PMD.AvoidFieldNameMatchingMethodName")
+public class Twisted {
+  private final String name;
+
+  Twisted(String name) {
+    this.name = name;
+  }
+  
+  private String name() {
+    return name;
+  }
+  
+  String getName() {
+    return name;
+  }
+  
+  private void reproduce() {
+    new Twisted("reproduce") {
+      void printName() {
+        System.out.println(name());
+      }
+    }.printName();
+  }
+  
+  private void reiterate() {
+    new Twisted("reiterate") {
+      void printName() {
+        System.out.println(getName());
+      }
+    }.printName();
+  }
+
+  public static void main(String[] args) {
+    new Twisted("main").reproduce();
+    new Twisted("main").reiterate();
+  }
+}
