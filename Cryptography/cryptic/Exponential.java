6315c89b66ad95ca8f0f8039b6777568ec520733
fernal73 <linus.fernandes@gmail.com>
2019-08-19 18:02:33 +0530

    Fixed checkstyle and pmd errors

--- Cryptography/cryptic/Exponential.java
+++ Cryptography/cryptic/Exponential.java
@@ -4,20 +4,22 @@ import java.util.Random;
 
 public enum Exponential {
   ;
+  private static final char BINARY_ONE = '1';
 
   static int fastExp1(int base, int exponent) {
     String binString = Integer.toBinaryString(exponent);
     char[] binary = binString.toCharArray();
-    return exp(base, binary, binary.length -1);
+    return exp(base, binary, binary.length - 1);
   }
 
   static int exp(int x, char[] binary, int k) {
     // Y = Y[k] Y[k-1] ... Y[1] Y[0] (in binary)
-    int y = 0, z = 1;
+    int y = 0;
+    int z = 1;
     for (int i = 0; i <= k; i++) {
       y = 2 * y;
       z = z * z;
-      if (binary[i] == '1') {
+      if (binary[i] == BINARY_ONE) {
         y++;
         z = z * x;
       }
@@ -25,8 +27,10 @@ public enum Exponential {
     return z;
   }
 
-  static int expSecond(int X, int Y) {
-    int x = X, y = Y, z = 1;
+  static int expSecond(int base, int exponent) {
+    int x = base;
+    int y = exponent;
+    int z = 1;
     while (y > 0) {
       while (y % 2 == 0) {
         x = x * x;
@@ -39,31 +43,31 @@ public enum Exponential {
   }
 
   public static void main(String... args) {
-  Random random = new Random();
-  int base = random.nextInt(5);
-  int exponent = random.nextInt(20);
-  
-  System.out.printf("Base: %d Exponent %d%n",base,exponent);
+    Random random = new Random();
+    int base = random.nextInt(5);
+    int exponent = random.nextInt(20);
 
-  long start = System.nanoTime();
-  int value = (int)Math.pow((double)base,(double)exponent);
-  long end = System.nanoTime();
+    System.out.printf("Base: %d Exponent %d%n", base, exponent);
 
-  System.out.printf("Time taken: %d%n",end-start);
-  System.out.printf("Solution: %d%n",value);
-  
-  start = System.nanoTime();
-  value = fastExp1(base,exponent);
-  end = System.nanoTime();
+    long start = System.nanoTime();
+    int value = (int)Math.pow((double)base, (double)exponent);
+    long end = System.nanoTime();
 
-  System.out.printf("Time taken: %d%n",end-start);
-  System.out.printf("Solution: %d%n",value);
+    System.out.printf("Time taken: %d%n", end - start);
+    System.out.printf("Solution: %d%n", value);
 
-  start = System.nanoTime();
-  value = expSecond(base,exponent);
-  end = System.nanoTime();
+    start = System.nanoTime();
+    value = fastExp1(base, exponent);
+    end = System.nanoTime();
 
-  System.out.printf("Time taken: %d%n",end-start);
-  System.out.printf("Solution: %d%n",value);
+    System.out.printf("Time taken: %d%n", end - start);
+    System.out.printf("Solution: %d%n", value);
+
+    start = System.nanoTime();
+    value = expSecond(base, exponent);
+    end = System.nanoTime();
+
+    System.out.printf("Time taken: %d%n", end - start);
+    System.out.printf("Solution: %d%n", value);
   }
 }
