0c2482a3abbda7e05264334032e0b178bb7eed18
fernal73 <linus.fernandes@gmail.com>
2019-08-22 05:58:59 +0530

    Added new example to Concurrency

---
+++ Concurrency/dailyimages/NamedThreadFactory.java
@@ -0,0 +1,23 @@
+package dailyimages;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class NamedThreadFactory implements ThreadFactory {
+  private String name;
+  private int count;
+
+  public NamedThreadFactory(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public Thread newThread(Runnable r) {
+    count++;
+    ThreadFactory factory = Executors.defaultThreadFactory();
+    Thread t = factory.newThread(r);
+    t.setName(name + "-" + count);
+    System.out.println("New thread " + t);
+    return t;
+  }
+}
