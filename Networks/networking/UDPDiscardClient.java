15447f56b6df15d73d724fc0b8e3f97c0e95bbb6
fernal73 <linus.fernandes@gmail.com>
2019-08-23 14:50:43 +0530

    Added UDPDiscard*

---
+++ Networks/networking/UDPDiscardClient.java
@@ -0,0 +1,42 @@
+package networking;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public enum UDPDiscardClient {
+  ;
+  private final static int PORT = 9;
+
+  public static void main(String[] args) {
+    String hostname = args.length > 0 ? args[0] : "localhost";
+    int port = args.length > 1 ? readPort(args[1]) : PORT;
+    try (DatagramSocket theSocket = new DatagramSocket()) {
+      InetAddress server = InetAddress.getByName(hostname);
+      BufferedReader userInput =
+          new BufferedReader(new InputStreamReader(System.in));
+      while (true) {
+        String theLine = userInput.readLine();
+        if (theLine.equals("."))
+          break;
+        byte[] data = theLine.getBytes();
+        DatagramPacket theOutput =
+            new DatagramPacket(data, data.length, server, port);
+        theSocket.send(theOutput);
+      }  // end while
+    } catch (IOException ex) {
+      System.err.println(ex);
+    }
+  }
+
+  private static int readPort(String portVal) {
+    try {
+      return Integer.parseInt(portVal);
+    } catch (NumberFormatException nfe) {
+      return PORT;
+    }
+  }
+}
---
+++ Networks/networking/UDPDiscardServer.java
@@ -0,0 +1,47 @@
+package networking;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public enum UDPDiscardServer {
+  ;
+  public final static int PORT = 9;
+  public final static int MAX_PACKET_SIZE = 65507;
+
+  private static int readPort(String portVal) {
+    try {
+      return Integer.parseInt(portVal);
+    } catch (NumberFormatException nfe) {
+      return PORT;
+    }
+  }
+
+  public static void main(String[] args) {
+    int port = args.length > 0 ? readPort(args[0]) : PORT;
+    byte[] buffer = new byte[MAX_PACKET_SIZE];
+    System.out.println("port: " + port);
+    try (DatagramSocket server = new DatagramSocket(port)) {
+      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+      while (true) {
+        try {
+          server.receive(packet);
+          String s =
+              new String(packet.getData(), 0, packet.getLength(), "8859_1");
+          System.out.println(packet.getAddress() + " at port "
+                             + packet.getPort() + " says: " + s);
+          // reset the length for the next packet
+          packet.setLength(buffer.length);
+        } catch (IOException ex) {
+          System.err.println(ex);
+        }
+      }  // end while
+    } catch (SocketException ex) {
+      System.err.println(ex);
+    }
+  }
+}
