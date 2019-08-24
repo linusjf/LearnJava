3d6b4918fba111ec01be18081c9b2860043a1464
fernal73 <linus.fernandes@gmail.com>
2019-08-24 20:57:44 +0530

    UDP servers with channels

---
+++ Networks/networking/UDPDiscardServerWithChannels.java
@@ -0,0 +1,46 @@
+package networking;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+public enum UDPDiscardServerWithChannels {
+  ;
+
+  public final static int PORT = 9;
+
+  public final static int MAX_PACKET_SIZE = 65_507;
+
+  private static int readPort(String portVal) {
+    try {
+      return Integer.parseInt(portVal);
+    } catch (NumberFormatException nfe) {
+      return PORT;
+    }
+  }
+
+  public static void main(String[] args) {
+    int port = args.length > 0 ? readPort(args[0]) : PORT;
+    try {
+      DatagramChannel channel = DatagramChannel.open();
+      DatagramSocket socket = channel.socket();
+      SocketAddress address = new InetSocketAddress(port);
+      socket.bind(address);
+      ByteBuffer buffer = ByteBuffer.allocateDirect(MAX_PACKET_SIZE);
+      while (true) {
+        SocketAddress client = channel.receive(buffer);
+        buffer.flip();
+        System.out.print(client + " says: ");
+        while (buffer.hasRemaining())
+          System.out.write(buffer.get());
+        System.out.println();
+        buffer.clear();
+      }
+    } catch (IOException ex) {
+      System.err.println(ex);
+    }
+  }
+}
