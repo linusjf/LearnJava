07df17d7a400ac58d374692f017a512fe2650f1b
fernal73 <linus.fernandes@gmail.com>
2019-08-25 21:40:26 +0530

    Fixed checkstyle errors

--- Collections/collections/FootShootWithSafetyCatch.java
+++ Collections/collections/FootShootWithSafetyCatch.java
@@ -1,25 +1,23 @@
 package collections;
 
-import java.util.List;
-import java.util.Collections;
-import java.util.ArrayList;
-
 import static java.util.stream.Collectors.joining;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public enum FootShootWithSafetyCatch {
   ;
   private static List<String> names;
 
   public static void main(String... args) {
-    Thread.currentThread().setUncaughtExceptionHandler((t,e) -> {
-      System.err.printf("Thread %s throws following exception: %s%n",t,e);
-    System.out.println("Printing names...");
-    System.out.println(names.stream().collect(joining("+")));
-    }
-    );
+    Thread.currentThread().setUncaughtExceptionHandler((t, e) -> {
+      System.err.printf("Thread %s throws following exception: %s%n", t, e);
+      System.out.println("Printing names...");
+      System.out.println(names.stream().collect(joining("+")));
+    });
 
-    names = Collections.checkedList(
-        new ArrayList<String>(), String.class);
+    names = Collections.checkedList(new ArrayList<String>(), String.class);
     Collections.addAll(names, "John", "Anton", "Heinz");
     List huh = names;
     List<Integer> numbers = huh;
