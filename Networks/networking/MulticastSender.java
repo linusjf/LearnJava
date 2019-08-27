298e045b3e3286031f320c6026b27af4085cf62c
fernal73 <linus.fernandes@gmail.com>
2019-08-27 08:01:37 +0530

    Added Multicast examples

---
+++ Networks/networking/MulticastSender.java
@@ -0,0 +1,48 @@
+package networking;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public final class MulticastSender {
+
+  private MulticastSender() {
+    throw new IllegalStateException("Private constructor");
+  }
+
+  public static void main(String[] args) {
+    InetAddress ia = null;
+    int port = 0;
+    byte ttl = (byte)1;
+    // read the address from the command line
+    try {
+      ia = InetAddress.getByName(args[0]);
+      port = Integer.parseInt(args[1]);
+      if (args.length > 2)
+        ttl = (byte)Integer.parseInt(args[2]);
+    } catch (NumberFormatException | IndexOutOfBoundsException
+             | UnknownHostException ex) {
+      System.err.println(ex);
+      System.err.println(
+          "Usage: java MulticastSender multicast_address port ttl");
+      System.exit(1);
+    }
+    byte[] data = "Here's some multicast data\r\n".getBytes();
+    DatagramPacket dp = new DatagramPacket(data, data.length, ia, port);
+    try (MulticastSocket ms = new MulticastSocket()) {
+      ms.setTimeToLive(ttl);
+      ms.joinGroup(ia);
+      for (int i = 1; i < 10; i++) {
+        ms.send(dp);
+      }
+      ms.leaveGroup(ia);
+    } catch (SocketException ex) {
+      System.err.println(ex);
+    } catch (IOException ex) {
+      System.err.println(ex);
+    }
+  }
+}
