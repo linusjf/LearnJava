09fc2f837d3e8ed3d02824eba73a2df4b43590df
fernal73 <linus.fernandes@gmail.com>
2019-09-01 15:54:47 +0530

    Added RandomAccess file examples

---
+++ Files/io/RanFile1.java
@@ -0,0 +1,95 @@
+package io;
+
+import static io.RanFileConstants.*;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public enum RanFile1 {
+  ;
+  private static long acctNum;
+  private static String surname;
+  private static String initials;
+  private static float balance;
+
+  public static void main(String[] args) {
+    try (Scanner input = new Scanner(System.in);
+         RandomAccessFile ranAccts =
+             new RandomAccessFile("accounts.dat", "rw");) {
+      String reply = "y";
+      do {
+        acctNum++;
+        System.out.println("\nAccount number " + acctNum + ".\n");
+        System.out.print("Surname: ");
+        surname = input.nextLine();
+        System.out.print("Initial(s): ");
+        initials = input.nextLine();
+        System.out.print("Balance: ");
+        balance = input.nextFloat();
+        // Now get rid of carriage return(!)…
+        input.nextLine();
+        writeRecord(ranAccts);  // Method defined below.
+        System.out.print("\nDo you wish to do this again (y/n)? ");
+        reply = input.nextLine();
+      } while ("y".equalsIgnoreCase(reply));
+      System.out.println();
+      showRecords(ranAccts);
+    } catch (IOException ioe) {
+      System.err.println(ioe);
+    }
+  }
+
+  public static void writeRecord(RandomAccessFile file) throws IOException {
+    // First find starting byte for current record…
+    long filePos = (acctNum - 1) * REC_SIZE;
+    // Position file pointer…
+    file.seek(filePos);
+    // Now write the four (fixed-size) fields.
+    // Note that a definition must be provided
+    // for method writeString…
+    file.writeLong(acctNum);
+    writeString(file, surname, SURNAME_SIZE);
+    writeString(file, initials, NUM_INITS);
+    file.writeFloat(balance);
+  }
+
+  public static void writeString(RandomAccessFile file,
+                                 String text,
+                                 int fixedSize) throws IOException {
+    int size = text.length();
+    if (size <= fixedSize) {
+      file.writeChars(text);
+      // Now 'pad out' the field with spaces…
+      for (int i = size; i < fixedSize; i++)
+        file.writeChar(' ');
+    } else  // String is too long!
+      file.writeChars(text.substring(0, fixedSize));
+    // Write to file the first fixedSize characters of
+    // string text, starting at byte zero.
+  }
+
+  public static void showRecords(RandomAccessFile file) throws IOException {
+    long numRecords = file.length() / REC_SIZE;
+    file.seek(0);  // Go to start of file.
+    for (int i = 0; i < numRecords; i++) {
+      acctNum = file.readLong();
+      surname = readString(file, SURNAME_SIZE);
+      // readString defined below.
+      initials = readString(file, NUM_INITS);
+      balance = file.readFloat();
+      System.out.printf(acctNum + " " + surname + " " + initials + " "
+                            + "%.2f %n",
+                        balance);
+    }
+  }
+
+  public static String readString(RandomAccessFile file, int fixedSize)
+      throws IOException {
+    StringBuilder value = new StringBuilder();  // Set up empty string.
+    for (int i = 0; i < fixedSize; i++)
+      // Read character and concatenate it onto value…
+      value.append(file.readChar());
+    return value.toString();
+  }
+}
---
+++ Files/io/RanFile2.java
@@ -0,0 +1,69 @@
+package io;
+
+// Allows the user to retrieve individual account
+// records and modify their balances.
+import static io.RanFileConstants.*;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public enum RanFile2 {
+  ;
+  private static long acctNum;
+  private static String surname;
+  private static String initials;
+  private static float balance;
+
+  public static void main(String[] args) {
+    try (Scanner input = new Scanner(System.in);
+         RandomAccessFile ranAccts =
+             new RandomAccessFile("accounts.dat", "rw");) {
+      long numRecords = ranAccts.length() / REC_SIZE;
+      String reply;
+      long currentPos;  // File pointer position.
+      do {
+        System.out.print("\nEnter account number: ");
+        acctNum = input.nextLong();
+        while (acctNum < 1 || acctNum > numRecords) {
+          System.out.println("\n*** Invalid number! ***\n");
+          System.out.print("\nEnter account number: ");
+          acctNum = input.nextLong();
+        }
+        showRecord(ranAccts);  // Defined below.
+        System.out.print("\nEnter new balance: ");
+        balance = input.nextFloat();
+        input.nextLine();
+        // Get rid of carriage return!
+        currentPos = ranAccts.getFilePointer();
+        ranAccts.seek(currentPos - 4);  // Back 4 bytes.
+        ranAccts.writeFloat(balance);
+        System.out.print("\nModify another balance (y/n)? ");
+        reply = input.nextLine();
+      } while ("y".equalsIgnoreCase(reply));
+    } catch (IOException exc) {
+      System.err.println(exc);
+    }
+  }
+
+  public static void showRecord(RandomAccessFile file) throws IOException {
+    file.seek((acctNum - 1) * REC_SIZE);
+    acctNum = file.readLong();
+    surname = readString(file, SURNAME_SIZE);
+    initials = readString(file, NUM_INITS);
+    balance = file.readFloat();
+    System.out.println("Surname: " + surname);
+    System.out.println("Initials: " + initials);
+    System.out.printf("Balance: %.2f %n", balance);
+  }
+
+  public static String readString(RandomAccessFile file, int fixedSize)
+      throws IOException {
+    // Set up empty buffer before reading from file…
+    StringBuilder buffer = new StringBuilder();
+    for (int i = 0; i < fixedSize; i++)
+      // Read character from file and append to buffer.
+      buffer.append(file.readChar());
+    return buffer.toString();  // Convert into String.
+  }
+}
