a716ef6f95256bfacfee32f3362388df1b855cca
fernal73 <linus.fernandes@gmail.com>
2019-09-01 11:19:59 +0530

    Added two new examples

---
+++ Files/io/FileTest3.java
@@ -0,0 +1,25 @@
+package io;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public enum FileTest3 {
+  ;
+
+  public static void main(String[] args) {
+    int mark;
+    int total = 0;
+    int count = 0;
+    try (Scanner input = new Scanner(new File("marks1.txt"))) {
+      while (input.hasNext()) {
+        mark = input.nextInt();
+        total += mark;
+        count++;
+      }
+      System.out.println("Mean = " + (float)total / count);
+    } catch (IOException ioe) {
+      System.err.println(ioe);
+    }
+  }
+}
