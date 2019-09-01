d94b4e3f457f9b60247caa623a6c88598eeb7874
fernal73 <linus.fernandes@gmail.com>
2019-09-01 10:29:05 +0530

    Added new example FileTest2

---
+++ Files/io/FileTest2.java
@@ -0,0 +1,33 @@
+package io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public enum FileTest2 {
+  ;
+
+  public static void main(String[] args) {
+    String fileName;
+    int mark;
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.print("Enter file name: ");
+      fileName = input.nextLine();
+      PrintWriter output = new PrintWriter(new File(fileName));
+      System.out.println("Ten marks needed.\n");
+      for (int i = 1; i < 11; i++) {
+        if (input.hasNext()) {
+          System.out.print("Enter mark " + i + ": ");
+          mark = input.nextInt();
+          // * Should really validate entry! *
+          output.println(mark);
+          output.flush();
+        }
+      }
+      output.close();
+    } catch (IOException ioe) {
+      System.err.println(ioe);
+    }
+  }
+}
