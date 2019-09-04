1cadf21c2be2127302c33795737648b8833f51db
fernal73 <linus.fernandes@gmail.com>
2019-09-04 14:19:19 +0530

    added more records

--- JDBC/AccountsScript.sql
+++ JDBC/AccountsScript.sql
@@ -4,9 +4,18 @@ create table Accounts(acctNum int primary key, surname
 varchar(15), firstNames varchar(25), balance real);
 insert into Accounts values(123456, 'Black', 'James 
 Michael', 123.45);
-select * from Accounts; 
+INSERT INTO Accounts  VALUES (234567,'Smith','John James',752.85);
+INSERT INTO Accounts  VALUES (333333,'Jones','Sally',5000);
+INSERT INTO Accounts  VALUES (444444,'White','Peter',45);
+select * from Accounts;
+SELECT acctNum, balance FROM Accounts;
+SELECT * FROM Accounts WHERE balance >= 0 AND balance <= 1000 order by balance desc;
+SELECT * FROM Accounts WHERE surname < 'Jones';
+UPDATE Accounts SET surname = 'Bloggs',firstNames = 'Fred Joseph' WHERE acctNum = 123456;
 update Accounts set balance=999.99 where acctNum=123456; 
+DELETE FROM Accounts WHERE balance < 100;
 delete from Accounts where acctNum = 234567;
 disconnect; 
 connect 'jdbc:derby:Finances';
+select * from Accounts;
 exit;
