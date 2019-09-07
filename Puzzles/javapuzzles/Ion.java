19f377056b7df05b9534a830e06e6f966ef90515
fernal73 <linus.fernandes@gmail.com>
2019-09-07 22:31:49 +0530

    Changed java version. Added Ion with var

---
+++ Puzzles/javapuzzles/Ion.java
@@ -0,0 +1,12 @@
+package javapuzzles;
+
+public enum Ion {
+  ;
+
+  public static void main(String... args) {
+    var x = 'a' + 2;
+    System.out.println(x);
+    char y = 'b' + 1;
+    System.out.println(y);
+  }
+}
