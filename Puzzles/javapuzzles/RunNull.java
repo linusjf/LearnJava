11b1d1b2a7327033c8a368381a0bf280b1f89224
fernal73 <linus.fernandes@gmail.com>
2019-09-07 06:02:30 +0530

    RunNull

---
+++ Puzzles/javapuzzles/RunNull.java
@@ -0,0 +1,16 @@
+package javapuzzles;
+
+public enum RunNull {
+  ;
+  public static void run(String str) {
+    System.out.print("String");
+  }
+
+  public static void run(Object obj) {
+    System.out.print("Object");
+  }
+
+  public static void main(String... args) {
+    run(null);
+  }
+}
