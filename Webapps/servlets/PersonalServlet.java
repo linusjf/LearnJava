a8bd9ee58064d09b3c043ba08b1b86a988c84377
fernal73 <linus.fernandes@gmail.com>
2019-09-09 16:32:46 +0530

    Added more files for servlets

---
+++ Webapps/servlets/PersonalServlet.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/PersonalServlet")
+public class PersonalServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    response.setContentType("text/HTML");
+    PrintWriter out = response.getWriter();
+    out.println("<HTML>");
+    out.println("<HEAD>");
+    out.println("<TITLE>Simple Servlet</TITLE>");
+    out.println("</HEAD>");
+    out.println("<BODY>");
+    out.println("<BR><BR><BR>");
+    String name = request.getParameter("FirstName");
+    out.println("<H1> A Simple Servlet for ");
+    out.println(name + "</H1></CENTER>");
+    out.println("</BODY>");
+    out.println("</HTML>");
+    out.flush();
+  }
+}
