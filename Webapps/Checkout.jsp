029cb6da4d8c77a2e01a6053580a8108f9ab831b
fernal73 <linus.fernandes@gmail.com>
2019-09-15 16:46:09 +0530

    Added more files to Webapps

---
+++ Webapps/Checkout.jsp
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!-- Checkout.jsp --> 
+<%@ page import="java.util.Enumeration" %>
+<HTML> 
+ <HEAD> 
+ <TITLE>Checkout</TITLE> 
+ <STYLE> 
+ body{text-align=center; } 
+ table,th,td{border:2px solid black;} 
+ table{background-color:aqua;} 
+ </STYLE> 
+ </HEAD> 
+ <BODY> 
+ <%
+ final float APPLES_PRICE = 1.45F;
+ final float PEARS_PRICE = 1.75F;
+ //In a real application, the above prices would be
+ //retrieved from a database, of course.
+ %>
+ <BR/><BR/><BR/> 
+ <H1><P STYLE="color:red">Order List</P></H1> 
+ <BR><BR><BR> 
+ <TABLE> 
+ <TR> 
+ <TH>Item</TH> 
+ <TH>Weight(kg)</TH> 
+ <TH>Cost(£)</TH> 
+ </TR> 
+<!-- Now make use of the implicit object session --> 
+<!-- to retrieve the contents of the shopping cart… --> 
+<%
+ session.removeAttribute("currentProd");
+ //(Removes "Checkout".)
+ Enumeration prodNames = session.getAttributeNames();
+ float totalCost = 0;
+ int numProducts = 0;
+ while (prodNames.hasMoreElements())
+ {
+ float wt=0,cost=0;
+ String product = (String)prodNames.nextElement();
+ String stringWt =
+ (String)session.getAttribute(product);
+ wt = Float.parseFloat(stringWt);
+ if (product.equals("Apples"))
+ cost = APPLES_PRICE * wt;
+ else if (product.equals("Pears"))
+ cost = PEARS_PRICE * wt;
+%>
+ <TR> 
+ <TD> <%= product %></TD> 
+ <TD> <%= wt %></TD> 
+ <TD> <%= String.format("%.2f",cost) %></TD> 
+ </TR> 
+<%
+ totalCost+=cost;
+ numProducts++;
+ }
+%>
+ <TR STYLE="background-color:yellow"> 
+<%
+ if (numProducts == 0)
+ {
+ %>
+ <TD>*** No orders placed! ***</TD> 
+ </TR> 
+<%
+ }
+ else
+ {
+%>
+ <TR STYLE="background-color:yellow"> 
+ <TD></TD> <!-- Blank cell --> 
+ <TD>Total cost:</TD> 
+ <TD> 
+ <%= String.format("%.2f",totalCost) %></ TD> 
+ </TR> 
+<%
+ }
+%>
+ </TABLE> 
+ </BODY> 
+</HTML>
---
+++ Webapps/servlets/WeightX.java
@@ -0,0 +1,68 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+// @WebServlet("/WeightX")
+public class WeightX extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    HttpSession cart = request.getSession();
+    // Note the necessity for a typecast from Object
+    // into String .
+    String choice = request.getParameter("Option");
+    /*
+    Above parameter determines whether user wishes
+    to select another product, add the current order
+    to the cart, remove an existing order from
+    the cart or proceed to the checkout.
+    User is redirected to the appropriate page
+    (after any required updating of the shopping
+    cart session variable has been carried out).
+    */
+    if ("Next".equals(choice))
+      response.sendRedirect("ShoppingCartX.html");
+    if ("Checkout".equals(choice))
+      response.sendRedirect("Checkout.jsp");
+    if ("Add".equals(choice)) {
+      doAdd(cart, request);
+      response.sendRedirect("ShoppingCartX.html");
+    }
+    if ("Remove".equals(choice)) {
+      // Not really possible for it to be
+      // anything else, but play safe!
+      doRemove(cart);
+      response.sendRedirect("ShoppingCartX.html");
+    }
+  }
+
+  private void doAdd(HttpSession cart, HttpServletRequest request) {
+    String currentProduct = (String)cart.getAttribute("currentProd");
+    String qty = request.getParameter("Qty");
+    // Value of weight entered by user retrieved here.
+    if (qty != null) {
+      // Check that user actually entered a value!
+      if ("Apples".equals(currentProduct))
+        cart.setAttribute("Apples", qty);
+      else
+        cart.setAttribute("Pears", qty);
+    }
+  }
+
+  private void doRemove(HttpSession cart) {
+    String currentProduct = (String)cart.getAttribute("currentProd");
+    Object product = cart.getAttribute(currentProduct);
+    // Note that there is no need for a typecast into
+    // String , since we only need to know that there
+    // is an order for the current product in the cart.
+    if (product != null)
+      // Product found in cart.
+      cart.removeAttribute(currentProduct);
+  }
+}
