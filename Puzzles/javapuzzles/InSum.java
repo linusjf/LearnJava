8ad8972f1ff13850c91213fa681fea2bb936fcaf
fernal73 <linus.fernandes@gmail.com>
2019-09-17 07:44:54 +0530

    Caching effect added

--- Puzzles/javapuzzles/InSum.java
+++ Puzzles/javapuzzles/InSum.java
@@ -1,7 +1,7 @@
 package javapuzzles;
 
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 
 public enum InSum {
   ;
@@ -23,8 +23,15 @@ public enum InSum {
   public static void main(String... args) {
     List<Integer> bigs = Arrays.asList(100, 200, 300);
     System.out.println(sumInteger(bigs) == sum(bigs));
-    System.out.println(sumInteger(bigs) == sumInteger(bigs)); 
+    System.out.println(sumInteger(bigs) == sumInteger(bigs));
     assert sumInteger(bigs) == sum(bigs);
     assert sumInteger(bigs) != sumInteger(bigs);  // not recommended
+
+    // the effect of caching demonstrated
+    List<Integer> smalls = Arrays.asList(1, 2, 3);
+    System.out.println(sumInteger(smalls) == sum(smalls));
+    System.out.println(sumInteger(smalls) == sumInteger(smalls));
+    assert sumInteger(smalls) == sum(smalls);
+    assert sumInteger(smalls) == sumInteger(smalls);
   }
 }
