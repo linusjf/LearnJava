ef66f5f7d90480880866f230dc6444a77d2f4f2c
fernal73 <linus.fernandes@gmail.com>
2019-09-17 19:50:44 +0530

    Initialise lists in one line. Version 9

---
+++ Collections/collections/Listing.java
@@ -0,0 +1,51 @@
+package collections;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+// https://www.baeldung.com/java-init-list-one-line
+public enum Listing {
+
+  ;
+
+  public static void main(String... args) {
+
+    Thread.currentThread().setUncaughtExceptionHandler( (t,e) -> {
+      System.err.println(e);
+      System.err.printf(" in thread %s...",t);
+    });
+    
+    List<String> list = Arrays.asList(new String[] {"foo", "bar"});
+    assert list.contains("foo");
+
+    list = Arrays.asList("foo", "bar");
+
+    assert list.contains("foo");
+
+    list.add("baz");
+
+    String[] array = {"foo", "bar"};
+    list = Arrays.asList(array);
+    array[0] = "baz";
+    assert "baz" == list.get(0);
+
+    list = Stream.of("foo", "bar").collect(Collectors.toList());
+
+    assert list.contains("foo");
+    list = List.of("foo", "bar", "baz");
+    Set<String> set = Set.of("foo", "bar", "baz");
+    List<String> cities = new ArrayList<>() {
+      {
+        add("New York");
+        add("Rio");
+        add("Tokyo");
+      }
+    };
+
+    assert cities.contains("Rio");
+  }
+}
