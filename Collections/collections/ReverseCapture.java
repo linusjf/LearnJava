87c2664a6ff5b5dc28f8d3ede672dcd985c9ece5
fernal73 <linus.fernandes@gmail.com>
2019-09-18 11:55:16 +0530

    ReverseCapture

---
+++ Collections/collections/ReverseCapture.java
@@ -0,0 +1,25 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public enum ReverseCapture {
+  ;
+  public static void reverse(List<?> list) {
+    rev(list);
+  }
+
+  private static <T> void rev(List<T> list) {
+    List<T> tmp = new ArrayList<T>(list);
+    for (int i = 0; i < list.size(); i++) {
+      list.set(i, tmp.get(list.size() - i - 1));
+    }
+  }
+
+  public static void main(String... args) {
+    List<?> objects = Arrays.asList("Hello", "Humble", "Bundle");
+    reverse(objects);
+    System.out.println(objects);
+  }
+}
