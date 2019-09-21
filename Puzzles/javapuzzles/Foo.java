4e82dd070d900885ab7e4a51188753814523bbd9
fernal73 <linus.fernandes@gmail.com>
2019-09-22 04:31:46 +0530

    Foo updated with meaning of life

--- Puzzles/javapuzzles/Foo.java
+++ Puzzles/javapuzzles/Foo.java
@@ -1,5 +1,6 @@
 package javapuzzles;
 
+// https://twitter.com/heinzkabutz/status/1175283793592233985
 public class Foo {
   private final int i;
 
@@ -9,5 +10,20 @@ public class Foo {
 
   public void method(Foo this) {
     System.out.println(this.i);
+  }
+
+  public static void main(String... args) {
+    Foo foo = new Foo(42);
+    foo.method();
+  }
+}
+
+class Bar extends Foo {
+  public Bar(int i) {
+    super(i);
+  }
+
+  public void method() {
+    // empty method
   }
 }
