61f8470a901adbb3dd5712bd0b0fe0ba3ad0d3c0
fernal73 <linus.fernandes@gmail.com>
2019-09-23 18:20:33 +0530

    Fixed checkstyle violations

--- Switch/player/ComputeEnduranceAlgorithm.java
+++ Switch/player/ComputeEnduranceAlgorithm.java
@@ -1,11 +1,12 @@
 package player;
 
 public final class ComputeEnduranceAlgorithm {
+  private ComputeEnduranceAlgorithm() {
+    throw new IllegalStateException("Private constructor");
+  }
+  
   public static int basicEndurance(int delta) {
     return 2 * delta;
   }
 
-  private ComputeEnduranceAlgorithm() {
-    throw new IllegalStateException("Private constructor");
-  }
 }
