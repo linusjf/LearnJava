25783a8f89c58173087ecb2ec77a01987806df64
fernal73 <linus.fernandes@gmail.com>
2019-09-25 04:57:01 +0530

    Overflow

--- Puzzles/javapuzzles/Overflow.java
+++ Puzzles/javapuzzles/Overflow.java
@@ -7,5 +7,12 @@ public enum Overflow {
     int x = -2_000_000_000;
     int z = 2_000_000_000;
     System.out.println(x - z);
+
+    int y = 1024 * 1024 * 1024;
+    System.out.println(y);
+    y = y * 2;
+    System.out.println(y);
+    y = y * 2;
+    System.out.println(y);
   }
 }
