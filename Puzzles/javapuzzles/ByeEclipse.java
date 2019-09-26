608bea8ab0504904889f4ec9416764de7d087ef0
fernal73 <linus.fernandes@gmail.com>
2019-09-26 19:28:58 +0530

    ByeEclipse

---
+++ Puzzles/javapuzzles/ByeEclipse.java
@@ -0,0 +1,14 @@
+package javapuzzles;
+
+public enum ByeEclipse {
+  ;
+  public static void main(String... args) {
+    boolean b1 = true;
+    boolean b2 = false;
+    if ((b1 = false) | (b1 ^ b2)) {
+      System.out.println("Eclipse.");
+      return;
+    }
+    System.out.println("Bye.");
+  }
+}
