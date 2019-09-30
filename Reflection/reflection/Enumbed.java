5ef8faf685bb6f5e2bd567436ea1325231ab55cd
fernal73 <linus.fernandes@gmail.com>
2019-10-01 05:01:22 +0530

    Fixed checkstyle errors

--- Reflection/reflection/Enumbed.java
+++ Reflection/reflection/Enumbed.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Field;
 public final class Enumbed {
 
   enum ExampleEnum { ONE, TWO, THREE, FOUR }
-  ;
 
   private Enumbed() {
     throw new IllegalStateException("Private constructor");
@@ -14,7 +13,7 @@ public final class Enumbed {
   public static void main(String... args) {
     ExampleEnum value = ExampleEnum.FOUR;
     ExampleEnum[] enumConstants = value.getClass().getEnumConstants();
-    for (ExampleEnum exampleEnum: enumConstants) 
+    for (ExampleEnum exampleEnum: enumConstants)
       System.out.println("enum constant " + exampleEnum);
     Field[] flds = value.getClass().getDeclaredFields();
     for (Field f: flds)
