e89ddebb6c5c3b4301555c77465a08cfd49525c1
fernal73 <linus.fernandes@gmail.com>
2019-10-01 04:51:54 +0530

    Added Enum reflection example

---
+++ Reflection/reflection/Enumbed.java
@@ -0,0 +1,23 @@
+package reflection;
+
+import java.lang.reflect.Field;
+
+public final class Enumbed {
+
+  enum ExampleEnum { ONE, TWO, THREE, FOUR }
+  ;
+
+  private Enumbed() {
+    throw new IllegalStateException("Private constructor");
+  }
+
+  public static void main(String... args) {
+    ExampleEnum value = ExampleEnum.FOUR;
+    ExampleEnum[] enumConstants = value.getClass().getEnumConstants();
+    for (ExampleEnum exampleEnum: enumConstants) 
+      System.out.println("enum constant " + exampleEnum);
+    Field[] flds = value.getClass().getDeclaredFields();
+    for (Field f: flds)
+      System.out.println(f.getName() + " " + f.isEnumConstant());
+  }
+}
