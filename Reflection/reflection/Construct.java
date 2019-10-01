0825dfcb9a066abe3440cace40d5a8414a3635af
fernal73 <linus.fernandes@gmail.com>
2019-10-01 12:21:06 +0530

    Construct.java

---
+++ Reflection/reflection/Construct.java
@@ -0,0 +1,43 @@
+package reflection;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+
+public final class Construct {
+  private Construct() {
+    throw new IllegalStateException("Private constructor.");
+  }
+
+  public static void main(String... args) {
+    // get all visible constructors
+    Constructor<?>[] constructors = String.class.getConstructors();
+    // all constructors
+    Constructor<?>[] declaredConstructors =
+        String.class.getDeclaredConstructors();
+    for (Constructor<?> constructor: declaredConstructors) {
+      int numberParams = constructor.getParameterCount();
+      System.out.println("constructor " + constructor.getName());
+      System.out.println("number of arguments " + numberParams);
+      // public, private, etc.
+      int modifiersConstructor = constructor.getModifiers();
+      System.out.println("modifiers " + modifiersConstructor);
+      // array of parameters, more info in the methods section
+      Parameter[] parameters = constructor.getParameters();
+      System.out.println(parameters.length + " parameters:");
+      // also method.getParameterCount() is possible
+      for (Parameter parameter: parameters) {
+        System.out.println("parameter name: " + parameter.getName());
+        System.out.println("parameter type: " + parameter.getType());
+      }
+
+      // annotations array, more info in the annotations section
+      Annotation[] annotations = constructor.getAnnotations();
+      if (annotations.length > 0) {
+        System.out.println("Annotations: ");
+        for (Annotation anno: annotations)
+          System.out.println(anno.annotationType());
+      }
+    }
+  }
+}
