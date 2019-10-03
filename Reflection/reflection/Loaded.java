d0741bfd35c3dd28e47cd87b2521c61f1af079a2
fernal73 <linus.fernandes@gmail.com>
2019-10-03 06:45:50 +0530

    Fixed checkstyle and pmd violations"

--- Reflection/reflection/Loaded.java
+++ Reflection/reflection/Loaded.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 public enum Loaded {
   ;
 
+  private static final String REFLECTABLE_CLASS = "reflection.ReflectableClass";
+
   @SuppressWarnings({"PMD.UseProperClassLoader",
                      "PMD.CompareObjectsWithEquals"})
   public static void
@@ -14,14 +16,13 @@ public enum Loaded {
       ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
       ClassLoader classClassLoader = ReflectableClass.class.getClassLoader();
       Class<?> reflectableClassInstanceLoaded =
-          systemClassLoader.loadClass("reflection.ReflectableClass");
+          systemClassLoader.loadClass(REFLECTABLE_CLASS);
       Class<?> reflectableClassInstanceForName =
-          Class.forName("reflection.ReflectableClass", true, systemClassLoader);
+          Class.forName(REFLECTABLE_CLASS, true, systemClassLoader);
       Class<?> reflectableClassInstanceLoadedClass =
-          classClassLoader.loadClass("reflection.ReflectableClass");
+          classClassLoader.loadClass(REFLECTABLE_CLASS);
       ClassLoader testClassLoader = new TestClassLoader();
-      Class<?> classInstance =
-          testClassLoader.loadClass("reflection.ReflectableClass");
+      Class<?> classInstance = testClassLoader.loadClass(REFLECTABLE_CLASS);
       System.out.println(reflectableClassInstanceLoaded
                          == reflectableClassInstanceForName);
       System.out.println(reflectableClassInstanceLoaded.equals(
@@ -41,18 +42,18 @@ public enum Loaded {
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-      if (!name.equals("reflection.ReflectableClass")) {
+      if (!REFLECTABLE_CLASS.replace(".", "/").equals(name)) {
         return super.loadClass(name);
       }
       try {
         InputStream in = ClassLoader.getSystemResourceAsStream(
             name.replace(".", "/") + ".class");
-        byte[] a = new byte[10000];
+        byte[] a = new byte[10_000];
         int len = in.read(a);
         in.close();
         return defineClass(name, a, 0, len);
       } catch (IOException e) {
-        throw new ClassNotFoundException();
+        throw new ClassNotFoundException(e.getMessage(), e);
       }
     }
   }
