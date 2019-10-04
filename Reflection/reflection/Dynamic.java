ad0ce9992131a451058bf10567188c0c1909e26e
fernal73 <linus.fernandes@gmail.com>
2019-10-04 18:00:56 +0530

    Added another InvocationHandler

--- Reflection/reflection/Dynamic.java
+++ Reflection/reflection/Dynamic.java
@@ -3,10 +3,13 @@ package reflection;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
 public final class Dynamic {
+
+  private static final String HELLO = "Hello";
 
   private Dynamic() {
     throw new IllegalStateException("Private constructor");
@@ -20,8 +23,22 @@ public final class Dynamic {
             Thread.currentThread().getContextClassLoader(),
             new Class<?>[] {Map.class},
             new DynamicInvocationHandler());
-    proxyInstance.put("hello", "world");
-    System.out.println(proxyInstance.get("hello"));
+    proxyInstance.put(HELLO, "world");
+    System.out.println(proxyInstance.get(HELLO));
+
+    proxyInstance = (Map<Object, Object>)Proxy.newProxyInstance(
+        Thread.currentThread().getContextClassLoader(),
+        new Class<?>[] {Map.class},
+        new TimingDynamicInvocationHandler(new HashMap<Object, Object>()));
+
+    CharSequence csProxyInstance = (CharSequence)Proxy.newProxyInstance(
+        Thread.currentThread().getContextClassLoader(),
+        new Class<?>[] {CharSequence.class},
+        new TimingDynamicInvocationHandler("Hello World"));
+
+    proxyInstance.put(HELLO, "world");
+    System.out.println(proxyInstance.get(HELLO));
+    csProxyInstance.length();
   }
 
   static class DynamicInvocationHandler implements InvocationHandler {
@@ -39,6 +56,39 @@ public final class Dynamic {
           });
       // clang-format on
       return 42;
+    }
+  }
+
+  static class TimingDynamicInvocationHandler implements InvocationHandler {
+
+    private static final Logger LOGGER =
+        Logger.getLogger(TimingDynamicInvocationHandler.class.getName());
+
+    private final Map<String, Method> methods = new HashMap<>();
+
+    private Object target;
+
+    TimingDynamicInvocationHandler(Object target) {
+      this.target = target;
+      for (Method method: target.getClass().getDeclaredMethods()) {
+        this.methods.put(method.getName(), method);
+      }
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args)
+        throws Throwable {
+      long start = System.nanoTime();
+      Object result = methods.get(method.getName()).invoke(target, args);
+      long elapsed = System.nanoTime() - start;
+
+      // clang-format off
+      LOGGER.info(
+          () -> {
+            return String.format("Executing %s finished in %d ns", method.getName(), elapsed);
+          });
+      // clang-format on
+      return result;
     }
   }
 }
