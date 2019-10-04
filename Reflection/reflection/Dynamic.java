62a60d029773f131e7a7b49b09a9efd43f102294
fernal73 <linus.fernandes@gmail.com>
2019-10-04 16:32:57 +0530

    Formatted. Added checkstyle suppressions

--- Reflection/reflection/Dynamic.java
+++ Reflection/reflection/Dynamic.java
@@ -6,28 +6,38 @@ import java.lang.reflect.Proxy;
 import java.util.Map;
 import java.util.logging.Logger;
 
-public class Dynamic {
+public final class Dynamic {
+
+  private Dynamic() {
+    throw new IllegalStateException("Private constructor");
+  }
 
   @SuppressWarnings("unchecked")
   public static void main(String... args) {
 
-    Map<? super Object,? super Object> proxyInstance =
-        (Map<? super Object,? super Object>)Proxy.newProxyInstance(Dynamic.class.getClassLoader(),
-                                    new Class<?>[] {Map.class},
-                                    new DynamicInvocationHandler());
+    Map<? super Object, ? super Object> proxyInstance =
+        (Map<? super Object, ? super Object>)Proxy.newProxyInstance(
+            Thread.currentThread().getContextClassLoader(),
+            new Class<?>[] {Map.class},
+            new DynamicInvocationHandler());
     proxyInstance.put("hello", "world");
-    System.out.println(proxyInstance.get("hello")); 
+    System.out.println(proxyInstance.get("hello"));
   }
 
   static class DynamicInvocationHandler implements InvocationHandler {
 
-    private static Logger LOGGER =
+    private static final Logger LOGGER =
         Logger.getLogger(DynamicInvocationHandler.class.getName());
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args)
         throws Throwable {
-      LOGGER.info(() -> { return "Invoked method: " + method.getName(); });
+      // clang-format off
+      LOGGER.info(
+          () -> {
+            return "Invoked method: " + method.getName();
+          });
+      // clang-format on
       return 42;
     }
   }
