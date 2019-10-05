efb1dd7945b99d353b164b0e65e89c192fe69c87
fernal73 <linus.fernandes@gmail.com>
2019-10-05 09:05:11 +0530

    Fixed pmd checkstyle errors

--- Cryptography/cryptic/RsaAlgorithm.java
+++ Cryptography/cryptic/RsaAlgorithm.java
@@ -1,27 +1,24 @@
 package cryptic;
 
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.Random;
 
 public class RsaAlgorithm {
   private static final int MAX_LENGTH = 1024;
-  private BigInteger p;
-  private BigInteger q;
   private BigInteger n;
-  private BigInteger phi;
   private BigInteger e;
   private BigInteger d;
-  private Random random;
 
   public RsaAlgorithm() {
-    random = new Random();
-    p = BigInteger.probablePrime(MAX_LENGTH, random);
-    q = BigInteger.probablePrime(MAX_LENGTH, random);
+    Random random = new Random();
+    BigInteger p = BigInteger.probablePrime(MAX_LENGTH, random);
+    BigInteger q = BigInteger.probablePrime(MAX_LENGTH, random);
     n = p.multiply(q);
-    phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+    BigInteger phi =
+        p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
     e = BigInteger.probablePrime(MAX_LENGTH / 2, random);
     while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0) {
       e.add(BigInteger.ONE);
@@ -36,37 +33,37 @@ public class RsaAlgorithm {
   }
 
   public static void main(String[] arguments) throws IOException {
-    RsaAlgorithm rsa = new RsaAlgorithm();
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     String inputString;
     System.out.println("Enter message you wish to send.");
     inputString = input.readLine();
     System.out.println("Encrypting the message: " + inputString);
     System.out.println("The message in bytes is:: "
-                       + bToS(inputString.getBytes()));
+                       + bytesToString(inputString.getBytes()));
+    RsaAlgorithm rsa = new RsaAlgorithm();
     // encryption
     byte[] cipher = rsa.encryptMessage(inputString.getBytes());
     // decryption
     byte[] plain = rsa.decryptMessage(cipher);
-    System.out.println("Decrypting Bytes: " + bToS(plain));
+    System.out.println("Decrypting Bytes: " + bytesToString(plain));
+
     System.out.println("Plain message is: " + new String(plain));
   }
 
-  private static String bToS(byte[] cipher) {
-    String temp = "";
-    for (byte b: cipher) {
-      temp += Byte.toString(b);
-    }
-    return temp;
+  private static String bytesToString(byte[] cipher) {
+    StringBuilder temp = new StringBuilder();
+    for (byte b: cipher)
+      temp.append(Byte.toString(b));
+    return temp.toString();
   }
 
   // Encrypting the message
   public byte[] encryptMessage(byte[] message) {
-    return (new BigInteger(message)).modPow(e, n).toByteArray();
+    return new BigInteger(message).modPow(e, n).toByteArray();
   }
 
   // Decrypting the message
   public byte[] decryptMessage(byte[] message) {
-    return (new BigInteger(message)).modPow(d, n).toByteArray();
+    return new BigInteger(message).modPow(d, n).toByteArray();
   }
 }
