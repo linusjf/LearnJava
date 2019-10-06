ec1b6b0ea66949699f95bde35b3587ef7acbe786
fernal73 <linus.fernandes@gmail.com>
2019-10-06 16:52:46 +0530

    Retrieve annotations

---
+++ Annotations/annotations/Retrieve.java
@@ -0,0 +1,30 @@
+package annotations;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public enum Retrieve {
+  ;
+
+  public static void main(String[] args) {
+    Class<AnnotatedClass> object = AnnotatedClass.class;
+    // Retrieve all annotations from the class
+    Annotation[] annotations = object.getAnnotations();
+    for (Annotation annotation: annotations)
+      System.out.println(annotation);
+    // Checks if an annotation is present
+    if (object.isAnnotationPresent(CustomAnnotationClass.class)) {
+      // Gets the desired annotation
+      Annotation annotation = object.getAnnotation(CustomAnnotationClass.class);
+      System.out.println(annotation);
+    }
+    // the same for all methods of the class
+    for (Method method: object.getDeclaredMethods()) {
+      if (method.isAnnotationPresent(CustomAnnotationMethod.class)) {
+        Annotation annotation =
+            method.getAnnotation(CustomAnnotationMethod.class);
+        System.out.println(annotation);
+      }
+    }
+  }
+}
