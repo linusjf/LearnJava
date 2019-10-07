d7363bc271f026da09402010dc343717d4b147a6
fernal73 <linus.fernandes@gmail.com>
2019-10-07 18:38:34 +0530

    pmd violations fixed. modified pmd target to capture number of violatioons for output

--- Collections/collections/ReverseCapture.java
+++ Collections/collections/ReverseCapture.java
@@ -12,7 +12,7 @@ public enum ReverseCapture {
   }
 
   private static <T> void rev(List<T> list) {
-    List<T> tmp = new ArrayList<T>(list);
+    List<T> tmp = new ArrayList<>(list);
     for (int i = 0; i < list.size(); i++) {
       list.set(i, tmp.get(list.size() - i - 1));
     }
--- Collections/collections/WildcardRestrictions.java
+++ Collections/collections/WildcardRestrictions.java
@@ -7,7 +7,7 @@ public enum WildcardRestrictions {
   ;
 
   public static void main(String... args) {
-    List<Number> nums = new ArrayList<Number>();
+    List<Number> nums = new ArrayList<>();
     List<? super Number> sink = nums;
     List<? extends Number> source = nums;
     for (int i = 0; i < 10; i++)
