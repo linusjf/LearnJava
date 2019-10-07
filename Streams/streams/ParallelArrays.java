7af1dfdd3f5aff48a53056e38afc2c3f8d8d7df7
fernal73 <linus.fernandes@gmail.com>
2019-10-07 21:16:24 +0530

    retain formatting.

--- Streams/streams/ParallelArrays.java
+++ Streams/streams/ParallelArrays.java
@@ -5,11 +5,13 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public enum ParallelArrays {
   ;
-
+  
+  // clang-format off
   public static void main(String[] args) {
     long[] arrayOfLong = new long[20_000];
     Arrays.parallelSetAll(
-        arrayOfLong, index -> ThreadLocalRandom.current().nextInt(1_000_000));
+        arrayOfLong, 
+        index -> ThreadLocalRandom.current().nextInt(1_000_000));
     Arrays.stream(arrayOfLong)
         .limit(10)
         .forEach(i -> System.out.print(i + " "));
@@ -20,4 +22,5 @@ public enum ParallelArrays {
         .forEach(i -> System.out.print(i + " "));
     System.out.println();
   }
+  // clang-format on
 }
