fd8b7e103c164db51cd32197fde832765800c045
fernal73 <linus.fernandes@gmail.com>
2019-10-12 11:21:10 +0530

    Catch exception

--- Puzzles/javapuzzles/Voiding.java
+++ Puzzles/javapuzzles/Voiding.java
@@ -1,14 +1,18 @@
 package javapuzzles;
 
 public enum Voiding {
-;
+  ;
 
   public static void main(String... args) {
-    Void[] voids = new Void[1];
-    System.out.println(voids);
-    voids = new Void[0];
-    System.out.println(voids);
-    voids = new Void[-20];
-    System.out.println(voids);
+    try {
+      Void[] voids = new Void[1];
+      System.out.println(voids);
+      voids = new Void[0];
+      System.out.println(voids);
+      voids = new Void[-20];
+      System.out.println(voids);
+    } catch (NegativeArraySizeException nase) {
+      System.err.println(nase);
+    }
   }
 }
