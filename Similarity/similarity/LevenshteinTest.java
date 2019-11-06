8f12258da3187addf31205191dfc36dab4dd24f7
fernal73 <linus.fernandes@gmail.com>
2019-11-06 19:41:20 +0530

    Added Levenshtein test

---
+++ Similarity/similarity/LevenshteinTest.java
@@ -0,0 +1,17 @@
+package similarity;
+
+import info.debatty.java.stringsimilarity.*;
+
+public enum LevenshteinTest {
+  ;
+    
+    public static void main (String[] args) {
+        Levenshtein l = new Levenshtein();
+
+        System.out.println(l.distance("My string", "My $tring"));
+        System.out.println(l.distance("column", "columns"));
+        System.out.println(l.distance("col", "col1"));
+        System.out.println(l.distance("col", "col12"));
+        System.out.println(l.distance("col", "col123"));
+    }
+}
