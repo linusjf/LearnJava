e697ed6ae481d15f7877bca2564d38478a7c8886
fernal73 <linus.fernandes@gmail.com>
2019-11-06 21:06:28 +0530

    NormalizedLevenshtein

---
+++ Similarity/similarity/NormalizedLevenshteinTest.java
@@ -0,0 +1,22 @@
+package similarity;
+
+import info.debatty.java.stringsimilarity.NormalizedLevenshtein;
+
+public enum NormalizedLevenshteinTest {
+  ;
+
+  public static void main(String[] args) {
+    NormalizedLevenshtein nl = new NormalizedLevenshtein();
+
+    System.out.println(nl.distance("My string", "My $tring"));
+    System.out.println(nl.distance("column", "columns"));
+    System.out.println(nl.distance("col", "col1"));
+    System.out.println(nl.distance("col", "col12"));
+    System.out.println(nl.distance("col", "col123"));
+    System.out.println(1 - nl.distance("My string", "My $tring"));
+    System.out.println(1 - nl.distance("column", "columns"));
+    System.out.println(1 - nl.distance("col", "col1"));
+    System.out.println(1 - nl.distance("col", "col12"));
+    System.out.println(1 - nl.distance("col", "col123"));
+  }
+}
