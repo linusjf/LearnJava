c2d157fd5a61ab37e911681d4ac4cb65dcb4ecb3
fernal73 <linus.fernandes@gmail.com>
2019-11-06 19:55:53 +0530

    Formatted. Fixed build errors

--- Similarity/similarity/LevenshteinTest.java
+++ Similarity/similarity/LevenshteinTest.java
@@ -4,14 +4,14 @@ import info.debatty.java.stringsimilarity.*;
 
 public enum LevenshteinTest {
   ;
-    
-    public static void main (String[] args) {
-        Levenshtein l = new Levenshtein();
 
-        System.out.println(l.distance("My string", "My $tring"));
-        System.out.println(l.distance("column", "columns"));
-        System.out.println(l.distance("col", "col1"));
-        System.out.println(l.distance("col", "col12"));
-        System.out.println(l.distance("col", "col123"));
-    }
+  public static void main(String[] args) {
+    Levenshtein l = new Levenshtein();
+
+    System.out.println(l.distance("My string", "My $tring"));
+    System.out.println(l.distance("column", "columns"));
+    System.out.println(l.distance("col", "col1"));
+    System.out.println(l.distance("col", "col12"));
+    System.out.println(l.distance("col", "col123"));
+  }
 }
