5b147da8d5f5be3571f5d1d493db7f3f6cf49d01
fernal73 <linus.fernandes@gmail.com>
2019-11-06 22:03:25 +0530

    WeightedLevenshtein

---
+++ Similarity/similarity/WeightedLevenshteinTest.java
@@ -0,0 +1,29 @@
+package similarity;
+
+import info.debatty.java.stringsimilarity.CharacterSubstitutionInterface;
+import info.debatty.java.stringsimilarity.WeightedLevenshtein;
+
+public enum WeightedLevenshteinTest {
+  ;
+
+  public static void main(String... args) {
+    WeightedLevenshtein wl =
+        new WeightedLevenshtein(new CharacterSubstitutionInterface() {
+          public double cost(char c1, char c2) {
+
+            // The cost for substituting 't' and 'r' is considered
+            // smaller as these 2 are located next to each other
+            // on a keyboard
+            if (c1 == 't' && c2 == 'r') {
+              return 0.5;
+            }
+
+            // For most cases, the cost of substituting 2 characters
+            // is 1.0
+            return 1.0;
+          }
+        });
+
+    System.out.println(wl.distance("String1", "Srring2"));
+  }
+}
