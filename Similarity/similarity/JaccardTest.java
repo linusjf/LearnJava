63eb2095d0a67ab1788b3bd4e0ee7c1187d77ad1
fernal73 <linus.fernandes@gmail.com>
2019-11-07 22:48:33 +0530

    Added Jaccard placeholder

---
+++ Similarity/similarity/JaccardTest.java
@@ -0,0 +1,19 @@
+package similarity;
+
+import info.debatty.java.stringsimilarity.Jaccard;
+
+public enum JaccardTest {
+;
+
+public static void main(String... args) {
+
+  Jaccard jac = new Jaccard();
+
+  System.out.println("\nJaccard: \n");
+
+  System.out.println("Similarity:\n");
+
+
+}
+
+}
