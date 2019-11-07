81e4940d051dcefcf8e117bc4f1726ea961d5d43
fernal73 <linus.fernandes@gmail.com>
2019-11-07 12:41:52 +0530

    Added two more string similarity tests

---
+++ Similarity/similarity/JaroWinklerTest.java
@@ -0,0 +1,64 @@
+package similarity;
+
+import info.debatty.java.stringsimilarity.JaroWinkler;
+
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+public enum JaroWinklerTest {
+  ;
+
+  public static void main(String[] args) {
+    JaroWinkler jw = new JaroWinkler();
+
+    System.out.println("\nJaro-Winkler: \n");
+
+    System.out.println("\nSimilarity: \n");
+    // substitution of s and t
+    System.out.println(jw.similarity("My string", "My tsring"));
+
+    // substitution of s and n
+    System.out.println(jw.similarity("My string", "My ntrisg"));
+
+    // 1 substitution
+    System.out.println(jw.similarity("ABCDEF", "ABDCEF"));
+
+    // 2 substitutions
+    System.out.println(jw.similarity("ABCDEF", "BACDFE"));
+
+    // 1 deletion
+    System.out.println(jw.similarity("ABCDEF", "ABCDE"));
+    System.out.println(jw.similarity("ABCDEF", "BCDEF"));
+    System.out.println(jw.similarity("ABCDEF", "ABCGDEF"));
+
+    // All different
+    System.out.println(jw.similarity("ABCDEF", "POIU"));
+    System.out.println(jw.similarity("column", "columns"));
+    System.out.println(jw.similarity("col", "col1"));
+    System.out.println(jw.similarity("col", "col12"));
+    System.out.println(jw.similarity("col", "col123"));
+
+    System.out.println("\nDistance: \n");
+    // substitution of s and t
+    System.out.println(1 - jw.similarity("My string", "My tsring"));
+
+    // substitution of s and n
+    System.out.println(1 - jw.similarity("My string", "My ntrisg"));
+
+    // 1 substitution
+    System.out.println(1 - jw.similarity("ABCDEF", "ABDCEF"));
+
+    // 2 substitutions
+    System.out.println(1 - jw.similarity("ABCDEF", "BACDFE"));
+
+    // 1 deletion
+    System.out.println(1 - jw.similarity("ABCDEF", "ABCDE"));
+    System.out.println(1 - jw.similarity("ABCDEF", "BCDEF"));
+    System.out.println(1 - jw.similarity("ABCDEF", "ABCGDEF"));
+
+    // All different
+    System.out.println(1 - jw.similarity("ABCDEF", "POIU"));
+    System.out.println(1 - jw.similarity("column", "columns"));
+    System.out.println(1 - jw.similarity("col", "col1"));
+    System.out.println(1 - jw.similarity("col", "col12"));
+    System.out.println(1 - jw.similarity("col", "col123"));
+  }
+}
---
+++ Similarity/similarity/LCSTest.java
@@ -0,0 +1,18 @@
+package similarity;
+
+import info.debatty.java.stringsimilarity.LongestCommonSubsequence;
+
+public enum LCSTest {
+  ;
+
+  public static void main(String[] args) {
+    LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+
+    System.out.println("\nLongest Common Subsequence: \n");
+    // Will produce 4.0
+    System.out.println(lcs.distance("AGCAT", "GAC"));
+
+    // Will produce 1.0
+    System.out.println(lcs.distance("AGCAT", "AGCT"));
+  }
+}
