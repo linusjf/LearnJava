d72ba230d5aacd37c60561332760a4fb0f0d3edd
fernal73 <linus.fernandes@gmail.com>
2019-11-07 18:23:57 +0530

    Added QGramTest

---
+++ Similarity/similarity/QGramTest.java
@@ -0,0 +1,19 @@
+package similarity;
+
+import info.debatty.java.stringsimilarity.QGram;
+
+public enum QGramTest {
+  ;
+
+  public static void main(String[] args) {
+    QGram dig = new QGram(2);
+
+    // AB BC CD CE
+    // 1  1  1  0
+    // 1  1  0  1
+    // Total: 2
+
+    System.out.println("\nQGram: \n");
+    System.out.println(dig.distance("ABCD", "ABCE"));
+  }
+}
