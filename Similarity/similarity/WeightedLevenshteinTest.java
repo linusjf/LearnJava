aac2fa093175910c39f3f95cc6f932f3de0bea92
fernal73 <linus.fernandes@gmail.com>
2019-11-07 05:57:19 +0530

    Fixed errors and warnings

--- Similarity/similarity/WeightedLevenshteinTest.java
+++ Similarity/similarity/WeightedLevenshteinTest.java
@@ -3,6 +3,8 @@ package similarity;
 import info.debatty.java.stringsimilarity.CharacterSubstitutionInterface;
 import info.debatty.java.stringsimilarity.WeightedLevenshtein;
 import org.javatuples.Pair;
+import java.util.List;
+import java.util.ArrayList;
 
 public enum WeightedLevenshteinTest {
   ;
@@ -10,31 +12,31 @@ public enum WeightedLevenshteinTest {
   private static List<Pair<Character,Character>> keyPairs = new ArrayList<>();
 
   static {
-    keyPairs.add(new Pair('q','w'));
-    keyPairs.add(new Pair('w','e'));
-    keyPairs.add(new Pair('e','r'));
-    keyPairs.add(new Pair('r','t'));
-    keyPairs.add(new Pair('t','y'));
-    keyPairs.add(new Pair('y','u'));
-    keyPairs.add(new Pair('u','i'));
-    keyPairs.add(new Pair('i','o'));
-    keyPairs.add(new Pair('o','p'));
+    keyPairs.add(new Pair<>('q','w'));
+    keyPairs.add(new Pair<>('w','e'));
+    keyPairs.add(new Pair<>('e','r'));
+    keyPairs.add(new Pair<>('r','t'));
+    keyPairs.add(new Pair<>('t','y'));
+    keyPairs.add(new Pair<>('y','u'));
+    keyPairs.add(new Pair<>('u','i'));
+    keyPairs.add(new Pair<>('i','o'));
+    keyPairs.add(new Pair<>('o','p'));
 
-    keyPairs.add(new Pair('a','s'));
-    keyPairs.add(new Pair('s','d'));
-    keyPairs.add(new Pair('d','f'));
-    keyPairs.add(new Pair('f','g'));
-    keyPairs.add(new Pair('g','h'));
-    keyPairs.add(new Pair('h','j'));
-    keyPairs.add(new Pair('j','k'));
-    keyPairs.add(new Pair('k','l'));
+    keyPairs.add(new Pair<>('a','s'));
+    keyPairs.add(new Pair<>('s','d'));
+    keyPairs.add(new Pair<>('d','f'));
+    keyPairs.add(new Pair<>('f','g'));
+    keyPairs.add(new Pair<>('g','h'));
+    keyPairs.add(new Pair<>('h','j'));
+    keyPairs.add(new Pair<>('j','k'));
+    keyPairs.add(new Pair<>('k','l'));
     
-    keyPairs.add(new Pair('z','x'));
-    keyPairs.add(new Pair('x','c'));
-    keyPairs.add(new Pair('c','v'));
-    keyPairs.add(new Pair('v','b'));
-    keyPairs.add(new Pair('b','n'));
-    keyPairs.add(new Pair('n','m'));
+    keyPairs.add(new Pair<>('z','x'));
+    keyPairs.add(new Pair<>('x','c'));
+    keyPairs.add(new Pair<>('c','v'));
+    keyPairs.add(new Pair<>('v','b'));
+    keyPairs.add(new Pair<>('b','n'));
+    keyPairs.add(new Pair<>('n','m'));
   }
 
 
