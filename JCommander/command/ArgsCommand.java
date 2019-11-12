73f9d4ce56b06918415f1ac753459917ab91f71a
fernal73 <linus.fernandes@gmail.com>
2019-11-12 11:22:49 +0530

    More entries and another Command

---
+++ JCommander/command/ArgsCommand.java
@@ -0,0 +1,29 @@
+package command;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import org.junit.Assert;
+
+class ArgsMaster {
+  @Parameter(names = "-master")
+  String master;
+}
+
+class ArgsSlave {
+  @Parameter(names = "-slave")
+  String slave;
+}
+
+public enum ArgsCommand {
+  ;
+
+  public static void main(String... args) {
+    ArgsMaster m = new ArgsMaster();
+    ArgsSlave s = new ArgsSlave();
+    JCommander.newBuilder().addObject(new Object[] {m, s}).build().parse(args);
+
+    Assert.assertEquals(m.master, "master");
+    Assert.assertEquals(s.slave, "slave");
+    System.out.println("Asserts cleared.");
+  }
+}
