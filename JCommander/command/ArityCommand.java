4337cdc9f7d717ebd60479eee07f1dde6540874c
fernal73 <linus.fernandes@gmail.com>
2019-11-12 14:42:29 +0530

    Added arity commands

---
+++ JCommander/command/ArityCommand.java
@@ -0,0 +1,23 @@
+package command;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import java.util.List;
+
+public class ArityCommand {
+
+  @Parameter(names = "-pairs", arity = 2, description = "Pairs")
+  private List<String> pairs;
+
+  public static void main(String... argv) {
+    ArityCommand ac = new ArityCommand();
+
+    JCommander.newBuilder().addObject(ac).build().parse(argv);
+    ac.run();
+  }
+
+  public void run() {
+    for (String one: pairs)
+      System.out.println(one);
+  }
+}
---
+++ JCommander/command/VariableArityCommand.java
@@ -0,0 +1,24 @@
+package command;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VariableArityCommand {
+
+  @Parameter(names = "-foo", variableArity = true)
+  public List<String> foo = new ArrayList<>();
+
+  public static void main(String... argv) {
+    VariableArityCommand ac = new VariableArityCommand();
+
+    JCommander.newBuilder().addObject(ac).build().parse(argv);
+    ac.run();
+  }
+
+  public void run() {
+    for (String one: foo)
+      System.out.println(one);
+  }
+}
