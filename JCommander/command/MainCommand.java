2e0b40eae91e3a08c1284bc4bcd67b7bd15249c6
fernal73 <linus.fernandes@gmail.com>
2019-11-12 10:17:23 +0530

    Main and Separator commands

---
+++ JCommander/command/MainCommand.java
@@ -0,0 +1,26 @@
+package command;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainCommand {
+  @Parameter(description = "Files")
+  private List<String> files = new ArrayList<>();
+
+  @Parameter(names = "-debug", description = "Debugging level")
+  private Integer debug = 1;
+
+  public static void main(String... argv) {
+    MainCommand fc = new MainCommand();
+    JCommander.newBuilder().addObject(fc).build().parse(argv);
+    fc.run();
+  }
+
+  public void run() {
+    System.out.printf("%d %n", debug);
+    for (String file: files)
+      System.out.println(file);
+  }
+}
---
+++ JCommander/command/SeparatorCommand.java
@@ -0,0 +1,21 @@
+package command;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+@Parameters(separators = "=")
+public class SeparatorCommand {
+  @Parameter(names = "-level")
+  private Integer level = 2;
+
+  public static void main(String... argv) {
+    SeparatorCommand fc = new SeparatorCommand();
+    JCommander.newBuilder().addObject(fc).build().parse(argv);
+    fc.run();
+  }
+
+  public void run() {
+    System.out.printf("%d %n", level);
+  }
+}
