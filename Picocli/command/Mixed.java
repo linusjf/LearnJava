4e41c4e3de28a64b18abeee32d966114f1ada556
fernal73 <linus.fernandes@gmail.com>
2019-11-16 19:34:46 +0530

    Added more classes

---
+++ Picocli/command/Mixed.java
@@ -0,0 +1,25 @@
+package command;
+
+import java.util.Arrays;
+import java.util.List;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+public class Mixed {
+  @Parameters
+  List<String> positional;
+
+  @Option(names = "-o")
+  List<String> options;
+
+  public static void main(String... args) {
+    Mixed mixed = new Mixed();
+    new CommandLine(mixed).parseArgs(args);
+
+    assert mixed.positional.equals(
+        Arrays.asList("param0", "param1", "param2", "param3"));
+    assert mixed.options.equals(Arrays.asList("AAA", "BBB"));
+    System.out.println("asserts cleared");
+  }
+}
--- Picocli/command/PositionalParameters.java
+++ Picocli/command/PositionalParameters.java
@@ -1,32 +1,37 @@
 package command;
 
-import picocli.CommandLine;
-import picocli.CommandLine.Parameters;
-import java.util.Arrays;
-import java.util.List;
 import java.io.File;
 import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.List;
+import picocli.CommandLine;
+import picocli.CommandLine.Parameters;
 
 public class PositionalParameters {
-    // "hidden": don't show this parameter in usage help message
-    @Parameters(hidden = true)  
-    List<String> allParameters; 
-    // no "index" attribute: captures _all_ arguments (as Strings)
+  // "hidden": don't show this parameter in usage help message
+  @Parameters(hidden = true)
+  List<String> allParameters;
+  // no "index" attribute: captures _all_ arguments (as Strings)
 
-    @Parameters(index = "0")
-    InetAddress host;
-    @Parameters(index = "1")
-    int port;
-    @Parameters(index = "2..*")
-    File[] files;
+  @Parameters(index = "0")
+  InetAddress host;
 
-    public static void main(String... args) {
-PositionalParameters params = 
-  CommandLine.populateCommand(new PositionalParameters(), args);
-assert params.host.getHostName().equals("localhost");
-assert params.port == 12_345;
-assert Arrays.equals(params.files, new File[] {new File("file1.txt"), new File("file2.txt")});
-assert params.allParameters.equals(Arrays.asList("localhost", "12345", "file1.txt", "file2.txt"));
-System.out.println("Cleared all asserts");
-    }
+  @Parameters(index = "1")
+  int port;
+
+  @Parameters(index = "2..*")
+  File[] files;
+
+  public static void main(String... args) {
+    PositionalParameters params =
+        CommandLine.populateCommand(new PositionalParameters(), args);
+    assert params.host.getHostName().equals("localhost");
+    assert params.port == 12_345;
+    assert Arrays.equals(
+        params.files,
+        new File[] {new File("file1.txt"), new File("file2.txt")});
+    assert params.allParameters.equals(
+        Arrays.asList("localhost", "12345", "file1.txt", "file2.txt"));
+    System.out.println("Cleared all asserts");
+  }
 }
