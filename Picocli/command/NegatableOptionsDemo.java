a28d9f586a8429b6068aa01956f92c21987a4d45
fernal73 <linus.fernandes@gmail.com>
2019-11-16 14:08:44 +0530

    Added negatable command

---
+++ Picocli/command/NegatableOptionsDemo.java
@@ -0,0 +1,29 @@
+package command;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "negatable-options-demo")
+class NegatableOptionsDemo {
+  @Option(names = "--verbose", negatable = true)
+  boolean verbose;
+
+  @Option(names = "-XX:+PrintGCDetails", negatable = true)
+  boolean printGCDetails;
+
+  @Option(names = "-XX:-UseG1GC", negatable = true)
+  boolean useG1GC = true;
+
+  @Option(names = "--no-backup",
+          negatable = true,
+          description = "Make a backup. True by default.")
+  boolean backup = true;
+
+  public static void main(String... args) {
+
+    NegatableOptionsDemo nod = new NegatableOptionsDemo();
+    CommandLine cl = new CommandLine(nod);
+    cl.usage(System.err);
+  }
+}
