756cdc1aeb71f7d72f74ae6907445031c7ede611
fernal73 <linus.fernandes@gmail.com>
2019-11-29 16:43:33 +0530

    fixed pmd errors

--- Networks/networking/IntgenClient.java
+++ Networks/networking/IntgenClient.java
@@ -19,30 +19,40 @@ public final class IntgenClient {
       System.out.println("Usage: java IntgenClient host [port]");
       return;
     }
-    int port;
-    try {
-      port = Integer.parseInt(args[1]);
-    } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
-      port = DEFAULT_PORT;
-    }
+    int port = getPort(args);
     try {
       SocketAddress address = new InetSocketAddress(args[0], port);
       SocketChannel client = SocketChannel.open(address);
       ByteBuffer buffer = ByteBuffer.allocate(4);
       IntBuffer view = buffer.asIntBuffer();
-      for (int expected = 0;; expected++) {
-        client.read(buffer);
-        int actual = view.get();
-        buffer.clear();
-        view.rewind();
-        if (actual != expected) {
-          System.err.println("Expected " + expected + "; was " + actual);
-          break;
-        }
-        System.out.println(actual);
-      }
+      printInts(buffer, view, client);
     } catch (IOException ex) {
       System.err.println(ex);
+    }
+  }
+
+  @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+  private static void printInts(ByteBuffer buffer,
+                                IntBuffer view,
+                                SocketChannel client) throws IOException {
+    for (int expected = 0;; expected++) {
+      client.read(buffer);
+      int actual = view.get();
+      buffer.clear();
+      view.rewind();
+      if (actual != expected) {
+        System.err.println("Expected " + expected + "; was " + actual);
+        break;
+      }
+      System.out.println(actual);
+    }
+  }
+
+  private static int getPort(String... args) {
+    try {
+      return Integer.parseInt(args[1]);
+    } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
+      return DEFAULT_PORT;
     }
   }
 }
