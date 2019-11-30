70e12508a68d3dc16489cc3db402ad6f37ca9c90
fernal73 <linus.fernandes@gmail.com>
2019-11-30 14:54:21 +0530

    fixed pmd errors

--- Networks/networking/PooledDaytimeServer.java
+++ Networks/networking/PooledDaytimeServer.java
@@ -16,17 +16,22 @@ public final class PooledDaytimeServer {
 
   public static void main(String[] args) {
     try (ServerSocket server = new ServerSocket(PORT)) {
-    ExecutorService pool = Executors.newFixedThreadPool(50);
-      while (true) {
-        try {
-          Socket connection = server.accept();
-          pool.submit(new DaytimeTask(connection));
-        } catch (IOException ex) {
-          System.err.println(ex.getMessage());
-        }
-      }
+      ExecutorService pool = Executors.newFixedThreadPool(50);
+      acceptAndSubmit(pool, server);
     } catch (IOException ex) {
       System.err.println("Couldn't start server");
+    }
+  }
+
+  private static void acceptAndSubmit(ExecutorService pool,
+                                      ServerSocket server) {
+    while (true) {
+      try {
+        Socket connection = server.accept();
+        pool.submit((Callable<Void>)new DaytimeTask(connection));
+      } catch (IOException ex) {
+        System.err.println(ex.getMessage());
+      }
     }
   }
 }
