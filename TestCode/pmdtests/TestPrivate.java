8557e2a5d1dc786ec4d320367fe98b02cd4a9017
fernal73 <linus.fernandes@gmail.com>
2019-12-02 08:48:00 +0530

    More test code

---
+++ TestCode/pmdtests/TestPrivate.java
@@ -0,0 +1,12 @@
+package pmdtests;
+
+public class TestPrivate<T> {
+    
+  protected Object getProtected(final T bean) {
+        return getPrivate(bean);
+    }
+
+    private Object getPrivate(final Object bean) {
+        return bean;
+    }
+}
---
+++ TestCode/pmdtests/TestString.java
@@ -0,0 +1,16 @@
+package pmdtests;
+
+public class TestString {
+
+  private void startProcess() {
+   this.createEvent(a,new Object().toString(),c);
+}
+
+// method invoked 
+private void createEvent(
+    String a, String b, String c) {
+  // do some logic
+  System.out.println("creating event...");
+}
+}
+
