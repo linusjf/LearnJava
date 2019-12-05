18316e0016840d8bea1d7fd7ca346f208ee1b5fa
fernal73 <linus.fernandes@gmail.com>
2019-12-05 19:54:10 +0530

    More spotbugs suppressions

--- Puzzles/javapuzzles/WeirdLoop.java
+++ Puzzles/javapuzzles/WeirdLoop.java
@@ -4,9 +4,9 @@ public class WeirdLoop {
   private static final int i = 99;
 
   public static void main(String[] args) {
-    for (int i = 0; i < 100; i++) {  // line n1
+    for (int i = 0; i < 100; i++) {  
       System.out.print(i);
-      i++;  // line n2
+      i++;
       break;
     }
     System.out.print(i);
