17fe7b032713f7a6e0eaa436e552909fcfed99ae
fernal73 <linus.fernandes@gmail.com>
2019-12-05 14:14:46 +0530

    Add TextEditor

---
+++ Puzzles/javapuzzles/TextEditor.java
@@ -0,0 +1,31 @@
+package javapuzzles;
+
+public class TextEditor {
+    static class Char { // line 1
+        char c;
+   
+        public Char(char c) {
+            this.c = c;
+        }
+        
+        public void print() {
+            System.out.print(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        typeText(new Char('A'));
+        
+        typeText(new Char('B') { // line 2
+            public void print() {
+                System.out.print("[");
+                super.print();  // line 3
+                System.out.print("]");
+            }
+        });
+    }
+ 
+    private static void typeText(Char c) {
+        c.print();
+    }
+}
