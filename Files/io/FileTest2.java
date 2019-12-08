52871b50a0ff5b91cb9e6e3ee983652ec6e2633b
fernal73 <linus.fernandes@gmail.com>
2019-12-09 03:34:17 +0530

    Fix spotbugs error

--- Files/io/FileTest2.java
+++ Files/io/FileTest2.java
@@ -10,28 +10,30 @@ public enum FileTest2 {
   ;
 
   public static void main(String[] args) {
-    String fileName;
-    int mark;
+    PrintWriter output = null;
     try (Scanner input =
              new Scanner(System.in, StandardCharsets.UTF_8.name())) {
       System.out.print("Enter file name: ");
-      fileName = input.nextLine();
-      PrintWriter output =
-          new PrintWriter(new File(fileName), StandardCharsets.UTF_8.name());
-      System.out.println("Ten marks needed.\n");
+    String  fileName = input.nextLine();
+    inputMarks(input,fileName);
+    } catch (IOException ioe) {
+
+      System.err.println(ioe.getMessage());
+    }
+  }
+
+  private static void inputMarks(Scanner input,String fileName) throws IOException {
+      try (PrintWriter output =
+          new PrintWriter(new File(fileName), StandardCharsets.UTF_8.name())) {
+      System.out.printf("Ten marks needed.%n");
       for (int i = 1; i < 11; i++) {
         if (input.hasNext()) {
           System.out.print("Enter mark " + i + ": ");
-          mark = input.nextInt();
-
-          // * Should really validate entry! *
+        int  mark = input.nextInt();
           output.println(mark);
           output.flush();
         }
       }
-      output.close();
-    } catch (IOException ioe) {
-      System.err.println(ioe);
-    }
+  }
   }
 }
