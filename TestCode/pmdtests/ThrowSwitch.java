dda93ed2b3c6ef71cb31ba92c313da69b0e79924
fernal73 <linus.fernandes@gmail.com>
2019-12-11 17:03:23 +0530

    Added new test example

---
+++ TestCode/pmdtests/ThrowSwitch.java
@@ -0,0 +1,31 @@
+package pmdtests;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class ThrowSwitch {
+
+  private ThrowSwitch() {
+    throw new IllegalStateException("Private constructor");
+  }
+
+  @SuppressWarnings({"fallthrough", "PMD.MissingBreakInSwitch"})
+  public static void main(String... args) {
+    int errCode = getSimulatedErrorCode();
+
+    switch (errCode) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+        throw new AssertionError("Error code: " + errCode);
+      default:
+        System.exit(0);
+    }
+  }
+
+  public static int getSimulatedErrorCode() {
+    return ThreadLocalRandom.current().nextInt(0, 6);
+  }
+}
+
