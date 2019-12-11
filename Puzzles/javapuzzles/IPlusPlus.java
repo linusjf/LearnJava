551e2f67e127986a4d34701d1da3ffa7080a84d1
fernal73 <linus.fernandes@gmail.com>
2019-12-11 10:26:30 +0530

    Fix pmd errors

--- Puzzles/javapuzzles/IPlusPlus.java
+++ Puzzles/javapuzzles/IPlusPlus.java
@@ -1,12 +1,14 @@
 package javapuzzles;
 
-public class IPlusPlus {
+public enum IPlusPlus {
+  ;
 
   public static void main(String... args) {
     for (int i = 0; i < 2; i = iplusplus(i))
       System.out.println("t" + i);
   }
 
+  @SuppressWarnings("PMD.AvoidReassigningParameters")
   private static int iplusplus(int i) {
     System.out.println("t" + i);
     return ++i;
