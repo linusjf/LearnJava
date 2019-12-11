c4afc2a6493648d8be6c67b300936cbfdaa3066e
fernal73 <linus.fernandes@gmail.com>
2019-12-11 16:46:08 +0530

    Fix pmd error. Format.

--- Switch/trifunction/FunctionalStatistics.java
+++ Switch/trifunction/FunctionalStatistics.java
@@ -7,8 +7,9 @@ import java.util.Objects;
 import player.TennisPlayer;
 
 public final class FunctionalStatistics {
-  private static final Map<String, TriFunction<TennisPlayer, Period, String, String>> STATISTICS =
-      new HashMap<>();
+  private static final
+      Map<String, TriFunction<TennisPlayer, Period, String, String>>
+          STATISTICS = new HashMap<>();
 
   private FunctionalStatistics() {
     throw new AssertionError();
@@ -20,8 +21,10 @@ public final class FunctionalStatistics {
     STATISTICS.put("BACKHAND", Statistics::computeBackhandTrend);
   }
 
-  public static String computeTrend(
-      TennisPlayer tennisPlayer, Period period, String owner, String trend) {
+  public static String computeTrend(TennisPlayer tennisPlayer,
+                                    Period period,
+                                    String owner,
+                                    String trend) {
     // clang-format off
     TriFunction<TennisPlayer, Period, String, String> function =
         Objects.requireNonNull(STATISTICS.get(trend), "Invalid trend type: " + trend);
