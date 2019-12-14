56532f840e1ce95004a7fb2c9d7c91149bbfae0f
fernal73 <linus.fernandes@gmail.com>
2019-12-14 17:35:24 +0530

    Fix law of demeter. Reassigning vriables also flags a law of Demeter violation

--- AbstractFactory/com/javacodegeeks/patterns/abstractfactorypattern/TestAbstractFactoryPattern.java
+++ AbstractFactory/com/javacodegeeks/patterns/abstractfactorypattern/TestAbstractFactoryPattern.java
@@ -3,15 +3,16 @@ package com.javacodegeeks.patterns.abstractfactorypattern;
 public enum TestAbstractFactoryPattern {
   ;
 
+static final AbstractParserFactory PARSER_FACTORY_NY = ParserFactoryProducer.getFactory("NYFactory");
+static final AbstractParserFactory PARSER_FACTORY_TW = ParserFactoryProducer.getFactory("TWFactory");
+
   public static void main(String[] args) {
-    AbstractParserFactory parserFactory = ParserFactoryProducer.getFactory("NYFactory");
-    XMLParser parser = parserFactory.getParserInstance("NYORDER");
+    XMLParser parser = PARSER_FACTORY_NY.getParserInstance("NYORDER");
     String msg = parser.parse();
     System.out.println(msg);
     System.out.println("************************************");
-    parserFactory = ParserFactoryProducer.getFactory("TWFactory");
-    parser = parserFactory.getParserInstance("TWFEEDBACK");
-    msg = parser.parse();
+    XMLParser parser2 = PARSER_FACTORY_TW.getParserInstance("TWFEEDBACK");
+    msg = parser2.parse();
     System.out.println(msg);
   }
 }
