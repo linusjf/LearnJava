3aba5a7f60c8839c525165096868c60567282022
fernal73 <linus.fernandes@gmail.com>
2019-12-14 20:05:35 +0530

    suppress for law of demeter false positive. abstract method result flagged as object created elsewhere

--- FactoryMethod/com/javacodegeeks/patterns/factorymethodpattern/DisplayService.java
+++ FactoryMethod/com/javacodegeeks/patterns/factorymethodpattern/DisplayService.java
@@ -7,11 +7,13 @@ package com.javacodegeeks.patterns.factorymethodpattern;
  * @version 1.0
  */
 public abstract class DisplayService {
+
+
   /** Describe <code>display</code> method here. */
+  @SuppressWarnings("PMD.LawOfDemeter")
   public void display() {
     XMLParser parser = getParser();
-    String msg = parser.parse();
-    System.out.println(msg);
+    System.out.println(parser.parse());
   }
 
   /**
