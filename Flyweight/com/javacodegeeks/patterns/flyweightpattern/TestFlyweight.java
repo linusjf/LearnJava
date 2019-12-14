47ea69e68565cbb58c904823b06fe9c7cdf3dfe5
fernal73 <linus.fernandes@gmail.com>
2019-12-14 14:17:05 +0530

    Suppressed for Law of Demeter. Converting factory calls to static members

--- Flyweight/com/javacodegeeks/patterns/flyweightpattern/TestFlyweight.java
+++ Flyweight/com/javacodegeeks/patterns/flyweightpattern/TestFlyweight.java
@@ -5,6 +5,10 @@ public enum TestFlyweight {
   ;
   private static final String ASTERIXES = "*************************";
 
+    private static Platform platformC = PlatformFactory.getPlatformInstance("C");
+    private static Platform platformJava = PlatformFactory.getPlatformInstance("Java");
+    private static Platform platformRuby = PlatformFactory.getPlatformInstance("Ruby");
+
   /**
    * Main class.
    *
@@ -13,24 +17,19 @@ public enum TestFlyweight {
   public static void main(String[] args) {
     Code code = new Code();
     code.setCode("C Code...");
-    Platform platform = PlatformFactory.getPlatformInstance("C");
-    platform.execute(code);
+    platformC.execute(code);
     System.out.println(ASTERIXES);
     code.setCode("C Code2...");
-    platform = PlatformFactory.getPlatformInstance("C");
-    platform.execute(code);
+    platformC.execute(code);
     System.out.println(ASTERIXES);
     code.setCode("JAVA Code...");
-    platform = PlatformFactory.getPlatformInstance("JAVA");
     code.setCode("JAVA Code2...");
-    platform.execute(code);
+    platformJava.execute(code);
     System.out.println(ASTERIXES);
     code.setCode("RUBY Code...");
-    platform = PlatformFactory.getPlatformInstance("RUBY");
-    platform.execute(code);
+    platformRuby.execute(code);
     System.out.println(ASTERIXES);
     code.setCode("RUBY Code2...");
-    platform = PlatformFactory.getPlatformInstance("RUBY");
-    platform.execute(code);
+    platformRuby.execute(code);
   }
 }
