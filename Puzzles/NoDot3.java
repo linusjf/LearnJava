e2dbd82051c670a734e9a8a74e3ed773f1f2072f
fernal73 <linus.fernandes@gmail.com>
2019-12-19 06:02:48 +0530

    Added another one:

--- Puzzles/NoDot3.java
+++ Puzzles/NoDot3.java
@@ -1,5 +1,5 @@
 class NoDot3 {
-  public static void main(String[] args) throws Exception {
-    throw new AssertionError("Hello, world!");
+  public static void main(String[] args) throws Throwable {
+    throw new Throwable("\rHello, world!                           ",null,true,false){};
 }
 }
