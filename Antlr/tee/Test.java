7f3dda4317518b704e3a425aa22b24a8eeb1ebcb
fernal73 <linus.fernandes@gmail.com>
2019-12-19 11:08:22 +0530

    Fixed for build. entries added

--- Antlr/tee/Test.java
+++ Antlr/tee/Test.java
@@ -1,8 +1,8 @@
 package tee;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -12,20 +12,20 @@ public enum Test {
 
   private static final Charset UTF_8 = StandardCharsets.UTF_8;
 
-public static void main(String[] args)  {
-  try {
-    // create a CharStream that reads from standard input
-CharStream input = CharStreams.fromStream(System.in,UTF_8);
-// create a lexer that feeds off of input CharStream
-TeeLexer lexer = new TeeLexer(input);
-// create a buffer of tokens pulled from the lexer
-CommonTokenStream tokens = new CommonTokenStream(lexer);
-// create a parser that feeds off the tokens buffer
-TeeParser parser = new TeeParser(tokens);
-// begin parsing at rule r
-parser.r();
-  } catch (IOException e) {
-    System.err.println(e.getMessage());
+  public static void main(String[] args) {
+    try {
+      // create a CharStream that reads from standard input
+      CharStream input = CharStreams.fromStream(System.in, UTF_8);
+      // create a lexer that feeds off of input CharStream
+      TeeLexer lexer = new TeeLexer(input);
+      // create a buffer of tokens pulled from the lexer
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+      // create a parser that feeds off the tokens buffer
+      TeeParser parser = new TeeParser(tokens);
+      // begin parsing at rule r
+      parser.r();
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
   }
-}
 }
