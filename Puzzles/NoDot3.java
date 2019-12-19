6b79803f61c50702cd36117a909b9f864fbf0610
fernal73 <linus.fernandes@gmail.com>
2019-12-19 08:21:50 +0530

    entries

--- Puzzles/NoDot3.java
+++ Puzzles/NoDot3.java
@@ -1,6 +1,6 @@
 class NoDot3 {
   // using semi-colon, though
   public static void main(String[] args) throws Throwable {
-    throw new Throwable((char)13 + "Hello, world!                           ",null,true,false){};
+    throw new Throwable((char)13 + "Hello, world!                           ",null,true,false);
 }
 }
