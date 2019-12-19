1fe18111d455f48ea35fa3e696e84ff4eb6ef634
fernal73 <linus.fernandes@gmail.com>
2019-12-19 12:15:07 +0530

    suppressions

--- Puzzles/NoDot3.java
+++ Puzzles/NoDot3.java
@@ -1,3 +1,11 @@
+@SuppressWarnings({"checkstyle:packagedeclaration",
+                   "checkstyle:hideutilityclassconstructor",
+                   "checkstyle:magicnumber",
+                   "checkstyle:illegalthrows",
+                   "PMD.NoPackage",
+                   "PMD.DoubleBraceInitialization",
+                   "PMD.SignatureDeclareThrowsException",
+                   "PMD.UseUtilityClass"})
 class NoDot3 {
   // using semi-colon, though
   public static void main(String[] args) throws Throwable {
