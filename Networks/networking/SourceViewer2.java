41d28fe78fc1be247b3c2990876c1fca7f957cef
fernal73 <linus.fernandes@gmail.com>
2019-12-25 04:51:19 +0530

    Fix lod. csrules.xml

--- Networks/networking/SourceViewer2.java
+++ Networks/networking/SourceViewer2.java
@@ -13,6 +13,9 @@ import java.nio.charset.StandardCharsets;
 public enum SourceViewer2 {
   ;
 
+  private static final String UTF_8 =
+    StandardCharsets.UTF_8.name();
+
   @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
   public static void main(String[] args) {
     if (args.length > 0) {
@@ -22,9 +25,11 @@ public enum SourceViewer2 {
         URLConnection uc = u.openConnection();
         try (InputStream raw = uc.getInputStream();
             InputStream buffer = new BufferedInputStream(raw);
-            Reader reader = new InputStreamReader(buffer, StandardCharsets.UTF_8.name()); ) {
+            Reader reader = new InputStreamReader(buffer, 
+              UTF_8); ) {
           int c;
-          while ((c = reader.read()) != -1) System.out.print((char) c);
+          while ((c = reader.read()) != -1) 
+            System.out.print((char) c);
         }
       } catch (MalformedURLException ex) {
         System.err.println(args[0] + " is not a parseable URL");
