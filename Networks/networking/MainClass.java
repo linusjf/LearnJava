41934928343a23cfa6f3df6096e69ea27035c49d
fernal73 <linus.fernandes@gmail.com>
2019-12-24 06:23:02 +0530

    Fix lod

--- Networks/networking/MainClass.java
+++ Networks/networking/MainClass.java
@@ -10,24 +10,27 @@ import javax.net.ssl.SSLServerSocketFactory;
 
 public final class MainClass {
 
-  private static String UTF_8 =
+  private static final String UTF_8 =
     StandardCharsets.UTF_8.name();
 
   private MainClass() {
     throw new IllegalStateException("Private constructor");
   }
 
+  @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
   public static void main(String... args) {
     try {
       SSLServerSocketFactory ssf =
           (SSLServerSocketFactory)
               SSLServerSocketFactory.getDefault();
-      SSLServerSocket ss =
-          (SSLServerSocket)ssf.createServerSocket(5432);
-      ss.setNeedClientAuth(true);
-      ss.setEnabledCipherSuites(new String[] {
-          "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256"});
-      ss.setEnabledProtocols(new String[] {"TLSv1.2"});
+      SSLServerSocket ss = createServerSocket(ssf);
+      acceptConnections(ss);
+    } catch (IOException ioe) {
+      System.err.println(ioe.getMessage());
+    }
+  }
+
+  private static void acceptConnections(SSLServerSocket ss) throws IOException {
       while (true) {
         try (
         Socket socket = ss.accept();
@@ -39,8 +42,15 @@ public final class MainClass {
         out.println("Hello World!");
                 }
       }
-    } catch (IOException ioe) {
-      System.err.println(ioe.getMessage());
-    }
+  }
+
+  private static SSLServerSocket createServerSocket(SSLServerSocketFactory ssf) throws IOException {
+      SSLServerSocket ss =
+          (SSLServerSocket)ssf.createServerSocket(5432);
+      ss.setNeedClientAuth(true);
+      ss.setEnabledCipherSuites(new String[] {
+          "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256"});
+      ss.setEnabledProtocols(new String[] {"TLSv1.2"});
+      return ss;
   }
 }
