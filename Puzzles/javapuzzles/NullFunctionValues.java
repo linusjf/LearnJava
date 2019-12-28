60c4a8501aaea202a3819fda8f84f74619fb6893
fernal73 <linus.fernandes@gmail.com>
2019-12-28 08:00:14 +0530

    Updated

--- Puzzles/javapuzzles/NullFunctionValues.java
+++ Puzzles/javapuzzles/NullFunctionValues.java
@@ -17,11 +17,18 @@ public enum NullFunctionValues {
 
     System.out.println(map);
     for (int i = 0; i < 37; i++)
-      map.merge("Batman", "Hulk", (s1, s2) -> s1);
+      map.merge("Batman", "Hulk", (s1, s2) -> s2);
 
     System.out.println(map);
     for (int i = 0; i < 42; i++)
-      map.merge("Batman", "Hulk", (s1, s2) -> s1);
+      map.merge("Batman", "Hulk", (s1, s2) -> s2);
     System.out.println(map);
+    
+    for (int i = 0; i < 42; i++) {
+      map.merge("Batman", "Hulk", (oldValue, newValue) -> newValue);
+      if (i % 2 == 0)
+      map.merge("Batman", "The Thing", (oldValue, newValue) -> newValue);
+    System.out.println(i + ": " + map);
+    }
   }
 }
