3a30080e5bf8900397413cd9464c42d7ed1a3bc7
fernal73 <linus.fernandes@gmail.com>
2019-12-28 09:51:21 +0530

    Updated

--- Puzzles/javapuzzles/NullFunctionValues.java
+++ Puzzles/javapuzzles/NullFunctionValues.java
@@ -6,48 +6,52 @@ import java.util.concurrent.ConcurrentHashMap;
 public enum NullFunctionValues {
   ;
 
+  private static final String HULK = "Hulk";
+  private static final String BATMAN = "Batman";
+  private static final String THING = "The Thing";
+
   public static void main(String... args) {
     Map<String, String> map = new ConcurrentHashMap<>();
 
     System.out.println("Returning map.get(s1):");
     for (int i = 0; i < 42; i++)
-      map.merge("Batman", "Hulk", (s1, s2) -> map.get(s1));
+      map.merge(BATMAN, HULK, (s1, s2) -> map.get(s1));
 
     System.out.println(map);
     for (int i = 0; i < 37; i++)
-      map.merge("Batman", "Hulk", (s1, s2) -> map.get(s1));
+      map.merge(BATMAN, HULK, (s1, s2) -> map.get(s1));
 
     System.out.println(map);
-    
-     map.clear();
+
+    map.clear();
     System.out.println("Returning null directly:");
     for (int i = 0; i < 42; i++)
-      map.merge("Batman", "Hulk", (s1, s2) -> null);
+      map.merge(BATMAN, HULK, (s1, s2) -> null);
 
     System.out.println(map);
     for (int i = 0; i < 37; i++)
-      map.merge("Batman", "Hulk", (s1, s2) -> null);
+      map.merge(BATMAN, HULK, (s1, s2) -> null);
 
     System.out.println(map);
-    
-     map.clear();
+
+    map.clear();
     System.out.println("Returning s2 directly:");
     for (int i = 0; i < 42; i++)
-      map.merge("Batman", "Hulk", (s1, s2) -> s2);
+      map.merge(BATMAN, HULK, (s1, s2) -> s2);
     System.out.println(map);
     for (int i = 0; i < 37; i++)
-      map.merge("Batman", "Hulk", (s1, s2) -> s2);
+      map.merge(BATMAN, HULK, (s1, s2) -> s2);
 
     System.out.println(map);
-    
-     map.clear();
+
+    map.clear();
     System.out.println("Alternating values:");
     for (int i = 0; i < 42; i++) {
       if (i % 2 == 0)
-      map.merge("Batman", "The Thing", (oldValue, newValue) -> newValue);
+        map.merge(BATMAN, THING, (oldValue, newValue) -> newValue);
       else
-      map.merge("Batman", "Hulk", (oldValue, newValue) -> newValue);
-    System.out.println(i + ": " + map);
+        map.merge(BATMAN, HULK, (oldValue, newValue) -> newValue);
+      System.out.println(i + ": " + map);
     }
   }
 }
