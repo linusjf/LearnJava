4e589bb09056597f99618948a3ded3a671684f7a
fernal73 <linus.fernandes@gmail.com>
2019-12-29 18:16:45 +0530

    Added expr version 2

---
+++ Antlr/exprv2/Exprv2BaseListener.java
@@ -0,0 +1,102 @@
+// Generated from Exprv2.g4 by ANTLR 4.7.2
+package exprv2;
+
+import java.util.HashMap;
+
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link Exprv2Listener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class Exprv2BaseListener implements Exprv2Listener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProg(Exprv2Parser.ProgContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProg(Exprv2Parser.ProgContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStat(Exprv2Parser.StatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStat(Exprv2Parser.StatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpr(Exprv2Parser.ExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpr(Exprv2Parser.ExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMultExpr(Exprv2Parser.MultExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMultExpr(Exprv2Parser.MultExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAtom(Exprv2Parser.AtomContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAtom(Exprv2Parser.AtomContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ Antlr/exprv2/Exprv2Listener.java
@@ -0,0 +1,63 @@
+// Generated from Exprv2.g4 by ANTLR 4.7.2
+package exprv2;
+
+import java.util.HashMap;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link Exprv2Parser}.
+ */
+public interface Exprv2Listener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link Exprv2Parser#prog}.
+	 * @param ctx the parse tree
+	 */
+	void enterProg(Exprv2Parser.ProgContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link Exprv2Parser#prog}.
+	 * @param ctx the parse tree
+	 */
+	void exitProg(Exprv2Parser.ProgContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link Exprv2Parser#stat}.
+	 * @param ctx the parse tree
+	 */
+	void enterStat(Exprv2Parser.StatContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link Exprv2Parser#stat}.
+	 * @param ctx the parse tree
+	 */
+	void exitStat(Exprv2Parser.StatContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link Exprv2Parser#expr}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpr(Exprv2Parser.ExprContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link Exprv2Parser#expr}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpr(Exprv2Parser.ExprContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link Exprv2Parser#multExpr}.
+	 * @param ctx the parse tree
+	 */
+	void enterMultExpr(Exprv2Parser.MultExprContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link Exprv2Parser#multExpr}.
+	 * @param ctx the parse tree
+	 */
+	void exitMultExpr(Exprv2Parser.MultExprContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link Exprv2Parser#atom}.
+	 * @param ctx the parse tree
+	 */
+	void enterAtom(Exprv2Parser.AtomContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link Exprv2Parser#atom}.
+	 * @param ctx the parse tree
+	 */
+	void exitAtom(Exprv2Parser.AtomContext ctx);
+}
