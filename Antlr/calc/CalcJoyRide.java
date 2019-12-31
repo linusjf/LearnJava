79957c01dbc13c25969540659fe753a33914b06f
fernal73 <linus.fernandes@gmail.com>
2019-12-31 13:34:42 +0530

    Test harness for calc

---
+++ Antlr/calc/CalcJoyRide.java
@@ -0,0 +1,48 @@
+package calc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+/**
+ * Excerpted from "The Definitive ANTLR Reference", published by The Pragmatic Bookshelf. Copyrights
+ * apply to this code. It may not be used to create training material, courses, books, articles, and
+ * the like. Contact us if you are in doubt. We make no guarantees that this code is fit for any
+ * purpose. Visit http://www.pragmaticprogrammer.com/titles/tpantlr for more book information.
+ */
+public enum CalcJoyRide {
+  ;
+  private static final Charset UTF_8 = StandardCharsets.UTF_8;
+
+  @SuppressWarnings("PMD.LawOfDemeter")
+  public static void main(String[] args) {
+    String inputFile = null;
+    if (args.length > 0)
+      inputFile = args[0];
+    try (InputStream is = inputFile == null
+                              ? System.in
+                              : Files.newInputStream(Paths.get(inputFile));) {
+      // Create an input character stream from standard in
+      CharStream input = CharStreams.fromStream(is, UTF_8);
+      // Create an ExprLexer that feeds from that stream
+      CalcLexer lexer = new CalcLexer(input);
+      // Create a stream of tokens fed by the lexer
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+      // Create a parser that feeds off the token stream
+      CalcParser parser = new CalcParser(tokens);
+      ParseTree tree = parser.prog();
+      // begin parsing at init rule
+      System.out.println(tree.toStringTree(parser));
+      // printLISP-styletree
+    } catch (IOException ioe) {
+      System.err.println(ioe.getMessage());
+    }
+  }
+}
