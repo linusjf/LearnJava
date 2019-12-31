1f2d42a6f595e2ba4561c5c1bd2f9a18693a20a0
fernal73 <linus.fernandes@gmail.com>
2019-12-31 10:58:29 +0530

    Added calc grammar

---
+++ Antlr/calc/CalcBaseListener.java
@@ -0,0 +1,75 @@
+// Generated from Calc.g4 by ANTLR 4.7.2
+package calc;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link CalcListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class CalcBaseListener implements CalcListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProg(CalcParser.ProgContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProg(CalcParser.ProgContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStat(CalcParser.StatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStat(CalcParser.StatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpr(CalcParser.ExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpr(CalcParser.ExprContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ Antlr/calc/CalcListener.java
@@ -0,0 +1,40 @@
+// Generated from Calc.g4 by ANTLR 4.7.2
+package calc;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link CalcParser}.
+ */
+public interface CalcListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link CalcParser#prog}.
+	 * @param ctx the parse tree
+	 */
+	void enterProg(CalcParser.ProgContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CalcParser#prog}.
+	 * @param ctx the parse tree
+	 */
+	void exitProg(CalcParser.ProgContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link CalcParser#stat}.
+	 * @param ctx the parse tree
+	 */
+	void enterStat(CalcParser.StatContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CalcParser#stat}.
+	 * @param ctx the parse tree
+	 */
+	void exitStat(CalcParser.StatContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link CalcParser#expr}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpr(CalcParser.ExprContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CalcParser#expr}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpr(CalcParser.ExprContext ctx);
+}
