fa659b2566494946d69665cbb359e43a79a9f669
fernal73 <linus.fernandes@gmail.com>
2019-12-31 18:30:27 +0530

    Fixed for static analysis violations

---
+++ Antlr/labeledexpr/Calc.java
@@ -0,0 +1,46 @@
+package labeledexpr;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+/**
+ * Excerpted from "The Definitive ANTLR Reference", published by The Pragmatic Bookshelf. Copyrights
+ * apply to this code. It may not be used to create training material, courses, books, articles, and
+ * the like. Contact us if you are in doubt. We make no guarantees that this code is fit for any
+ * purpose. Visit http://www.pragmaticprogrammer.com/titles/tpantlr for more book information.
+ */
+public enum Calc {
+  ;
+  private static final Charset UTF_8 = StandardCharsets.UTF_8;
+
+  @SuppressWarnings({"PMD.LawOfDemeter", "PMD.DataflowAnomalyAnalysis"})
+  public static void main(String[] args) {
+    String inputFile = args.length > 0 ? args[0] : null;
+    try (InputStream is = inputFile == null
+                              ? System.in
+                              : Files.newInputStream(Paths.get(inputFile));) {
+      // Create an input character stream from standard in
+      CharStream input = CharStreams.fromStream(is, UTF_8);
+      // Create an ExprLexer that feeds from that stream
+      LabeledExprLexer lexer = new LabeledExprLexer(input);
+      // Create a stream of tokens fed by the lexer
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+      // Create a parser that feeds off the token stream
+      LabeledExprParser parser = new LabeledExprParser(tokens);
+      ParseTree tree = parser.prog();
+      // begin parsing at init rule
+      EvalVisitorImpl eval = new EvalVisitorImpl();
+      eval.visit(tree);
+    } catch (IOException ioe) {
+      System.err.println(ioe.getMessage());
+    }
+  }
+}
---
+++ Antlr/labeledexpr/EvalVisitorImpl.java
@@ -0,0 +1,108 @@
+package labeledexpr;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * * Excerpted from "The Definitive ANTLR 4 Reference", published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, courses, books,
+ * articles, and the like. Contact us if you are in doubt. We make no guarantees that this code is
+ * fit for any purpose. Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book
+ * information. *
+ */
+public class EvalVisitorImpl extends LabeledExprBaseVisitor<Integer> {
+  /** 
+   * "memory" for our calculator; variable/value pairs go here.
+   **/
+  Map<String, Integer> memory = new HashMap<>();
+
+  /** 
+   * ID '=' expr NEWLINE.
+   **/
+  @Override
+  @SuppressWarnings("PMD.LawOfDemeter")
+  public Integer visitAssign(LabeledExprParser.AssignContext ctx) {
+    String id = ctx.ID().getText();  
+    // id is left-hand side of '='
+    int value = visit(ctx.expr());   
+    // compute value of expression on right
+    memory.put(id, value);           
+    // store it in our memory
+    return value;
+  }
+
+  /** 
+   * expr NEWLINE.
+   **/
+  @Override
+  public Integer visitPrintExpr(LabeledExprParser.PrintExprContext ctx) {
+    Integer value = visit(ctx.expr());  
+    // evaluate the expr child
+    System.out.println(value);          
+    // print the result
+    return 0;                           
+    // return dummy value
+  }
+
+  /** 
+   * INT.
+   **/
+  @Override
+  @SuppressWarnings("PMD.LawOfDemeter")
+  public Integer visitInt(LabeledExprParser.IntContext ctx) {
+    return Integer.valueOf(ctx.INT().getText());
+  }
+
+  /** 
+   * ID.
+   **/
+  @Override
+  @SuppressWarnings("PMD.LawOfDemeter")
+  public Integer visitId(LabeledExprParser.IdContext ctx) {
+    String id = ctx.ID().getText();
+    if (memory.containsKey(id))
+      return memory.get(id);
+    return 0;
+  }
+
+  /** 
+   * expr op=('*'|'/') expr.
+   **/
+  @Override
+  @SuppressWarnings("PMD.LawOfDemeter")
+  public Integer visitMulDiv(LabeledExprParser.MulDivContext ctx) {
+    int left = visit(ctx.expr(0));   
+    // get value of left subexpression
+    int right = visit(ctx.expr(1));  
+    // get value of right subexpression
+    if (ctx.op.getType() == LabeledExprParser.MUL)
+      return left * right;
+    return left / right;  
+    // must be DIV
+  }
+
+  /** 
+   * expr op=('+'|'-') expr.
+   **/
+  @Override
+  @SuppressWarnings("PMD.LawOfDemeter")
+  public Integer visitAddSub(LabeledExprParser.AddSubContext ctx) {
+    int left = visit(ctx.expr(0));   
+    // get value of left subexpression
+    int right = visit(ctx.expr(1));  
+    // get value of right subexpression
+    if (ctx.op.getType() == LabeledExprParser.ADD)
+      return left + right;
+    return left - right;  
+    // must be SUB
+  }
+
+  /** 
+   * '(' expr ')'.
+   **/
+  @Override
+  public Integer visitParens(LabeledExprParser.ParensContext ctx) {
+    return visit(ctx.expr());  
+    // return child expr's value
+  }
+}
