a7072c6a825915f54561c7970bd89ad7bc0a208a
fernal73 <linus.fernandes@gmail.com>
2019-12-31 18:52:06 +0530

    reworked pmd target.

--- Antlr/labeledexpr/EvalVisitorImpl.java
+++ Antlr/labeledexpr/EvalVisitorImpl.java
@@ -11,51 +11,41 @@ import java.util.Map;
  * information. *
  */
 public class EvalVisitorImpl extends LabeledExprBaseVisitor<Integer> {
-  /** 
-   * "memory" for our calculator; variable/value pairs go here.
-   **/
+  /** "memory" for our calculator; variable/value pairs go here. */
   Map<String, Integer> memory = new HashMap<>();
 
-  /** 
-   * ID '=' expr NEWLINE.
-   **/
+  /** ID '=' expr NEWLINE. */
   @Override
   @SuppressWarnings("PMD.LawOfDemeter")
   public Integer visitAssign(LabeledExprParser.AssignContext ctx) {
-    String id = ctx.ID().getText();  
+    String id = ctx.ID().getText();
     // id is left-hand side of '='
-    int value = visit(ctx.expr());   
+    int value = visit(ctx.expr());
     // compute value of expression on right
-    memory.put(id, value);           
+    memory.put(id, value);
     // store it in our memory
     return value;
   }
 
-  /** 
-   * expr NEWLINE.
-   **/
+  /** expr NEWLINE. */
   @Override
   public Integer visitPrintExpr(LabeledExprParser.PrintExprContext ctx) {
-    Integer value = visit(ctx.expr());  
+    Integer value = visit(ctx.expr());
     // evaluate the expr child
-    System.out.println(value);          
+    System.out.println(value);
     // print the result
-    return 0;                           
+    return 0;
     // return dummy value
   }
 
-  /** 
-   * INT.
-   **/
+  /** INT. */
   @Override
   @SuppressWarnings("PMD.LawOfDemeter")
   public Integer visitInt(LabeledExprParser.IntContext ctx) {
     return Integer.valueOf(ctx.INT().getText());
   }
 
-  /** 
-   * ID.
-   **/
+  /** ID. */
   @Override
   @SuppressWarnings("PMD.LawOfDemeter")
   public Integer visitId(LabeledExprParser.IdContext ctx) {
@@ -65,44 +55,38 @@ public class EvalVisitorImpl extends LabeledExprBaseVisitor<Integer> {
     return 0;
   }
 
-  /** 
-   * expr op=('*'|'/') expr.
-   **/
+  /** expr op=('*'|'/') expr. */
   @Override
   @SuppressWarnings("PMD.LawOfDemeter")
   public Integer visitMulDiv(LabeledExprParser.MulDivContext ctx) {
-    int left = visit(ctx.expr(0));   
+    int left = visit(ctx.expr(0));
     // get value of left subexpression
-    int right = visit(ctx.expr(1));  
+    int right = visit(ctx.expr(1));
     // get value of right subexpression
     if (ctx.op.getType() == LabeledExprParser.MUL)
       return left * right;
-    return left / right;  
+    return left / right;
     // must be DIV
   }
 
-  /** 
-   * expr op=('+'|'-') expr.
-   **/
+  /** expr op=('+'|'-') expr. */
   @Override
   @SuppressWarnings("PMD.LawOfDemeter")
   public Integer visitAddSub(LabeledExprParser.AddSubContext ctx) {
-    int left = visit(ctx.expr(0));   
+    int left = visit(ctx.expr(0));
     // get value of left subexpression
-    int right = visit(ctx.expr(1));  
+    int right = visit(ctx.expr(1));
     // get value of right subexpression
     if (ctx.op.getType() == LabeledExprParser.ADD)
       return left + right;
-    return left - right;  
+    return left - right;
     // must be SUB
   }
 
-  /** 
-   * '(' expr ')'.
-   **/
+  /** '(' expr ')'. */
   @Override
   public Integer visitParens(LabeledExprParser.ParensContext ctx) {
-    return visit(ctx.expr());  
+    return visit(ctx.expr());
     // return child expr's value
   }
 }
