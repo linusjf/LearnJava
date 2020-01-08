7c91496f372522a555172da5639a389273ad7798
fernal73 <linus.fernandes@gmail.com>
2020-01-08 12:10:17 +0530

    Fix spotbugs error

---
+++ Antlr/dot/DisplayTokens.java
@@ -0,0 +1,37 @@
+package dot;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+
+/**
+ * Excerpted from "The Definitive ANTLR Reference", published by The Pragmatic Bookshelf. Copyrights
+ * apply to this code. It may not be used to create training material, courses, books, articles, and
+ * the like. Contact us if you are in doubt. We make no guarantees that this code is fit for any
+ * purpose. Visit http://www.pragmaticprogrammer.com/titles/tpantlr for more book information.
+ */
+public enum DisplayTokens {
+  ;
+  private static final Logger LOGGER =
+      Logger.getLogger(DisplayTokens.class.getName());
+  private static final Charset UTF_8 = StandardCharsets.UTF_8;
+
+  @SuppressWarnings({"PMD.LawOfDemeter",
+                     "PMD.DataflowAnomalyAnalysis",
+                     "PMD.SystemPrintln"})
+  public static void
+  main(String[] args) {
+    try {
+      // Create an input character stream from standard in
+      CharStream input = CharStreams.fromStream(System.in, UTF_8);
+      // Create an ExprLexer that feeds from that stream
+      DOTLexer lexer = new DOTLexer(input);
+      System.out.println(lexer.getAllTokens());
+    } catch (IOException ioe) {
+      LOGGER.severe(ioe.getMessage());
+    }
+  }
+}
--- Antlr/graph/Vertex.java
+++ Antlr/graph/Vertex.java
@@ -1,6 +1,7 @@
 package graph;
 
 import java.util.Locale;
+import java.util.Objects;
 
 public class Vertex implements Comparable<Vertex> {
   private String label;
@@ -12,6 +13,18 @@ public class Vertex implements Comparable<Vertex> {
   @Override
   public int compareTo(Vertex o) {
     return this.getLabel().compareTo(o.getLabel());
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o instanceof Vertex)
+      return compareTo((Vertex)o) == 0;
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(label);
   }
 
   public String getLabel() {
