5feaa9d1586e81d8fc94d72cfc5dabe2bac7f18a
fernal73 <linus.fernandes@gmail.com>
2020-01-21 22:44:04 +0530

    Add new puzzle

---
+++ Puzzles/javapuzzles/JavaHungry.java
@@ -0,0 +1,19 @@
+package javapuzzles;
+
+public enum JavaHungry {
+  ;
+  @SuppressWarnings({"PMD.DoNotCallSystemExit",
+  "PMD.AvoidCatchingGenericException"})
+  public static void main(String[] args) {
+    try {
+      System.out.println("Inside try block ");
+      /* After executing below line
+      jvm terminates the program */
+      System.exit(0);
+    } catch (Exception e) {
+      System.err.println("Inside catch block");
+    } finally {
+      System.out.println("Inside finally block");
+    }
+  }
+}
