074c7a24ca1ad8c78f6d0c811b2d067e840f3705
fernal73 <linus.fernandes@gmail.com>
2020-01-22 21:33:54 +0530

    Suppress static violations

--- Puzzles/javapuzzles/Cretin.java
+++ Puzzles/javapuzzles/Cretin.java
@@ -4,18 +4,21 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class Cretin {
+public enum Cretin {
+  ;
+
+  @SuppressWarnings("PMD.LawOfDemeter")
   public static void main(String... args) throws InterruptedException {
     AtomicBoolean allowed = new AtomicBoolean(false);
     Thread[] threads = new Thread[1000];
     for (int i = 0; i < threads.length; i++) {
       threads[i] = new Thread(() -> {
-    Random random = ThreadLocalRandom.current(); 
-         allowed.set(random.nextBoolean());
+        Random random = ThreadLocalRandom.current();
+        allowed.set(random.nextBoolean());
       });
       threads[i].start();
     }
-    for (Thread thread : threads) 
+    for (Thread thread: threads)
       thread.join();
     System.out.println("Maurice is a cretin? " + allowed);
   }
--- Puzzles/javapuzzles/JavaHungry.java
+++ Puzzles/javapuzzles/JavaHungry.java
@@ -2,9 +2,11 @@ package javapuzzles;
 
 public enum JavaHungry {
   ;
+
   @SuppressWarnings({"PMD.DoNotCallSystemExit",
-  "PMD.AvoidCatchingGenericException"})
-  public static void main(String[] args) {
+                     "PMD.AvoidCatchingGenericException"})
+  public static void
+  main(String[] args) {
     try {
       System.out.println("Inside try block ");
       /* After executing below line
