efcabb1f33e3279f1d509a4c125de38c97e7a769
fernal73 <linus.fernandes@gmail.com>
2020-02-06 07:09:26 +0530

    Fix static analysis violations

--- Inlining/inlining/ConsecutiveNumbersSum.java
+++ Inlining/inlining/ConsecutiveNumbersSum.java
@@ -1,31 +1,28 @@
 package inlining;
 
-public enum ConsecutiveNumbersSum {
-  ;
- 
-    private long totalSum;
-    private int totalNumbers;
- 
-    public ConsecutiveNumbersSum(int totalNumbers) {
-        this.totalNumbers = totalNumbers;
-    }
- 
-    public long getTotalSum() {
-        totalSum = 0;
-        for (int i = 0; i < totalNumbers; i++) 
-            totalSum += i;
-        return totalSum;
-    }
+public class ConsecutiveNumbersSum {
 
-    private static long calculateSum(int n) {
+  private final int totalNumbers;
+
+  public ConsecutiveNumbersSum(int totalNumbers) {
+    this.totalNumbers = totalNumbers;
+  }
+
+  public long getTotalSum() {
+    long totalSum = 0;
+    for (int i = 0; i < totalNumbers; i++)
+      totalSum += i;
+    return totalSum;
+  }
+
+  private static long calculateSum(int n) {
     return new ConsecutiveNumbersSum(n).getTotalSum();
-}
+  }
 
-    public static void main(String... args) {
-      int numIterations = Integer.parseInt(args[0]);
-for (int i = 1; i < numIterations; i++) 
-    calculateSum(i);
-    }
-
-
+  @SuppressWarnings("PMD.LawOfDemeter")
+  public static void main(String... args) {
+    int numIterations = Integer.parseInt(args[0]);
+    for (int i = 1; i < numIterations; i++)
+      assert calculateSum(i) > 0;
+  }
 }
