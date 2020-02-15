962ba4365d6ff7636292e967ac3fc545fc32125d
fernal73 <linus.fernandes@gmail.com>
2020-02-15 07:34:45 +0530

    Fix pmd violations

--- Antlr/arrayinit/ShortToUnicodeString.java
+++ Antlr/arrayinit/ShortToUnicodeString.java
@@ -1,21 +1,12 @@
 package arrayinit;
 
 /**
- Excerpted from "The Definitive ANTLR 4
- Reference", published by The Pragmatic
- Bookshelf.
- Copyrights apply to this code. It may not be
- used to create training material, courses,
- books,
- articles, and the like. Contact us if you are in
- doubt. We make no guarantees that this code is
- fit for any purpose. Visit
-http://www.pragmaticprogrammer.com/titles/tpantlr2
-for more book
- information. Convert short array inits like
- {1,2,3} to "\u0001\u0002\u0003"
+ * Excerpted from "The Definitive ANTLR 4 Reference", published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, courses, books,
+ * articles, and the like. Contact us if you are in doubt. We make no guarantees that this code is
+ * fit for any purpose. Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book
+ * information. Convert short array inits like {1,2,3} to "\u0001\u0002\u0003"
  */
-
 @SuppressWarnings("PMD.SystemPrintln")
 public class ShortToUnicodeString extends ArrayInitBaseListener {
   /** Translate { to ". */
--- Antlr/properties/TestPropertyFile.java
+++ Antlr/properties/TestPropertyFile.java
@@ -1,17 +1,12 @@
 package properties;
 
-/**
- * Excerpted from "The Definitive ANTLR 4 Reference", published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, courses, books,
- * articles, and the like. Contact us if you are in doubt. We make no guarantees that this code is
- * fit for any purpose. Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book
- * information.
- */
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
+import java.util.logging.Logger;
 import org.antlr.v4.misc.OrderedHashMap;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -19,12 +14,24 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+/**
+ * Excerpted from "The Definitive ANTLR 4 Reference", published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, courses, books,
+ * articles, and the like. Contact us if you are in doubt. We make no guarantees that this code is
+ * fit for any purpose. Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book
+ * information.
+ */
 public enum TestPropertyFile {
   ;
 
-  public static class PropertyFileLoader extends PropertyFileBaseListener {
-    Map<String, String> props = new OrderedHashMap<String, String>();
+  private static final Logger LOGGER =
+    Logger.getLogger(TestPropertyFile.class.getName());
 
+  public static class PropertyFileLoader extends PropertyFileBaseListener {
+    Map<String, String> props = new OrderedHashMap<>();
+
+    @Override
+    @SuppressWarnings("PMD.LawOfDemeter")
     public void exitProp(PropertyFileParser.PropContext ctx) {
       String id = ctx.ID().getText();
       // prop : ID '=' STRING '\n' ;
@@ -33,14 +40,13 @@ public enum TestPropertyFile {
     }
   }
 
+  @SuppressWarnings("PMD.LawOfDemeter")
   public static void main(String[] args) {
-    String inputFile = null;
-    if (args.length > 0)
-      inputFile = args[0];
+    String inputFile = args.length > 0 ? args[0]:null;
     try {
-      InputStream is = System.in;
-      if (inputFile != null)
-        is = new FileInputStream(inputFile);
+      InputStream is = inputFile == null ? 
+        System.in:
+        Files.newInputStream(Paths.get(inputFile));
       CharStream input = CharStreams.fromStream(is, StandardCharsets.UTF_8);
       PropertyFileLexer lexer = new PropertyFileLexer(input);
       CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -54,9 +60,9 @@ public enum TestPropertyFile {
       // walk parse tree
       walker.walk(loader, tree);
       // print results
-      System.out.println(loader.props);
+      LOGGER.info(loader.props);
     } catch (IOException ioe) {
-      System.err.println(ioe.getMessage());
+      LOGGER.severe(ioe.getMessage());
     }
   }
 }
