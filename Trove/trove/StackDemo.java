e54cbb7c3c62dfdf24c314dba026e4fa8efa7430
fernal73 <linus.fernandes@gmail.com>
2020-02-26 20:51:20 +0530

    Add stackdemo to build config

--- Trove/trove/StackDemo.java
+++ Trove/trove/StackDemo.java
@@ -6,8 +6,8 @@ import java.util.logging.Logger;
 
 public enum StackDemo {
   ;
-  private final static Logger LOGGER = 
-    Logger.getLogger(StackDemo.class.getName());
+  private static final Logger LOGGER =
+      Logger.getLogger(StackDemo.class.getName());
 
   private static final int COUNT = 15_000;
 
@@ -25,41 +25,43 @@ public enum StackDemo {
     LOGGER.info(() -> "Time taken: " + (System.currentTimeMillis() - start2));
   }
 
-/**
- * Demonstrate Trove's Double Array Stack.
- *
- * Trove's TDoubleArrayStack allows access to its
- * contents via push, pop, and peek.
- */
-public static void demonstrateTroveDoubleArrayStack()
-{
-   final TDoubleArrayStack stack = new TDoubleArrayStack();
-   stack.push(15.5);
-   stack.push(17.3);
-   stack.push(16.6);
-   stack.push(2.2);
-   LOGGER.fine(() -> "Trove Array Stack of Doubles");
-   LOGGER.fine(() -> "\tPeek: " + stack.peek() + "; After Size: " + stack.size());
-   LOGGER.fine(() -> "\tPop:  " + stack.pop() + "; After Size: " + stack.size());
-   LOGGER.fine(() -> "\tPeek: " + stack.peek() + "; After Size: " + stack.size());
-}
+  /**
+   * Demonstrate Trove's Double Array Stack.
+   *
+   * <p>Trove's TDoubleArrayStack allows access to its contents via push, pop, and peek.
+   */
+  public static void demonstrateTroveDoubleArrayStack() {
+    final TDoubleArrayStack stack = new TDoubleArrayStack();
+    stack.push(15.5);
+    stack.push(17.3);
+    stack.push(16.6);
+    stack.push(2.2);
+    LOGGER.fine(() -> "Trove Array Stack of Doubles");
+    LOGGER.fine(
+        () -> "\tPeek: " + stack.peek() + "; After Size: " + stack.size());
+    LOGGER.fine(
+        () -> "\tPop:  " + stack.pop() + "; After Size: " + stack.size());
+    LOGGER.fine(
+        () -> "\tPeek: " + stack.peek() + "; After Size: " + stack.size());
+  }
 
-/**
- * Demonstrate JDK Stack.
- *
- * JDK Stack allows access to its
- * contents via push, pop, and peek.
- */
-public static void demonstrateJDKDoubleStack()
-{
-   final Stack<Double> stack = new Stack<>();
-   stack.push(15.5);
-   stack.push(17.3);
-   stack.push(16.6);
-   stack.push(2.2);
-   LOGGER.fine(() -> "JDK Stack of Doubles");
-   LOGGER.fine(() -> "\tPeek: " + stack.peek() + "; After Size: " + stack.size());
-   LOGGER.fine(() -> "\tPop:  " + stack.pop() + "; After Size: " + stack.size());
-   LOGGER.fine(() -> "\tPeek: " + stack.peek() + "; After Size: " + stack.size());
-}
+  /**
+   * Demonstrate JDK Stack.
+   *
+   * <p>JDK Stack allows access to its contents via push, pop, and peek.
+   */
+  public static void demonstrateJDKDoubleStack() {
+    final Stack<Double> stack = new Stack<>();
+    stack.push(15.5);
+    stack.push(17.3);
+    stack.push(16.6);
+    stack.push(2.2);
+    LOGGER.fine(() -> "JDK Stack of Doubles");
+    LOGGER.fine(
+        () -> "\tPeek: " + stack.peek() + "; After Size: " + stack.size());
+    LOGGER.fine(
+        () -> "\tPop:  " + stack.pop() + "; After Size: " + stack.size());
+    LOGGER.fine(
+        () -> "\tPeek: " + stack.peek() + "; After Size: " + stack.size());
+  }
 }
