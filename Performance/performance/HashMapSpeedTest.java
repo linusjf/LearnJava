ed0e039a53c420f2a80a1365707c6e5af4d6a68e
fernal73 <linus.fernandes@gmail.com>
2020-02-27 12:58:57 +0530

    Rename Inlining. Add testng

---
+++ Performance/performance/HashMapSpeedTest.java
@@ -0,0 +1,91 @@
+package performance;
+
+import com.google.common.collect.Maps;
+import gnu.trove.map.hash.THashMap;
+import javolution.util.FastMap;
+import org.apache.commons.collections.FastHashMap;
+import org.apache.commons.collections.FastTreeMap;
+import org.springframework.util.StopWatch;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class HashMapSpeedTest {
+
+  public static final int SIZE = 1_000_000;
+  private Set<String> objects;
+  private StopWatch stopWatch;
+
+  @BeforeClass
+  public void setUp(){
+    System.out.println("creating " + SIZE + " objects");
+    System.out.println("Implementation;get();put()");
+    objects = getObjects();
+    stopWatch = new StopWatch();
+  }
+
+  @DataProvider
+  public Object[][] mapProvider(){
+    return new Object[][]{
+        { new HashMap<String,Object>(SIZE,1f),"SIZE,1f" },
+        { new HashMap<String,Object>(SIZE,10f),"SIZE,10f" },
+        { new HashMap<String,Object>(SIZE,0.3f),"SIZE,0.3f" },
+        { new HashMap<String,Object>(SIZE,0.1f),"SIZE,0.1f" },
+        { new HashMap<String,Object>(),"" },
+        { new HashMap<String,Object>(SIZE),"SIZE" },
+        { new THashMap<String,Object>(),""},
+        { new THashMap<String,Object>(SIZE), "SIZE"},
+        { new THashMap<String,Object>(SIZE,1f), "SIZE,1f"},
+        { new FastMap<String,Object>(), ""},
+        { new FastMap<String,Object>(SIZE), "SIZE"},
+        { new FastHashMap(),""},
+        { new FastHashMap(SIZE),"SIZE"},
+        { new TreeMap<String,Object>(),""},
+        {Maps.newHashMap(),""},
+        { new FastTreeMap(),""},
+    };
+  }
+
+  @Test(dataProvider = "mapProvider",singleThreaded = true)
+  public void test(Map<String,Object> map,String typeExtension) {
+    String type = map.getClass().getName() + "("+typeExtension+")";
+
+    stopWatch.start(type+"put()");
+    for(String o:objects){
+      map.put(o,o);
+    }
+    stopWatch.stop();
+    long putTime = stopWatch.getLastTaskTimeMillis();
+
+    stopWatch.start(type+"get()");
+    for(String o:objects){
+      map.get(o);
+    }
+    stopWatch.stop();
+    long getTime = stopWatch.getLastTaskTimeMillis();
+    System.out.println(type + ";" + getTime + ";" + putTime);
+    map.clear();
+    System.gc();
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    System.out.println(stopWatch.prettyPrint());
+  }
+
+  private Set<String> getObjects() {
+    Set<String> objects = new HashSet<>();
+    for(int i=0;i< SIZE;i++){
+      objects.add("" + i);
+    }
+    return objects;
+  }
+}
