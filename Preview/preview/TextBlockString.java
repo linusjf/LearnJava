92a39499e1150ce11da37aa1394086dc94005e98
fernal73 <linus.fernandes@gmail.com>
2020-02-28 19:37:47 +0530

    Add project Preview

---
+++ Preview/preview/TextBlockString.java
@@ -0,0 +1,33 @@
+package preview;
+
+@SuppressWarnings("preview")
+public enum TextBlockString {
+  ;
+
+  /** * JEP 355: Preview Feature */
+  public static void main(String[] args) {
+    String textBlock =
+        """
+        Hi Hello Yes
+        """;
+
+        String str = "Hi\nHello\nYes";
+
+    System.out.println("Text Block String:\n" + textBlock);
+    System.out.println("Normal String Literal:\n" + str);
+
+    System.out.println("Text Block and String Literal equals() Comparison: "
+                       + (textBlock.equals(str)));
+    System.out.println("Text Block and String Literal == Comparison: "
+                       + (textBlock == str));
+    String textBlockHTML = """
+  <html><head><link href='/css/style.css' rel='stylesheet' /></head><body><h1>Hello World</h1></body></html>
+  """;
+
+String textBlockJSON = """
+{
+      "name" : "Pankaj", "website" : "JournalDev"
+    }
+    """;
+  }
+}
