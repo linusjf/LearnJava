dce07222104b482f81dd9d72c050ee65578d85cb
fernal73 <linus.fernandes@gmail.com>
2020-05-20 13:31:40 +0530

    Refactor build file to dymamically set and load classpath. Format

--- JDBC/jdbc/JdbcBeanX.java
+++ JDBC/jdbc/JdbcBeanX.java
@@ -12,10 +12,13 @@ public class JdbcBeanX {
 
   public JdbcBeanX() throws JdbcBeanException {
     numAccounts = 0;
-    try (Connection connection = DriverManager.getConnection("jdbc:derby:Finances", "", "");
-        Statement statement = connection.createStatement();
-        ResultSet results = statement.executeQuery("SELECT COUNT(*) FROM Accounts"); ) {
-      if (results.next()) numAccounts = results.getInt(1);
+    try (Connection connection =
+             DriverManager.getConnection("jdbc:derby:Finances", "", "");
+         Statement statement = connection.createStatement();
+         ResultSet results =
+             statement.executeQuery("SELECT COUNT(*) FROM Accounts");) {
+      if (results.next())
+        numAccounts = results.getInt(1);
     } catch (SQLException sqe) {
       throw new JdbcBeanException(sqe);
     }
