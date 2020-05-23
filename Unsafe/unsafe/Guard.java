fd8b9a661a50012c6d1802316defc1603fe6afba
fernal73 <linus.fernandes@gmail.com>
2020-05-23 10:43:19 +0530

    Add Guard example

---
+++ Unsafe/unsafe/Guard.java
@@ -0,0 +1,35 @@
+package unsafe;
+
+import java.lang.reflect.Field;
+import sun.misc.Unsafe; // NOPMD
+
+@SuppressWarnings({"PMD.SystemPrintln",
+                   "PMD.FinalFieldCouldBeStatic",
+                   "PMD.LawOfDemeter",
+                   "PMD.ImmutableField"})
+public class Guard {
+  private int accessAllowed = 1;
+
+  public boolean giveAccess() {
+    return 42 == accessAllowed;
+  }
+
+  public static void main(String... args) {
+    try {
+      Guard guard = new Guard();
+      // false, no access
+      System.out.println(guard.giveAccess());
+
+      // bypass
+      Unsafe unsafe = Counter.getUnsafe();
+      Field f = Guard.class.getDeclaredField("accessAllowed");
+      // memory corruption
+      unsafe.putInt(guard, unsafe.objectFieldOffset(f), 42);
+
+      // true, access granted
+      System.out.println(guard.giveAccess());
+    } catch (ReflectiveOperationException roe) {
+      System.err.println(roe.getMessage());
+    }
+  }
+}
