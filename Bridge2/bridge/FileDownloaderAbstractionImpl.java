50b03457e9d4378051ec3e3cd2cb074ce0b18abd
fernal73 <linus.fernandes@gmail.com>
2020-06-05 21:46:47 +0530

    Fix spotbugs suppressions

--- Bridge2/bridge/FileDownloaderAbstractionImpl.java
+++ Bridge2/bridge/FileDownloaderAbstractionImpl.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.Objects;
+
 /**
  * Describe class <code>FileDownloaderAbstractionImpl</code> here.
  *
@@ -16,8 +18,28 @@ public class FileDownloaderAbstractionImpl
    * @param provider a <code>FileDownloadImplementor</code> value
    */
   public FileDownloaderAbstractionImpl(FileDownloadImplementor provider) {
-
     this.provider = provider;
+  }
+
+  @Override
+  public String toString() {
+   return getClass() + " : " +
+     "Provider = " + provider;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+   if (this == o)
+     return true;
+  if (o instanceof FileDownloaderAbstractionImpl) {
+return provider.equals(((FileDownloaderAbstractionImpl)o).provider);
+  }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(provider);
   }
 
   @Override
