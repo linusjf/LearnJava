05bd9133331a833ca3f8fa3cddf0b6afb8c1757d
fernal73 <linus.fernandes@gmail.com>
2020-06-07 16:58:22 +0530

    Lombok DeliveryContext

--- State/statepattern/DeliveryContext.java
+++ State/statepattern/DeliveryContext.java
@@ -10,7 +10,6 @@ public class DeliveryContext {
   public DeliveryContext(PackageState currentState, String packageId) {
     this.currentState = currentState;
     this.packageId = packageId;
-
     Optional<PackageState> pkgState = Optional.ofNullable(currentState);
     this.currentState = pkgState.orElse(Acknowledged.getInstance());
   }
@@ -33,5 +32,44 @@ public class DeliveryContext {
 
   public void update() {
     currentState.updateState(this);
+  }
+
+  @Override
+  @SuppressWarnings("all")
+  public String toString() {
+    return "DeliveryContext(currentState=" + this.getCurrentState() + ", packageId=" + this.getPackageId() + ")";
+  }
+
+  @Override
+  @SuppressWarnings("all")
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (!(o instanceof DeliveryContext)) return false;
+    DeliveryContext other = (DeliveryContext) o;
+    if (!other.canEqual((Object) this)) return false;
+    Object this$currentState = this.getCurrentState();
+    Object other$currentState = other.getCurrentState();
+    if (this$currentState == null ? other$currentState != null : !this$currentState.equals(other$currentState)) return false;
+    Object this$packageId = this.getPackageId();
+    Object other$packageId = other.getPackageId();
+    if (this$packageId == null ? other$packageId != null : !this$packageId.equals(other$packageId)) return false;
+    return true;
+  }
+
+  @SuppressWarnings("all")
+  protected boolean canEqual(Object other) {
+    return other instanceof DeliveryContext;
+  }
+
+  @Override
+  @SuppressWarnings("all")
+  public int hashCode() {
+    int PRIME = 59;
+    int result = 1;
+    Object $currentState = this.getCurrentState();
+    result = result * PRIME + ($currentState == null ? 43 : $currentState.hashCode());
+    Object $packageId = this.getPackageId();
+    result = result * PRIME + ($packageId == null ? 43 : $packageId.hashCode());
+    return result;
   }
 }
