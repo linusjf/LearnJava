fdfa178692aa00c6fef95ae4bb291b3ca64d9860
fernal73 <linus.fernandes@gmail.com>
2020-06-07 15:21:49 +0530

    Update for spotbugs violations

--- Strategy/com/javacodegeeks/patterns/strategypattern/TextEditor.java
+++ Strategy/com/javacodegeeks/patterns/strategypattern/TextEditor.java
@@ -11,4 +11,34 @@ public class TextEditor {
   public void publishText(String text) {
     textFormatter.format(text);
   }
+
+  @Override
+  public String toString() {
+    return "TextEditor(textFormatter=" + this.textFormatter + ")";
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (!(o instanceof TextEditor)) return false;
+    TextEditor other = (TextEditor) o;
+    if (!other.canEqual((Object) this)) return false;
+    Object this$textFormatter = this.textFormatter;
+    Object other$textFormatter = other.textFormatter;
+    if (this$textFormatter == null ? other$textFormatter != null : !this$textFormatter.equals(other$textFormatter)) return false;
+    return true;
+  }
+
+  protected boolean canEqual(Object other) {
+    return other instanceof TextEditor;
+  }
+
+  @Override
+  public int hashCode() {
+    int PRIME = 59;
+    int result = 1;
+    Object $textFormatter = this.textFormatter;
+    result = result * PRIME + ($textFormatter == null ? 43 : $textFormatter.hashCode());
+    return result;
+  }
 }
