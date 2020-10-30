21309d97cb7c8921981f205e3f41867425a9b4f3
fernal73 <linus.fernandes@gmail.com>
2020-10-30 09:42:55 +0530

    Debug jar.groovy. Remove semi-colon from jmh cp

--- JMH/jar.groovy
+++ JMH/jar.groovy
@@ -1,16 +1,46 @@
 import java.util.jar.Manifest
+import java.util.jar.JarEntry
+import java.util.jar.JarOutputStream
 import java.io.File
+import java.io.BufferedInputStream
+import java.util.Set
+import java.io.FilenameFilter
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
 
-public void run() throws IOException
+System.out.println(properties.get("groovy-ant.cp"))
+System.out.println(properties.get("version"))
+System.out.println(properties.get("build.dir"))
+System.out.println(properties.get("runclasspath"))
+createJar()
+
+public void createJar() throws IOException
    {
+     def ver = properties.get("version")
+       def buildDir = properties.get("build.dir")  
       Manifest manifest = new Manifest(Files.newInputStream(Paths.get("manifest.mf")));
-      JarOutputStream target = new JarOutputStream(new FileOutputStream("output.jar"), manifest);
-      File inputDirectory = new File("inputDirectory");
+      JarOutputStream target = new JarOutputStream(Files.newOutputStream(Paths.get("dist/" + project.name + ver + ".jar")), manifest);
+      File inputDirectory = new File(buildDir);
       for (File nestedFile : inputDirectory.listFiles())
          add("", nestedFile, target);
+      String jars = properties.get("runclasspath")
+        for (String jar: jars.split(":")) { 
+        JarEntry entry = new JarEntry(jar);
+        entry.setTime(inputDirectory.lastModified());
+         target.putNextEntry(entry);
+         in = new BufferedInputStream(new FileInputStream(jar));
+
+         byte[] buffer = new byte[1024];
+         while (true)
+         {
+            int count = in.read(buffer);
+            if (count == -1)
+               break;
+            target.write(buffer, 0, count);
+         }
+         target.closeEntry();
+        }
       target.close();
    }
 
@@ -37,6 +67,8 @@ public void run() throws IOException
             return;
          }
 
+        if (name.endsWith(".jar") ||
+            name.endsWith(".class"))  {
          JarEntry entry = new JarEntry(name);
          entry.setTime(source.lastModified());
          target.putNextEntry(entry);
@@ -51,6 +83,7 @@ public void run() throws IOException
             target.write(buffer, 0, count);
          }
          target.closeEntry();
+        }
       }
       finally
       {
