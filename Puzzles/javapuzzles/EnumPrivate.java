e79260f0613fa7bf13ed1c0152f2a7772e32f64a
fernal73 <linus.fernandes@gmail.com>
2020-11-07 11:37:55 +0530

    Update ShortClassName length.

--- Puzzles/javapuzzles/EnumPrivate.java
+++ Puzzles/javapuzzles/EnumPrivate.java
@@ -1,9 +1,24 @@
 package javapuzzles;
 
-enum HelloEnum {
+enum EnumPrivate {
   ;
 
-  HelloEnum() {
+  EnumPrivate() {
     throw new IllegalStateException("Hello");
+  }
+
+  public void sayHello() {
+    System.out.println("Hello!");
+  }
+
+  public static void main(String... args) {
+    try {
+      System.out.println("Invoking hello enum");
+      EnumPrivate hello = EnumPrivate.INSTANCE;
+      hello.sayHello();
+    } catch (ExceptionInInitializerError eie) {
+      System.err.println(eie.getMessage());
+      System.err.println(eie.getCause());
+    }
   }
 }
