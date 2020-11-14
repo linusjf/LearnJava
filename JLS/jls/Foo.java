2ac4266e29cef9d3ffd52a2bdb33f95191802447
fernal73 <linus.fernandes@gmail.com>
2020-11-14 17:57:21 +0530

    Add new enum sample

---
+++ JLS/jls/Foo.java
@@ -0,0 +1,23 @@
+package jls;
+
+enum Foo {
+  A;
+  private final Object obj = new Object();
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSomething() {
+    synchronized (obj) {
+      // synchronized? If the enum constant is really immutable, then no data race can occur
+      // So the synchronized hints that there is something fishy... or at least it is redundant
+      return null;
+    }
+  }
+}
