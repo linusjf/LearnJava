372ebf4d28b09ce23b69e3a8fdf38ad3295af4c1
fernal73 <linus.fernandes@gmail.com>
2020-12-10 14:31:19 +0530

    Modify Ternary

--- JLS/jls/Ternary.java
+++ JLS/jls/Ternary.java
@@ -11,8 +11,18 @@ public final class Ternary {
     return b ? first : second;
   }
 
+  static Class<? super Integer> chooseClass(boolean b,
+                                            Class<Integer> c1,
+                                            Class<Number> c2) {
+    return b ? c1 : c2;
+  }
+
   public static void main(String... args) {
     System.out.println(choose(true, Integer.MAX_VALUE, Long.MAX_VALUE));
     System.out.println(choose(false, Integer.MAX_VALUE, Long.MAX_VALUE));
+    System.out.println(choose(true, Integer.class, Number.class));
+    System.out.println(choose(false, Integer.class, Number.class));
+    System.out.println(chooseClass(true, Integer.class, Number.class));
+    System.out.println(chooseClass(false, Integer.class, Number.class));
   }
 }
