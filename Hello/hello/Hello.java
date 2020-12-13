9f62687240e2d132a9188518e582c09d48e25aa5
fernal73 <linus.fernandes@gmail.com>
2020-12-13 11:56:32 +0530

    Update rules

--- Hello/hello/Hello.java
+++ Hello/hello/Hello.java
@@ -8,7 +8,6 @@ public enum Hello {
    *
    * @param args a <code>String</code> value
    */
-  @SuppressWarnings("PMD.SystemPrintln")
   public static void main(String[] args) {
     System.out.println("Hello world, on termux!");
   }
