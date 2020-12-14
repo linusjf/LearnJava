27e5e7d82714d1b4f2b52aa6f6e9ce6af3f9e18b
fernal73 <linus.fernandes@gmail.com>
2020-12-14 16:00:03 +0530

    Add .java.xml to .gitignore. change suffix of generated file

--- Hello/hello/Hello.java
+++ Hello/hello/Hello.java
@@ -1,14 +1,20 @@
 package hello;
 
-/** Describe class <code>Hello</code> here. */
-public enum Hello {
-  ;
+/**
+ * Describe class <code>Hello</code> here.
+ */
+public final class Hello {
+  @SuppressWarnings("all")
+  private Hello() {
+    throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+  }
+  
   /**
    * Describe <code>main</code> method here.
    *
    * @param args a <code>String</code> value
    */
-  public static void main(String[] args) {
+  public static void main(String... args) {
     System.out.println("Hello world, on termux!");
   }
 }
