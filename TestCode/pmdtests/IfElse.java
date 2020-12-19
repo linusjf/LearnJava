ec7ab0b26926a0541cdadba30ffa24d07ec2638d
fernal73 <linus.fernandes@gmail.com>
2020-12-19 12:36:10 +0530

    fix IfElse

--- TestCode/pmdtests/IfElse.java
+++ TestCode/pmdtests/IfElse.java
@@ -1,6 +1,11 @@
 package pmdtests;
 
-public class IfElse {
+public final class IfElse {
+
+  private IfElse() {
+    throw new IllegalStateException("Private constructor.");
+  }
+
   public static void main(String... argv) {
     int argc = argv.length;
     int hpos = 0;
@@ -13,6 +18,24 @@ public class IfElse {
         ;
       else if (argv[i] == "down")
         ;
+      else
+        System.err.printf("Unknown option %s%n", argv[i]);
+    }
+  }
+
+  public static void nextMain(String... argv) {
+    int argc = argv.length;
+    int hpos = 0;
+    int vpos = 0;
+    for (int i = 0; i < argc; i++) {
+      if (argv[i] == "left")
+        hpos++;
+      else if (argv[i] == "right")
+        hpos--;
+      else if (argv[i] == "up")
+        vpos++;
+      else if (argv[i] == "down")
+        vpos--;
       else
         System.err.printf("Unknown option %s%n", argv[i]);
     }
