7acab7c6f6b62c9858aac3725a9f82067afd9596
fernal73 <linus.fernandes@gmail.com>
2020-12-21 09:51:19 +0530

    Add more tests for SwitchString

--- JMH/jmh/SwitchString.java
+++ JMH/jmh/SwitchString.java
@@ -1,5 +1,6 @@
 package jmh;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -10,6 +11,7 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -20,16 +22,40 @@ import org.openjdk.jmh.runner.options.VerboseMode;
 @State(Scope.Thread)
 public class SwitchString {
 
+  private static final int SIZE = 1000;
+
   volatile String value;
+  volatile String[] months;
+  volatile int[] indexes;
+  volatile Random random;
 
   @Setup(Level.Trial)
   public void doSetup() {
     value = "July";
+    months = new String[] {"January",
+                           "February",
+                           "March",
+                           "April",
+                           "May",
+                           "June",
+                           "July",
+                           "August",
+                           "September",
+                           "October",
+                           "November",
+                           "December"};
+    random = new Random(123456789L);
+    indexes = new int[SIZE];
+    for (int i = 0; i < SIZE; i++)
+      indexes[i] = random.nextInt(12);
   }
 
   @TearDown(Level.Trial)
   public void doTearDown() {
     value = null;
+    months = null;
+    indexes = null;
+    random = null;
   }
 
   @Benchmark
@@ -119,6 +145,101 @@ public class SwitchString {
         break;
     }
     return month;
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.Throughput)
+  @OutputTimeUnit(TimeUnit.SECONDS)
+  public void ifElseRandom(Blackhole bh) {
+    for (int i = 0; i < SIZE; i++) {
+      int month = 0;
+      String value = months[indexes[i]];
+      if (value.equals("January"))
+        month = 1;
+      else if (value.equals("February"))
+        month = 2;
+      else if (value.equals("March"))
+        month = 3;
+      else if (value.equals("April"))
+        month = 4;
+      else if (value.equals("May"))
+        month = 5;
+      else if (value.equals("June"))
+        month = 6;
+      else if (value.equals("July"))
+        month = 7;
+      else if (value.equals("August"))
+        month = 8;
+      else if (value.equals("September"))
+        month = 9;
+      else if (value.equals("October"))
+        month = 10;
+      else if (value.equals("November"))
+        month = 11;
+      else if (value.equals("December"))
+        month = 12;
+      bh.consume(month);
+    }
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.Throughput)
+  @OutputTimeUnit(TimeUnit.SECONDS)
+  public void switchCaseRandom(Blackhole bh) {
+    for (int i = 0; i < SIZE; i++) {
+      int month = 0;
+      String value = months[indexes[i]];
+      switch (value) {
+        case "January":
+          month = 1;
+          break;
+
+        case "February":
+          month = 2;
+          break;
+
+        case "March":
+          month = 3;
+          break;
+
+        case "April":
+          month = 4;
+          break;
+
+        case "May":
+          month = 5;
+          break;
+
+        case "June":
+          month = 6;
+          break;
+
+        case "July":
+          month = 7;
+          break;
+
+        case "August":
+          month = 8;
+          break;
+
+        case "September":
+          month = 9;
+          break;
+
+        case "October":
+          month = 10;
+          break;
+
+        case "November":
+          month = 11;
+          break;
+
+        case "December":
+          month = 12;
+          break;
+      }
+      bh.consume(month);
+    }
   }
 
   public static void main(String[] args) throws RunnerException {
