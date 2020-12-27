759b817cb740bcda29befe03c729dc821eed4dde
fernal73 <linus.fernandes@gmail.com>
2020-12-27 16:51:12 +0530

    Add final static field

--- TestCode/pmdtests/TestFinal.java
+++ TestCode/pmdtests/TestFinal.java
@@ -4,6 +4,9 @@ import java.math.BigInteger;
 import java.util.Random;
 
 public class TestFinal {
+
+  public static String TEST = "Test";
+
   public TestFinal() {
     Random random = new Random();
     BigInteger e = BigInteger.probablePrime(Integer.MAX_VALUE / 2, random);
