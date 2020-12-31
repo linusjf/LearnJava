9fb116d863c6b5dcca96e8111f17e0b9d71190c9
fernal73 <linus.fernandes@gmail.com>
2020-12-31 17:30:13 +0530

    Revert to original example

--- Serialization/serial/ExternalizableXmlExample.java
+++ Serialization/serial/ExternalizableXmlExample.java
@@ -1,9 +1,9 @@
 package serial;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -15,7 +15,7 @@ public final class ExternalizableXmlExample {
         "This is a utility class and cannot be instantiated");
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     UserSettings settings = new UserSettings();
     settings.setFieldOne(10_000);
     settings.setFieldTwo("HowToDoInJava.com");
@@ -26,25 +26,19 @@ public final class ExternalizableXmlExample {
     System.out.println(loadedSettings);
   }
 
-  private static void serializeToXML(UserSettings settings) {
+  private static void serializeToXML(UserSettings settings) throws IOException {
     try (OutputStream fos = Files.newOutputStream(Paths.get("settings.xml"));
-         ObjectOutputStream oos = new ObjectOutputStream(fos);) {
-      settings.writeExternal(oos);
-    } catch (IOException ioe) {
-      System.err.println(ioe.getMessage());
+         XMLEncoder encoder = new XMLEncoder(fos);) {
+      encoder.setExceptionListener(
+          e -> System.out.println("Exception! :" + e.toString()));
+      encoder.writeObject(settings);
     }
   }
 
-  private static UserSettings deserializeFromXML() {
-    UserSettings decodedSettings = new UserSettings();
+  private static UserSettings deserializeFromXML() throws IOException {
     try (InputStream fis = Files.newInputStream(Paths.get("settings.xml"));
-         ObjectInputStream ois = new ObjectInputStream(fis);) {
-      decodedSettings.readExternal(ois);
-    } catch (IOException ioe) {
-      System.err.println(ioe.getMessage());
-    } catch (ClassNotFoundException cnfe) {
-      System.err.println(cnfe.getMessage());
+         XMLDecoder decoder = new XMLDecoder(fis);) {
+      return (UserSettings)decoder.readObject();
     }
-    return decodedSettings;
   }
 }
