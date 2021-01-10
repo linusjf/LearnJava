d3017be288f2a94e08fd49a155b60acfd8235ec5
fernal73 <linus.fernandes@gmail.com>
2021-01-10 09:33:03 +0530

    Suppress checkstyle violations

--- Puzzles/javapuzzles/AddNum.java
+++ Puzzles/javapuzzles/AddNum.java
@@ -7,12 +7,29 @@ public enum AddNum {
     System.out.println(computeSum());
     System.out.println(computeSumAgain());
     System.out.println(computeSummer());
+    System.out.println(computeSumExpanded());
+    System.out.println(computeSumSub());
   }
 
   private static int computeSum() {
     int sum = 0;
     int mod = 1;
     sum += mod * ++mod / 2;
+    return sum;
+  }
+
+  private static int computeSumExpanded() {
+    int sum = 0;
+    int mod = 1;
+    int b = mod + 1;
+    sum += mod * b / 2;
+    return sum;
+  }
+  
+  private static int computeSumSub() {
+    int sum = 0;
+    int mod = 1;
+    sum += mod * (mod = mod + 1)/ 2;
     return sum;
   }
 
