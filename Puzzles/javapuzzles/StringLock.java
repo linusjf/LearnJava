7dd0680c282e38a6be6200912a518c4e5700bc99
fernal73 <linus.fernandes@gmail.com>
2021-01-11 21:12:37 +0530

    Add StringLock and SubNum

---
+++ Puzzles/javapuzzles/StringLock.java
@@ -0,0 +1,33 @@
+package javapuzzles;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+// https://twitter.com/PeterLawrey/status/1348561466060906496?s=19
+@SuppressWarnings("all")
+public final class StringLock {
+  static String hi = "The quick brown fox jumped over the lazy dog.";
+/** static String name = IntStream.range(0, hi.length())
+                           .parallel()
+                           .map(i -> hi.charAt(i))
+                           .filter(s -> s != ' ')
+                           .mapToObj(c -> Character.toString((char)c))
+                           .sorted()
+                           .collect(Collectors.joining());
+*/
+  static String name = IntStream.range(0, hi.length())
+                           .map(i -> hi.charAt(i))
+                           .filter(s -> s != ' ')
+                           .mapToObj(c -> Character.toString((char)c))
+                           .sorted()
+                           .collect(Collectors.joining());
+  
+  private StringLock() {
+    throw new UnsupportedOperationException(
+        "This is a utility class and cannot be instantiated");
+  }
+
+  public static void main(String... args) {
+      System.out.println(name);
+  }
+}
--- Puzzles/javapuzzles/SubNum.java
+++ Puzzles/javapuzzles/SubNum.java
@@ -1,6 +1,6 @@
 package javapuzzles;
 
-public enum AddNum {
+public enum SubNum {
   ;
 
   public static void main(String... args) {
@@ -9,57 +9,57 @@ public enum AddNum {
     System.out.println(computeSummer());
     System.out.println(computeSumExpanded());
     System.out.println(computeSumSub());
-    System.out.println(computeSumPlus());
-    System.out.println(computeSumPlus30());
+    System.out.println(computeSumMinus());
+    System.out.println(computeSumMinus30());
   }
 
   private static int computeSumExpanded() {
     int sum = 0;
-    int mod = 1;
-    int b = mod + 1;
+    int mod = 2;
+    int b = mod - 1;
     sum += mod * b / 2;
     return sum;
   }
 
-  private static int computeSumPlus() {
+  private static int computeSumMinus() {
     int sum = 0;
-    int mod = 1;
-    sum += mod * (mod + 1) / 2;
+    int mod = 2;
+    sum += mod * (mod - 1) / 2;
     return sum;
   }
 
-  private static int computeSumPlus30() {
+  private static int computeSumMinus30() {
     int sum = 0;
-    int mod = 1;
-    sum += mod * (mod += 30) / 2;
+    int mod = 2;
+    sum += mod * (mod -= 30) / 2;
     return sum;
   }
 
   private static int computeSumSub() {
     int sum = 0;
-    int mod = 1;
-    sum += mod * (mod = mod + 1) / 2;
+    int mod = 2;
+    sum += mod * (mod = mod - 1) / 2;
     return sum;
   }
 
   private static int computeSum() {
     int sum = 0;
-    int mod = 1;
-    sum += mod * ++mod / 2;
+    int mod = 2;
+    sum += mod * --mod / 2;
     return sum;
   }
 
   private static int computeSumAgain() {
     int sum = 0;
-    int mod = 1;
-    sum += mod * (++mod / 2);
+    int mod = 2;
+    sum += mod * (--mod / 2);
     return sum;
   }
 
   private static int computeSummer() {
     int sum = 0;
-    int mod = 1;
-    sum += (mod * ++mod) / 2;
+    int mod = 2;
+    sum += (mod * --mod) / 2;
     return sum;
   }
 }
