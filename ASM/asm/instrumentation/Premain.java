4722d2c244b0636b834474b7a36026196976d2eb
fernal73 <linus.fernandes@gmail.com>
2021-02-12 14:10:55 +0530

    Fix static code analysis violations

--- ASM/asm/instrumentation/Premain.java
+++ ASM/asm/instrumentation/Premain.java
@@ -7,25 +7,28 @@ import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
 /**
+ * Premain.
  *
  * @author baeldung
  */
-public class Premain {
+public final class Premain {
 
-    public static void premain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new ClassFileTransformer() {
+  private Premain() {
+    throw new AssertionError("Private constructor");
+  }
 
-            @Override
-            public byte[] transform(ClassLoader l, String name, Class c,
-                    ProtectionDomain d, byte[] b)
-                    throws IllegalClassFormatException {
-
-                if (name.equals("java/lang/Integer")) {
-                    CustomClassWriter cr = new CustomClassWriter(b);
-                    return cr.addField();
-                }
-                return b;
-            }
-        });
-    }
+  public static void premain(String agentArgs, Instrumentation inst) {
+    inst.addTransformer(new ClassFileTransformer() {
+      @Override
+      public byte[] transform(
+          ClassLoader l, String name, Class<?> c, ProtectionDomain d, byte[] b)
+          throws IllegalClassFormatException {
+        if ("java/lang/Integer".equals(name)) {
+          CustomClassWriter cr = new CustomClassWriter(b);
+          return cr.addField();
+        }
+        return b;
+      }
+    });
+  }
 }
