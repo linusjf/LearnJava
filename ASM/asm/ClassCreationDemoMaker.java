e90accc9b0cb7edacf7632048c926e505f75727e
fernal73 <linus.fernandes@gmail.com>
2021-02-13 21:35:45 +0530

    Fix static code analysis violations

--- ASM/asm/ClassCreationDemoMaker.java
+++ ASM/asm/ClassCreationDemoMaker.java
@@ -3,35 +3,39 @@ package asm;
 import static org.objectweb.asm.Opcodes.*;
 
 import java.io.DataOutputStream;
-import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 
 public final class ClassCreationDemoMaker {
 
+  private static final String VERSION = "version";
+  private static final String STRINGBUILDER = "java/lang/StringBuilder";
+
   private ClassCreationDemoMaker() {
     throw new AssertionError("Private constructor");
   }
 
   @SuppressWarnings("checkstyle:ExecutableStatementCount")
-  public static byte[] dump() throws Exception {
+  public static byte[] dump() throws IOException {
 
     // ClassWriter is a class visitor that generates the code for the class
     ClassWriter cw = new ClassWriter(0);
-    FieldVisitor fv;
-    MethodVisitor mv;
     // Start creating the class.
-    cw.visit(V1_6,
+    cw.visit(V11,
              ACC_PUBLIC + ACC_SUPER,
              "com/geekyarticles/asm/ClassCreationDemo",
              null,
              "java/lang/Object",
              null);
-
+    FieldVisitor fv;
+    MethodVisitor mv;
     {
       // version field
-      fv = cw.visitField(ACC_PRIVATE, "version", "I", null, null);
+      fv = cw.visitField(ACC_PRIVATE, VERSION, "I", null, null);
       fv.visitEnd();
     }
     {
@@ -39,7 +43,8 @@ public final class ClassCreationDemoMaker {
       mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
       mv.visitCode();
       mv.visitVarInsn(ALOAD, 0);
-      mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+      mv.visitMethodInsn(
+          INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
       mv.visitInsn(RETURN);
       mv.visitMaxs(1, 1);
       mv.visitEnd();
@@ -50,7 +55,7 @@ public final class ClassCreationDemoMaker {
       mv.visitCode();
       mv.visitVarInsn(ALOAD, 0);
       mv.visitFieldInsn(
-          GETFIELD, "com/geekyarticles/asm/ClassCreationDemo", "version", "I");
+          GETFIELD, "com/geekyarticles/asm/ClassCreationDemo", VERSION, "I");
       mv.visitInsn(IRETURN);
       mv.visitMaxs(1, 1);
       mv.visitEnd();
@@ -62,7 +67,7 @@ public final class ClassCreationDemoMaker {
       mv.visitVarInsn(ALOAD, 0);
       mv.visitVarInsn(ILOAD, 1);
       mv.visitFieldInsn(
-          PUTFIELD, "com/geekyarticles/asm/ClassCreationDemo", "version", "I");
+          PUTFIELD, "com/geekyarticles/asm/ClassCreationDemo", VERSION, "I");
       mv.visitInsn(RETURN);
       mv.visitMaxs(2, 2);
       mv.visitEnd();
@@ -72,24 +77,23 @@ public final class ClassCreationDemoMaker {
       mv = cw.visitMethod(
           ACC_PUBLIC, "toString", "()Ljava/lang/String;", null, null);
       mv.visitCode();
-      mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
+      mv.visitTypeInsn(NEW, STRINGBUILDER);
       mv.visitInsn(DUP);
       mv.visitLdcInsn("ClassCreationDemo: ");
       mv.visitMethodInsn(INVOKESPECIAL,
-                         "java/lang/StringBuilder",
+                         STRINGBUILDER,
                          "<init>",
                          "(Ljava/lang/String;)V",
                          false);
       mv.visitVarInsn(ALOAD, 0);
-      mv.visitFieldInsn(
-          GETFIELD, "asm/ClassCreationDemo", "version", "I");
+      mv.visitFieldInsn(GETFIELD, "asm/ClassCreationDemo", VERSION, "I");
       mv.visitMethodInsn(INVOKEVIRTUAL,
-                         "java/lang/StringBuilder",
+                         STRINGBUILDER,
                          "append",
                          "(I)Ljava/lang/StringBuilder;",
                          false);
       mv.visitMethodInsn(INVOKEVIRTUAL,
-                         "java/lang/StringBuilder",
+                         STRINGBUILDER,
                          "toString",
                          "()Ljava/lang/String;",
                          false);
@@ -102,12 +106,11 @@ public final class ClassCreationDemoMaker {
     return cw.toByteArray();
   }
 
-  public static void main(String... args) throws Exception {
-    
-    DataOutputStream dout =
-        new DataOutputStream(new FileOutputStream("ClassCreationDemo.class"));
-    dout.write(dump());
-    dout.flush();
-    dout.close();
+  public static void main(String... args) throws IOException {
+    try (DataOutputStream dout = new DataOutputStream(
+             Files.newOutputStream(Paths.get("ClassCreationDemo.class")))) {
+      dout.write(dump());
+      dout.flush();
+    }
   }
 }
