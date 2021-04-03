1d37dd56c03733ff7d79b01c8cfea2e038db491e
fernal73 <linus.fernandes@gmail.com>
2021-04-03 14:44:54 +0530

    Update Hello

--- Hello/hello/Hello.java
+++ Hello/hello/Hello.java
@@ -1,14 +1,13 @@
 package hello;
 
-/**
- * Describe class <code>Hello</code> here.
- */
+/** Describe class <code>Hello</code> here. */
 public final class Hello {
   @SuppressWarnings("all")
   private Hello() {
-    throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    throw new UnsupportedOperationException(
+        "This is a utility class and cannot be instantiated");
   }
-  
+
   /**
    * Describe <code>main</code> method here.
    *
