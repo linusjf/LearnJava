0bf46a1f081d8c67046ef3d6dbbefdab277d8329
fernal73 <linus.fernandes@gmail.com>
2021-04-09 05:01:26 +0530

    Initiate AI project

---
+++ AI/ai/FrogsJumpPuzzle.java
@@ -0,0 +1,20 @@
+package ai;
+
+import java.util.List;
+
+public final class FrogsJumpPuzzle {
+
+  int emptyPadIndex;
+
+  List<Object> slots;
+
+  interface Frog {}
+
+  static class RedFrog implements Frog {
+    // empty class
+  }
+
+  static class BlueFrog implements Frog {
+    // empty class
+  }
+}
