dc22246f83e865a35e7db0f4055b38c4298d6c41
fernal73 <linus.fernandes@gmail.com>
2021-04-09 05:35:00 +0530

    Start on solver

--- AI/ai/FrogsJumpPuzzle.java
+++ AI/ai/FrogsJumpPuzzle.java
@@ -1,12 +1,37 @@
 package ai;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public final class FrogsJumpPuzzle {
 
+  int puzzleSize;
   int emptyPadIndex;
 
   List<Object> slots;
+
+  public FrogsJumpPuzzle(int size) {
+    puzzleSize = size;
+    slots = new ArrayList<>(2 * size + 2);
+    slots.add(Optional.ofNullable(null));
+    for (int i = 1; i < size + 1; i++)
+      slots.add(new RedFrog());
+    slots.add(null);
+    for (int i = size + 2; i < 2 * size + 2; i++)
+      slots.add(new BlueFrog());
+  }
+
+  public void solve() {
+    List<Integer> jumpIndices = new ArrayList<>();
+    int midIndex = puzzleSize + 1;
+    assert slots.indexOf(null) == puzzleSize + 1;
+  }
+
+  public static void main(String... args) {
+    FrogsJumpPuzzle fjp = new FrogsJumpPuzzle(1);
+    fjp.solve();
+  }
 
   interface Frog {}
 
