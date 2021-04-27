88120888c8a3090be8e671e85c51603738b26c09
fernal73 <linus.fernandes@gmail.com>
2021-04-27 16:50:03 +0530

    Add code for operations

--- MongoDB/mongodb/ConnectToDB.java
+++ MongoDB/mongodb/ConnectToDB.java
@@ -7,10 +7,13 @@ import com.mongodb.MongoSecurityException;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import java.util.ArrayList;
 import java.util.List;
 import org.bson.Document;
 
+@SuppressWarnings("PMD.LawOfDemeter")
 public final class ConnectToDB {
 
   private static final int MONGO_DB_PORT = 27_017;
@@ -42,6 +45,38 @@ public final class ConnectToDB {
       MongoCollection<Document> col = db.getCollection("mycol");
       for (Document doc: col.find())
         System.out.println(doc);
+
+      // Creating a collection
+      db.createCollection("sampleCollection");
+      System.out.println("Collection created");
+
+      Document document =
+          new Document("title", "MongoDB")
+              .append("id", 1)
+              .append("description", "database")
+              .append("likes", 100)
+              .append("url", "http://www.tutorialspoint.com/mongodb/")
+              .append("by", "tutorials point");
+      col.insertOne(document);
+      System.out.println("Document inserted successfully");
+      for (Document doc: col.find())
+        System.out.println(doc);
+
+      col.updateOne(Filters.eq("id", 1), Updates.set("likes", 150));
+      System.out.println("Document update successfully...");
+
+      // Deleting the documents
+      col.deleteOne(Filters.eq("id", 1));
+      System.out.println("Document deleted successfully...");
+
+      // Retieving a collection
+      MongoCollection<Document> collection =
+          db.getCollection("sampleCollection");
+      // Dropping a Collection
+      collection.drop();
+
+      for (String name: db.listCollectionNames())
+        System.out.println(name);
     } catch (MongoSecurityException mse) {
       System.err.println(mse.getMessage());
     }
