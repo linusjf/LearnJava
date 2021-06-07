25e00e1de55bb5e8d2b8195f0e49197cd8a7e075
fernal73 <linus.fernandes@gmail.com>
2021-06-07 14:55:35 +0530

    Add javafaker lib

---
+++ Streams/streams/ForLoops.java
@@ -0,0 +1,10 @@
+package streams;
+
+public class ForLoops
+{
+    public ForLoops()
+    {
+        
+    }
+}
+
---
+++ Streams/streams/Person.java
@@ -0,0 +1,33 @@
+package streams;
+import java.util.List;
+
+class Person {
+    private String name;
+    private int age;
+
+    private String gender;
+    private List<Person> siblings;
+
+    public Person(String name, int age, String gender, List<Person> siblings) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.siblings = siblings;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<Person> getSiblings() {
+        return siblings;
+    }
+}
