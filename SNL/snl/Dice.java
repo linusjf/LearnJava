b2f38fef81cdb7f1faf000d942ca65718e56d9e6
fernal73 <linus.fernandes@gmail.com>
2021-07-21 05:36:57 +0530

    Add sample files

---
+++ SNL/snl/Dice.java
@@ -0,0 +1,5 @@
+package snl;
+
+public interface Dice {
+  int roll();
+}
---
+++ SNL/snl/DiceFair.java
@@ -0,0 +1,20 @@
+package snl;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DiceFair implements Dice {
+  private final Random random;
+
+  public DiceFair() {
+    this(ThreadLocalRandom.current());
+  }
+
+  public DiceFair(Random random) {
+    this.random = random;
+  }
+ 
+  public int roll() {
+    return random.nextInt(1, 7);
+  }
+}
