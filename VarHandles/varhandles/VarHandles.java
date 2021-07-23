3b78f19e24c1d48e9cb5fa6bfaffde02ecc2205b
fernal73 <linus.fernandes@gmail.com>
2021-07-23 08:39:41 +0530

    Get example executing

--- VarHandles/varhandles/VarHandles.java
+++ VarHandles/varhandles/VarHandles.java
@@ -12,8 +12,22 @@ public class VarHandles {
   public int variableToGetAndAdd = 0;
   public byte variableToBitwiseOr = 0;
 
-  public void whenVarHandleForPublicVarIsCreated_ThenItIsInitializedProperly()
+  public static void main(String... args) throws Exception {
+
+    whenVarHandleForPublicVarIsCreated_ThenItIsInitializedProperly();
+    whenVarHandleForPrivateVarIsCreated_ThenItIsInitializedProperly();
+    whenVarHandleForArrayVarIsCreated_ThenItIsInitializedProperly();
+    givenVarHandle_whenGetIsInvoked_ThenValueOfVarIsReturned();
+    givenVarHandle_whenSetIsInvoked_ThenValueOfVarIsChanged();
+    givenVarHandle_whenCompareAndSetIsInvoked_ThenValueOfVarIsChanged();
+    givenVarHandle_whenGetAndAddIsInvoked_ThenValueOfVarIsChanged();
+    givenVarHandle_whenGetAndBitwiseOrIsInvoked_ThenValueOfVarIsChanged();
+  }
+
+  public static void
+  whenVarHandleForPublicVarIsCreated_ThenItIsInitializedProperly()
       throws NoSuchFieldException, IllegalAccessException {
+    VarHandles vh = new VarHandles();
     VarHandle PUBLIC_TEST_VARIABLE =
         MethodHandles.lookup()
             .in(VarHandles.class)
@@ -24,7 +38,8 @@ public class VarHandles {
         PUBLIC_TEST_VARIABLE.coordinateTypes().get(0));
   }
 
-  public void whenVarHandleForPrivateVarIsCreated_ThenItIsInitializedProperly()
+  public static void
+  whenVarHandleForPrivateVarIsCreated_ThenItIsInitializedProperly()
       throws NoSuchFieldException, IllegalAccessException {
     VarHandle PRIVATE_TEST_VARIABLE =
         MethodHandles.privateLookupIn(VarHandles.class, MethodHandles.lookup())
@@ -35,7 +50,8 @@ public class VarHandles {
         PRIVATE_TEST_VARIABLE.coordinateTypes().get(0));
   }
 
-  public void whenVarHandleForArrayVarIsCreated_ThenItIsInitializedProperly()
+  public static void
+  whenVarHandleForArrayVarIsCreated_ThenItIsInitializedProperly()
       throws NoSuchFieldException, IllegalAccessException {
     VarHandle arrayVarHandle = MethodHandles.arrayElementVarHandle(int[].class);
 
@@ -43,63 +59,69 @@ public class VarHandles {
     assert int[].class.equals(arrayVarHandle.coordinateTypes().get(0));
   }
 
-  public void givenVarHandle_whenGetIsInvoked_ThenValueOfVarIsReturned()
+  public static void givenVarHandle_whenGetIsInvoked_ThenValueOfVarIsReturned()
       throws NoSuchFieldException, IllegalAccessException {
+    VarHandles vh = new VarHandles();
     VarHandle PUBLIC_TEST_VARIABLE =
         MethodHandles.lookup()
             .in(VarHandles.class)
             .findVarHandle(VarHandles.class, "publicTestVar", int.class);
 
-    assert 1 == (int)PUBLIC_TEST_VARIABLE.get(this);
+    assert 1 == (int)PUBLIC_TEST_VARIABLE.get(vh);
   }
 
-  public void givenVarHandle_whenSetIsInvoked_ThenValueOfVarIsChanged()
+  public static void givenVarHandle_whenSetIsInvoked_ThenValueOfVarIsChanged()
       throws NoSuchFieldException, IllegalAccessException {
+    VarHandles vh = new VarHandles();
     VarHandle VARIABLE_TO_SET =
         MethodHandles.lookup()
             .in(VarHandles.class)
             .findVarHandle(VarHandles.class, "variableToSet", int.class);
 
-    VARIABLE_TO_SET.set(this, 15);
-    assert 15 == (int)VARIABLE_TO_SET.get(this);
+    VARIABLE_TO_SET.set(vh, 15);
+    assert 15 == (int)VARIABLE_TO_SET.get(vh);
   }
 
-  public void
+  public static void
   givenVarHandle_whenCompareAndSetIsInvoked_ThenValueOfVarIsChanged()
       throws NoSuchFieldException, IllegalAccessException {
+    VarHandles vh = new VarHandles();
     VarHandle VARIABLE_TO_COMPARE_AND_SET =
         MethodHandles.lookup()
             .in(VarHandles.class)
             .findVarHandle(
                 VarHandles.class, "variableToCompareAndSet", int.class);
 
-    VARIABLE_TO_COMPARE_AND_SET.compareAndSet(this, 1, 100);
-    assert 100 == (int)VARIABLE_TO_COMPARE_AND_SET.get(this);
+    VARIABLE_TO_COMPARE_AND_SET.compareAndSet(vh, 1, 100);
+    assert 100 == (int)VARIABLE_TO_COMPARE_AND_SET.get(vh);
   }
 
-  public void givenVarHandle_whenGetAndAddIsInvoked_ThenValueOfVarIsChanged()
+  public static void
+  givenVarHandle_whenGetAndAddIsInvoked_ThenValueOfVarIsChanged()
       throws NoSuchFieldException, IllegalAccessException {
+    VarHandles vh = new VarHandles();
     VarHandle VARIABLE_TO_GET_AND_ADD =
         MethodHandles.lookup()
             .in(VarHandles.class)
             .findVarHandle(VarHandles.class, "variableToGetAndAdd", int.class);
 
-    int before = (int)VARIABLE_TO_GET_AND_ADD.getAndAdd(this, 200);
+    int before = (int)VARIABLE_TO_GET_AND_ADD.getAndAdd(vh, 200);
 
     assert 0 == before;
-    assert 200 == (int)VARIABLE_TO_GET_AND_ADD.get(this);
+    assert 200 == (int)VARIABLE_TO_GET_AND_ADD.get(vh);
   }
 
-  public void
+  public static void
   givenVarHandle_whenGetAndBitwiseOrIsInvoked_ThenValueOfVarIsChanged()
       throws NoSuchFieldException, IllegalAccessException {
+    VarHandles vh = new VarHandles();
     VarHandle VARIABLE_TO_BITWISE_OR =
         MethodHandles.lookup()
             .in(VarHandles.class)
             .findVarHandle(VarHandles.class, "variableToBitwiseOr", byte.class);
-    byte before = (byte)VARIABLE_TO_BITWISE_OR.getAndBitwiseOr(this, (byte)127);
+    byte before = (byte)VARIABLE_TO_BITWISE_OR.getAndBitwiseOr(vh, (byte)127);
 
     assert 0 == before;
-    assert 127 == (byte)VARIABLE_TO_BITWISE_OR.get(this);
+    assert 127 == (byte)VARIABLE_TO_BITWISE_OR.get(vh);
   }
 }
