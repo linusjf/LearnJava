c8d6f2ad249714b71f88a501c163f6bdfc51a84b
fernal73 <linus.fernandes@gmail.com>
2021-11-17 05:27:00 +0530

    ci: added husky hooks for commit messages,upgraded tomcat and aws-java-sdk

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * commitlint.config.js
+ * Copyright (C) 2021  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+module.exports = {
+    extends: [
+        "@commitlint/config-conventional"
+    ],
+}
