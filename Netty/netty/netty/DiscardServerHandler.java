8213682e9b6ea955f8a9da37a848e54b4761cfb0
Linus Fernandes <linus.fernandes@gmail.com>
2023-04-09 14:20:06 +0530

    feat: add telnet task to Netty

--- Netty/netty/netty/DiscardServerHandler.java
+++ Netty/netty/netty/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,17 +18,23 @@ public class DiscardServerHandler extends ChannelHandlerAdapter {  // (1)
   }
 
   @Override
-  public void channelRead(ChannelHandlerContext ctx, Object msg) {  // (2)
-    // Discard the received data silently.
-    ((ByteBuf)msg).release();
-    // (3)
-  }
-
-  @Override
   public void exceptionCaught(ChannelHandlerContext ctx,
                               Throwable cause) {  // (4)
     // Close the connection when an exception is raised.
     logger.log(Level.SEVERE, cause.getMessage(), cause);
     ctx.close();
+  }
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    ByteBuf in = (ByteBuf)msg;
+    try {
+      while (in.isReadable()) {  // (1)
+        System.out.print((char)in.readByte());
+        System.out.flush();
+      }
+    } finally {
+      ReferenceCountUtil.release(msg);  // (2)
+    }
   }
 }
